46c10b37c9e483db21f44d5e00f0041e8cdd0785
Dave Shoup <dave.shoup@gmail.com>
2024-08-29 15:23:58 -0400

    check for `Error` before logging and updating secret; add comments

--- src/commands/connections.ts
+++ src/commands/connections.ts
@@ -6,7 +6,11 @@ import { getResourceManager } from "../storage/resourceManager";
 
 const logger = new Logger("commands.connections");
 
-/** Allow creating a session via the auth provider outside of the Accounts section of the VS Code UI. */
+/**
+ * Allow creating a session via the auth provider outside of the Accounts section of the VS Code UI.
+ * @remarks Depending on the outcome of the auth flow, this will also update the auth flow completed
+ * "secret" to either "true" or "false" to signal across windows that the auth flow has completed.
+ */
 async function createConnectionCommand() {
   const resourceManager = getResourceManager();
   try {
@@ -15,9 +19,11 @@ async function createConnectionCommand() {
     });
     await resourceManager.setAuthFlowCompleted(true);
   } catch (error) {
-    logger.error("error handling CCloud auth flow", error);
-    await resourceManager.setAuthFlowCompleted(false);
-    throw new Error("Failed to create new connection. Please try again.");
+    if (error instanceof Error) {
+      logger.error("error handling CCloud auth flow", { error });
+      await resourceManager.setAuthFlowCompleted(false);
+      throw new Error("Failed to create new connection. Please try again.");
+    }
   }
 }
 
