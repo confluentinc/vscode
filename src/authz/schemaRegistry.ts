7e000006098c61c39ab33fe2e120930e039ec711
Dave Shoup <dave.shoup@gmail.com>
2024-09-05 20:58:50 -0400

    not pretty, but functional

--- src/authz/schemaRegistry.ts
+++ src/authz/schemaRegistry.ts
@@ -8,6 +8,21 @@ import { getResourceManager } from "../storage/resourceManager";
 const logger = new Logger("authz.schemaRegistry");
 
 export async function canAccessSchemaForTopic(topic: KafkaTopic): Promise<boolean> {
+  // even if the topic only has one schema type or the other, we'll see a 403 if we can't access
+  // across both (key & value subject) request responses
+  // NOTE: if the subject doesn't follow the TopicNameStrategy, we won't be able to track it via
+  // other extension features
+  const [keyAccess, valueAccess] = await Promise.all([
+    canAccessSchemaTypeForTopic(topic, "key"),
+    canAccessSchemaTypeForTopic(topic, "value"),
+  ]);
+  return keyAccess || valueAccess;
+}
+
+export async function canAccessSchemaTypeForTopic(
+  topic: KafkaTopic,
+  type: "key" | "value",
+): Promise<boolean> {
   if (!topic.environmentId) {
     // no way of checking local topic schemas currently
     return true;
@@ -35,33 +50,51 @@ export async function canAccessSchemaForTopic(topic: KafkaTopic): Promise<boolea
 
   // this will either raise ResponseErrors or pass
   try {
-    const [keySchema, valueSchema] = await Promise.all([
-      // even if the topic only has one schema type or the other, we'll see a 403 if we can't access
-      // across both (key & value subject) request responses
-      // NOTE: if the subject doesn't follow the TopicNameStrategy, we won't be able to track it via
-      // other extension features
-      client.lookUpSchemaUnderSubject({
-        subject: `${topic.name}-key`,
-        RegisterSchemaRequest: {},
-      }),
-      client.lookUpSchemaUnderSubject({
-        subject: `${topic.name}-value`,
-        RegisterSchemaRequest: {},
-      }),
-    ]);
-    logger.debug("successfully looked up schemas for topic", {
-      keySchema: keySchema,
-      valueSchema: valueSchema,
+    const schemaResp = await client.lookUpSchemaUnderSubject({
+      subject: `${topic.name}-${type}`,
+      RegisterSchemaRequest: {},
+    });
+    logger.debug("successfully looked up schema for topic", {
+      schemaResp: schemaResp,
       topic: topic,
     });
     return true;
   } catch (error) {
     if (error instanceof ResponseError) {
-      const body = await error.response.json();
-      logger.error("error response looking up subject:", body);
+      return await determineAccessFromResponseError(error.response);
     } else {
       logger.error("error making lookupSchemaUnderSubject request:", error);
     }
     return false;
   }
+}
+
+async function determineAccessFromResponseError(response: Response): Promise<boolean> {
+  const body = await response.json();
+  logger.error("error response looking up subject:", body);
+  switch (response.status) {
+    case 401:
+      // unauthorized
+      return false;
+    case 403:
+      // forbidden
+      return false;
+    case 404:
+      {
+        // not found
+        if (body.error_code === 40401) {
+          // "Subject '...' not found" = no schema(s) for the topic
+          return true;
+        } else if (body.error_code === 40403) {
+          // "Schema not found" = schema exists but this endpoint can't get it (???)
+          return true;
+        }
+      }
+      break;
+    default:
+      // unknown error
+      return false;
+  }
+
+  return false;
 }
