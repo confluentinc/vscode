5114c61188eceb312ce06291bfe58f5b7d69cc85
Dave Shoup <dave.shoup@gmail.com>
2024-09-09 16:18:35 -0400

    update CCloud environment tooltip

--- src/models/environment.ts
+++ src/models/environment.ts
@@ -1,6 +1,7 @@
 import { Data, type Require as Enforced } from "dataclass";
 import * as vscode from "vscode";
 import { CCLOUD_CONNECTION_ID, IconNames } from "../constants";
+import { CustomMarkdownString } from "./main";
 
 // Main class representing CCloud environments, matching key/value pairs returned
 // by the `confluent environment list` command.
@@ -26,9 +27,17 @@ export class CCloudEnvironmentTreeItem extends vscode.TreeItem {
     this.resource = resource;
 
     this.contextValue = "ccloud-environment";
-    this.tooltip = JSON.stringify(this.resource, null, 2);
     this.description = this.resource.id;
 
     this.iconPath = new vscode.ThemeIcon(IconNames.CCLOUD_ENVIRONMENT);
+
+    this.tooltip = new CustomMarkdownString()
+      .appendMarkdown(`Name: \`${this.resource.name}\`\n\n`)
+      .appendMarkdown(`ID: \`${this.resource.id}\`\n\n`)
+      .appendMarkdown(
+        `Stream Governance Package: \`${this.resource.stream_governance_package}\`\n\n`,
+      )
+      .appendMarkdown("---\n\n")
+      .appendMarkdown(`[Open in Confluent Cloud](${this.resource.ccloudUrl})`);
   }
 }
