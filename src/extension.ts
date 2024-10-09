fca6be12df80fa167cf9b70687b7dda6190e9590
Dave Shoup <dave.shoup@gmail.com>
2024-10-09 10:05:42 -0400

    fix circular dependency

--- src/extension.ts
+++ src/extension.ts
@@ -50,7 +50,7 @@ import { registerTopicCommands } from "./commands/topics";
 import { AUTH_PROVIDER_ID, AUTH_PROVIDER_LABEL } from "./constants";
 import { activateMessageViewer } from "./consume";
 import { ContextValues, setContextValue, setExtensionContext } from "./context";
-import { listenForEvents } from "./docker";
+import { listenForEvents } from "./docker/listener";
 import { SchemaDocumentProvider } from "./documentProviders/schema";
 import { Logger, outputChannel } from "./logging";
 import { SSL_PEM_PATHS, SSL_VERIFY_SERVER_CERT_DISABLED } from "./preferences/constants";
