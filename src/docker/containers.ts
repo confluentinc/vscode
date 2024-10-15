9c159464bc572163d876fc659371937eb13dd32b
Dave Shoup <dave.shoup@gmail.com>
2024-10-15 19:34:32 -0400

    wip for stopping/deleting containers

--- src/docker/containers.ts
+++ src/docker/containers.ts
@@ -59,6 +59,12 @@ export async function createContainer(
   imageTag: string,
   request: ContainerCreateOperationRequest,
 ): Promise<ContainerCreateResponse | undefined> {
+  const existingContainers: ContainerSummary[] = await getContainersForImage(imageRepo, imageTag);
+  if (existingContainers.length > 0) {
+    // isn't shown to the user:
+    throw new ContainerExistsError("Container already exists");
+  }
+
   if (!(await imageExists(imageRepo, imageTag))) {
     await pullImage(imageRepo, imageTag);
   }
@@ -107,6 +113,26 @@ export async function startContainer(containerId: string) {
   }
 }
 
+export async function deleteContainer(id: string) {
+  const client = new ContainerApi();
+  const init: RequestInit = defaultRequestInit();
+
+  try {
+    await client.containerDelete({ id }, init);
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      const body = await streamToString(error.response.clone().body);
+      logger.error("Error response deleting container:", {
+        status: error.response.status,
+        statusText: error.response.statusText,
+        body: body,
+      });
+    } else {
+      logger.error("Error removing container:", error);
+    }
+  }
+}
+
 export async function getContainer(id: string): Promise<ContainerInspectResponse | undefined> {
   const client = new ContainerApi();
   const init: RequestInit = defaultRequestInit();
@@ -123,6 +149,26 @@ export async function getContainer(id: string): Promise<ContainerInspectResponse
       });
     } else {
       logger.error("Error inspecting container:", error);
+    }
+  }
+}
+
+export async function stopContainer(id: string) {
+  const client = new ContainerApi();
+  const init: RequestInit = defaultRequestInit();
+
+  try {
+    await client.containerStop({ id }, init);
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      const body = await streamToString(error.response.clone().body);
+      logger.error("Error response stopping container:", {
+        status: error.response.status,
+        statusText: error.response.statusText,
+        body: body,
+      });
+    } else {
+      logger.error("Error stopping container:", error);
     }
   }
 }
