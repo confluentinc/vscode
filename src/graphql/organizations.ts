0607dc7624dccef7c92f53d95bff8bce61f124b0
Dave Shoup <dave.shoup@gmail.com>
2024-10-15 15:06:09 -0400

    also try/catch org loading

--- src/graphql/organizations.ts
+++ src/graphql/organizations.ts
@@ -1,7 +1,10 @@
 import { graphql } from "gql.tada";
 import { CCLOUD_CONNECTION_ID } from "../constants";
+import { Logger } from "../logging";
 import { CCloudOrganization } from "../models/organization";
 import { getSidecar } from "../sidecar";
+
+const logger = new Logger("graphql.organizations");
 
 export async function getOrganizations(): Promise<CCloudOrganization[]> {
   let orgResponse: CCloudOrganization[] = [];
@@ -19,11 +22,20 @@ export async function getOrganizations(): Promise<CCloudOrganization[]> {
   `);
 
   const sidecar = await getSidecar();
-  const response = await sidecar.query(query, CCLOUD_CONNECTION_ID, { id: CCLOUD_CONNECTION_ID });
-  if (response.ccloudConnectionById?.organizations) {
-    response.ccloudConnectionById.organizations.forEach((org: any) => {
-      orgResponse.push(CCloudOrganization.create(org));
-    });
+
+  try {
+    const response = await sidecar.query(query, CCLOUD_CONNECTION_ID, { id: CCLOUD_CONNECTION_ID });
+    if (response.ccloudConnectionById?.organizations) {
+      response.ccloudConnectionById.organizations.forEach((org: any) => {
+        try {
+          orgResponse.push(CCloudOrganization.create(org));
+        } catch (e) {
+          logger.error("Failed to create organization:", e);
+        }
+      });
+    }
+  } catch (e) {
+    logger.error("Failed to fetch organizations:", e);
   }
 
   return orgResponse;
