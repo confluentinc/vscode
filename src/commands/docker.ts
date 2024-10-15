af02c9a5afd628fc701690b36fa2421cacaeab0d
Dave Shoup <dave.shoup@gmail.com>
2024-10-15 19:34:32 -0400

    wip for stopping/deleting containers

--- src/commands/docker.ts
+++ src/commands/docker.ts
@@ -11,6 +11,10 @@ async function startLocalResourcesWithProgress() {
   await runWorkflowWithProgress();
 }
 
+async function stopLocalResourcesWithProgress() {
+  await runWorkflowWithProgress(false);
+}
+
 /** Run the local resource workflow(s) with a progress notification. */
 async function runWorkflowWithProgress(start: boolean = true) {
   const dockerAvailable = await isDockerAvailable();
@@ -67,6 +71,10 @@ export function registerDockerCommands(): Disposable[] {
     registerCommandWithLogging(
       "confluent.docker.startLocalResources",
       startLocalResourcesWithProgress,
+    ),
+    registerCommandWithLogging(
+      "confluent.docker.stopLocalResources",
+      stopLocalResourcesWithProgress,
     ),
   ];
 }
