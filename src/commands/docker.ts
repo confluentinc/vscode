654c7734945bcc45a2a044ecafd56b11720ab0c3
Dave Shoup <dave.shoup@gmail.com>
2024-10-16 19:40:38 -0400

    create image-specific workflows and remove image-specific startup logic from `src/docker/*.ts` and `src/commands/docker.ts`

--- src/commands/docker.ts
+++ src/commands/docker.ts
@@ -1,24 +1,12 @@
-import { CancellationToken, Disposable, Progress, ProgressLocation, window } from "vscode";
+import { CancellationToken, Disposable, ProgressLocation, window } from "vscode";
 import { registerCommandWithLogging } from ".";
-import { ContainerCreateResponse } from "../clients/docker";
 import { isDockerAvailable } from "../docker/configs";
-import { ContainerExistsError, createContainer, startContainer } from "../docker/containers";
-import {
-  getLocalKafkaImageName,
-  getLocalKafkaImageTag,
-  imageExists,
-  pullImage,
-} from "../docker/images";
-import { createNetwork } from "../docker/networks";
-import { localKafkaConnected } from "../emitters";
+import { getLocalKafkaImageName } from "../docker/images";
+import { LocalResourceWorkflow } from "../docker/workflows";
+import { ConfluentLocalWorkflow } from "../docker/workflows/confluent-local";
 import { Logger } from "../logging";
 
 const logger = new Logger("commands.docker");
-
-type NotificationProgress = Progress<{
-  message?: string;
-  increment?: number;
-}>;
 
 async function launchLocalKafkaWithProgress() {
   const dockerAvailable = await isDockerAvailable();
@@ -28,79 +16,30 @@ async function launchLocalKafkaWithProgress() {
   }
 
   const imageRepo: string = getLocalKafkaImageName();
-  const imageTag: string = getLocalKafkaImageTag();
+  logger.debug("using image repo", { imageRepo, confluent: ConfluentLocalWorkflow.imageRepo });
+  // based on the imageRepo chosen by the user, select the appropriate workflow before starting
+  let workflow: LocalResourceWorkflow;
+  switch (imageRepo) {
+    case ConfluentLocalWorkflow.imageRepo:
+      workflow = ConfluentLocalWorkflow.getInstance();
+      break;
+    // TODO: add support for other images here
+    default:
+      window.showErrorMessage(`Unsupported image repo: ${imageRepo}`);
+      return;
+  }
+
+  logger.debug("starting local Kafka with workflow", { imageRepo });
   window.withProgress(
     {
       location: ProgressLocation.Notification,
       title: "Local Kafka",
+      cancellable: true,
     },
-    async (progress: NotificationProgress, token: CancellationToken) => {
-      await launchLocalKafka(progress, token, imageRepo, imageTag);
+    async (progress, token: CancellationToken) => {
+      await workflow.start(token, progress);
     },
   );
-}
-
-async function launchLocalKafka(
-  progress: NotificationProgress,
-  token: CancellationToken,
-  imageRepo: string,
-  imageTag: string,
-): Promise<void> {
-  if (token.isCancellationRequested) {
-    // TODO: cleanup created container?
-    return;
-  }
-
-  // check for image first and pull if necessary
-  const checkImageMsg = `Checking for "${imageRepo}:${imageTag}"...`;
-  logger.debug(checkImageMsg);
-  progress.report({ message: checkImageMsg });
-  const existingImage = await imageExists(imageRepo, imageTag);
-  logger.debug(`Image exists: ${existingImage}`, { imageRepo, imageTag });
-  if (!existingImage) {
-    const pullImageMsg = `Pulling "${imageRepo}:${imageTag}"...`;
-    logger.debug(pullImageMsg);
-    progress.report({ message: pullImageMsg });
-    await pullImage(imageRepo, imageTag);
-  }
-
-  // create the network if it doesn't exist
-  await createNetwork("confluent-local-network");
-
-  const createContainerMsg = "Creating container...";
-  logger.debug(createContainerMsg);
-  progress.report({ message: createContainerMsg });
-  let containerId: string;
-  try {
-    const container: ContainerCreateResponse | undefined = await createContainer(
-      imageRepo,
-      imageTag,
-    );
-    if (!container) {
-      window.showErrorMessage("Failed to create local Kafka container.");
-      return;
-    }
-    containerId = container.Id;
-  } catch (error) {
-    if (error instanceof ContainerExistsError) {
-      // TODO: handle when container needs to be stopped first
-    } else {
-      logger.error("Error creating container:", error);
-    }
-    return;
-  }
-
-  const startContainerMsg = "Starting container...";
-  logger.debug(startContainerMsg);
-  progress.report({ message: startContainerMsg });
-  await startContainer(containerId);
-
-  // block until the `localKafkaConnected` emitter fires
-  await new Promise((resolve) => {
-    localKafkaConnected.event(() => {
-      resolve(void 0);
-    });
-  });
 }
 
 export function registerDockerCommands(): Disposable[] {
--- src/docker/containers.ts
+++ src/docker/containers.ts
@@ -1,9 +1,6 @@
-import * as net from "net";
-import { window, workspace, WorkspaceConfiguration } from "vscode";
 import {
   ContainerApi,
   ContainerCreateOperationRequest,
-  ContainerCreateRequest,
   ContainerCreateResponse,
   ContainerListRequest,
   ContainerStateStatusEnum,
@@ -11,11 +8,6 @@ import {
   ResponseError,
 } from "../clients/docker";
 import { Logger } from "../logging";
-import {
-  LOCAL_KAFKA_PLAINTEXT_PORT,
-  LOCAL_KAFKA_REST_HOST,
-  LOCAL_KAFKA_REST_PORT,
-} from "../preferences/constants";
 import { defaultRequestInit } from "./configs";
 import { imageExists, pullImage } from "./images";
 import { streamToString } from "./stream";
@@ -64,101 +56,20 @@ export async function getContainersForImage(
 export async function createContainer(
   imageRepo: string,
   imageTag: string,
+  request: ContainerCreateOperationRequest,
 ): Promise<ContainerCreateResponse | undefined> {
   const existingContainers: ContainerSummary[] = await getContainersForImage(imageRepo, imageTag);
   if (existingContainers.length > 0) {
-    // TODO(shoup): add buttons for (re)start / delete container in follow-on branch
-    window.showWarningMessage(
-      "Local Kafka container already exists. Please remove it and try again.",
-    );
-    // isn't shown to the user:
     throw new ContainerExistsError("Container already exists");
-  } else {
-    if (!(await imageExists(imageRepo, imageTag))) {
-      await pullImage(imageRepo, imageTag);
-    }
+  }
+
+  if (!(await imageExists(imageRepo, imageTag))) {
+    await pullImage(imageRepo, imageTag);
   }
   logger.debug("Creating container from image", { imageRepo, imageTag });
 
-  const repoTag = imageTag === "latest" ? imageRepo : `${imageRepo}:${imageTag}`;
-
   const client = new ContainerApi();
   const init: RequestInit = defaultRequestInit();
-
-  const config: WorkspaceConfiguration = workspace.getConfiguration();
-
-  const kafkaRestHost: string = config.get(LOCAL_KAFKA_REST_HOST, "localhost");
-  const kafkaRestPort: number = config.get(LOCAL_KAFKA_REST_PORT, 8082);
-  const plaintextPort: number = config.get(LOCAL_KAFKA_PLAINTEXT_PORT, 9092);
-
-  const brokerPort: number = await findFreePort();
-  const controllerPort: number = await findFreePort();
-  logger.debug("Using ports", {
-    plaintextPort,
-    brokerPort,
-    controllerPort,
-  });
-
-  // TODO: change this depending on image
-  const brokerContainerName: string = "confluent-local-broker-1";
-
-  const hostConfig = {
-    NetworkMode: "confluent-local-network",
-    PortBindings: {
-      [`${kafkaRestPort}/tcp`]: [
-        {
-          HostIp: "0.0.0.0",
-          HostPort: kafkaRestPort.toString(),
-        },
-      ],
-      [`${plaintextPort}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: plaintextPort.toString() }],
-      [`${brokerPort}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: brokerPort.toString() }],
-      [`${controllerPort}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: controllerPort.toString() }],
-    },
-  };
-
-  const containerEnv = [
-    "KAFKA_BROKER_ID=1",
-    "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT",
-    `KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://${brokerContainerName}:${plaintextPort},PLAINTEXT_HOST://${kafkaRestHost}:${plaintextPort}`,
-    "KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1",
-    "KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS=0",
-    "KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1",
-    "KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1",
-    "KAFKA_PROCESS_ROLES=broker,controller",
-    "KAFKA_NODE_ID=1",
-    `KAFKA_CONTROLLER_QUORUM_VOTERS=1@${brokerContainerName}:${controllerPort}`,
-    `KAFKA_LISTENERS=PLAINTEXT://${brokerContainerName}:${brokerPort},CONTROLLER://${brokerContainerName}:${controllerPort},PLAINTEXT_HOST://0.0.0.0:${plaintextPort}`,
-    "KAFKA_INTER_BROKER_LISTENER_NAME=PLAINTEXT",
-    "KAFKA_CONTROLLER_LISTENER_NAMES=CONTROLLER",
-    "KAFKA_LOG_DIRS=/tmp/kraft-combined-logs",
-    "KAFKA_REST_HOST_NAME=rest-proxy",
-    `KAFKA_REST_LISTENERS=http://0.0.0.0:8082`,
-    `KAFKA_REST_BOOTSTRAP_SERVERS=${brokerContainerName}:${brokerPort}`,
-  ];
-
-  // create the container before starting
-  const body: ContainerCreateRequest = {
-    Image: repoTag,
-    Hostname: brokerContainerName,
-    Cmd: ["bash", "-c", "'/etc/confluent/docker/run'"],
-    ExposedPorts: {
-      [`${kafkaRestPort}/tcp`]: {},
-      [`${plaintextPort}/tcp`]: {},
-      [`${brokerPort}/tcp`]: {},
-      [`${controllerPort}/tcp`]: {},
-    },
-    HostConfig: hostConfig,
-    Env: containerEnv,
-    Tty: true,
-  };
-  const request: ContainerCreateOperationRequest = {
-    body,
-    name: brokerContainerName,
-    // platform: process.platform, // TODO: determine how to provide this without raising 404s
-  };
-  logger.debug("Creating container with request", request);
-
   try {
     const response: ContainerCreateResponse = await client.containerCreate(request, init);
     logger.info("Container created successfully", response);
@@ -167,7 +78,7 @@ export async function createContainer(
     if (error instanceof ResponseError) {
       const body = await streamToString(error.response.clone().body);
 
-      // TODO: if port is occupied, float a notification with action to update LOCAL_KAFKA_PLAINTEXT_PORT setting
+      // TODO: if port is occupied, throw a more specific error
 
       logger.error("Container creation returned error response:", {
         status: error.response.status,
@@ -198,18 +109,6 @@ export async function startContainer(containerId: string) {
       logger.error("Error starting container:", error);
     }
   }
-}
-
-async function findFreePort(): Promise<number> {
-  return new Promise((resolve, reject) => {
-    const server = net.createServer();
-    server.unref();
-    server.on("error", reject);
-    server.listen(0, () => {
-      const port = (server.address() as net.AddressInfo).port;
-      server.close(() => resolve(port));
-    });
-  });
 }
 
 export class ContainerExistsError extends Error {
