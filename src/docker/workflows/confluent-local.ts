588459bdcbf8fb3c487591c9b6d9a62754db5447
Dave Shoup <dave.shoup@gmail.com>
2024-10-17 10:51:48 -0400

    placeholder

--- src/docker/workflows/confluent-local.ts
+++ src/docker/workflows/confluent-local.ts
@@ -63,6 +63,7 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
   async start(
     token: CancellationToken,
     progress?: Progress<{ message?: string; increment?: number }>,
+    withSchemaRegistry: boolean = false,
   ): Promise<void> {
     this.progress = progress;
     this.imageTag = getLocalKafkaImageTag();
