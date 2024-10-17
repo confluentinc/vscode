3679b9845322368d14a237002013be4278e9ef1f
Dave Shoup <dave.shoup@gmail.com>
2024-10-17 09:50:35 -0400

    cleanup/comments

--- src/docker/workflows/confluent-local.ts
+++ src/docker/workflows/confluent-local.ts
@@ -1,10 +1,6 @@
 import { CancellationToken, Progress, window, workspace, WorkspaceConfiguration } from "vscode";
 import { findFreePort, LocalResourceWorkflow } from ".";
-import {
-  ContainerCreateOperationRequest,
-  ContainerCreateRequest,
-  ContainerCreateResponse,
-} from "../../clients/docker";
+import { ContainerCreateRequest, ContainerCreateResponse, HostConfig } from "../../clients/docker";
 import { LOCAL_KAFKA_REST_PORT } from "../../constants";
 import { localKafkaConnected } from "../../emitters";
 import { Logger } from "../../logging";
@@ -16,7 +12,6 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
   static imageRepo = "confluentinc/confluent-local";
   logger = new Logger("docker.workflow.confluent-local");
 
-  brokerContainerName: string = "confluent-local-broker-1";
   networkName: string = "confluent-local-network";
 
   // ensure only one instance of this workflow can run at a time
@@ -54,11 +49,11 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
     this.progress?.report({ message: createNetworkMsg });
     await createNetwork(this.networkName);
 
+    // TODO: support multiple broker containers?
     const containerId: string | undefined = await this.createKafkaContainer();
     if (!containerId) {
       return;
     }
-
     const startContainerMsg = "Starting container...";
     this.logger.debug(startContainerMsg);
     this.progress?.report({ message: startContainerMsg });
@@ -84,6 +79,8 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
     // TODO(shoup): implement
   }
 
+  /** Block until we see the {@link localKafkaConnected} event fire. (Controlled by the EventListener
+   * in `src/docker/eventListener.ts` whenever a supported container starts or dies.) */
   async waitForConnectionChangeEvent(): Promise<void> {
     await new Promise((resolve) => {
       localKafkaConnected.event(() => {
@@ -92,91 +89,42 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
     });
   }
 
-  get plainTextPort(): number {
-    return workspace.getConfiguration().get(LOCAL_KAFKA_PLAINTEXT_PORT, 9092);
-  }
-
   async createKafkaContainer(): Promise<string | undefined> {
     const createContainerMsg = "Creating container...";
     this.logger.debug(createContainerMsg);
     this.progress?.report({ message: createContainerMsg });
 
-    const config: WorkspaceConfiguration = workspace.getConfiguration();
+    // TODO: support multiple broker containers?
+    const brokerContainerName: string = "confluent-local-broker-1";
 
-    // TODO: determine if this needs to be configurable (i.e. for WSL)
-    const kafkaRestHost: string = config.get(LOCAL_KAFKA_REST_HOST, "localhost");
-
-    const brokerPort: number = await findFreePort();
-    const controllerPort: number = await findFreePort();
-    this.logger.debug("Using ports", {
-      plaintextPort: this.plainTextPort,
-      brokerPort,
-      controllerPort,
-    });
-
-    const hostConfig = {
-      NetworkMode: this.networkName,
-      PortBindings: {
-        [`${LOCAL_KAFKA_REST_PORT}/tcp`]: [
-          {
-            HostIp: "0.0.0.0",
-            HostPort: LOCAL_KAFKA_REST_PORT.toString(),
-          },
-        ],
-        [`${this.plainTextPort}/tcp`]: [
-          { HostIp: "0.0.0.0", HostPort: this.plainTextPort.toString() },
-        ],
-        [`${brokerPort}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: brokerPort.toString() }],
-        [`${controllerPort}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: controllerPort.toString() }],
-      },
-    };
-
-    const containerEnv = [
-      "KAFKA_BROKER_ID=1",
-      "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT",
-      `KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://${this.brokerContainerName}:${this.plainTextPort},PLAINTEXT_HOST://${kafkaRestHost}:${this.plainTextPort}`,
-      "KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1",
-      "KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS=0",
-      "KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1",
-      "KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1",
-      "KAFKA_PROCESS_ROLES=broker,controller",
-      "KAFKA_NODE_ID=1",
-      `KAFKA_CONTROLLER_QUORUM_VOTERS=1@${this.brokerContainerName}:${controllerPort}`,
-      `KAFKA_LISTENERS=PLAINTEXT://${this.brokerContainerName}:${brokerPort},CONTROLLER://${this.brokerContainerName}:${controllerPort},PLAINTEXT_HOST://0.0.0.0:${this.plainTextPort}`,
-      "KAFKA_INTER_BROKER_LISTENER_NAME=PLAINTEXT",
-      "KAFKA_CONTROLLER_LISTENER_NAMES=CONTROLLER",
-      "KAFKA_LOG_DIRS=/tmp/kraft-combined-logs",
-      "KAFKA_REST_HOST_NAME=rest-proxy",
-      `KAFKA_REST_LISTENERS=http://0.0.0.0:${LOCAL_KAFKA_REST_PORT}`,
-      `KAFKA_REST_BOOTSTRAP_SERVERS=${this.brokerContainerName}:${brokerPort}`,
-    ];
+    const ports: KafkaContainerPorts = await this.configurePorts();
+    const hostConfig: HostConfig = this.generateHostConfig(ports);
+    const containerEnv: string[] = this.generateContainerEnv(brokerContainerName, ports);
 
     // create the container before starting
     const body: ContainerCreateRequest = {
       Image: `${ConfluentLocalWorkflow.imageRepo}:${this.imageTag}`,
-      Hostname: this.brokerContainerName,
+      Hostname: brokerContainerName,
       Cmd: ["bash", "-c", "'/etc/confluent/docker/run'"],
       ExposedPorts: {
         [`${LOCAL_KAFKA_REST_PORT}/tcp`]: {},
-        [`${this.plainTextPort}/tcp`]: {},
-        [`${brokerPort}/tcp`]: {},
-        [`${controllerPort}/tcp`]: {},
+        [`${ports.plainText}/tcp`]: {},
+        [`${ports.broker}/tcp`]: {},
+        [`${ports.controller}/tcp`]: {},
       },
       HostConfig: hostConfig,
       Env: containerEnv,
       Tty: true,
     };
 
-    const request: ContainerCreateOperationRequest = {
-      body,
-      name: this.brokerContainerName,
-    };
-
     try {
       const container: ContainerCreateResponse | undefined = await createContainer(
         ConfluentLocalWorkflow.imageRepo,
         this.imageTag,
-        request,
+        {
+          body,
+          name: brokerContainerName,
+        },
       );
       if (!container) {
         window.showErrorMessage("Failed to create Kafka container.");
@@ -195,4 +143,71 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
       return;
     }
   }
+
+  /**
+   * Configure the ports to use for a Kafka container.
+   *
+   * The plaintext port is configurable by the user, but the broker and controller ports are dynamically
+   * assigned based on available ports on the host machine.
+   */
+  private async configurePorts(): Promise<KafkaContainerPorts> {
+    const plainText: number = workspace.getConfiguration().get(LOCAL_KAFKA_PLAINTEXT_PORT, 9092);
+    const broker: number = await findFreePort();
+    const controller: number = await findFreePort();
+    this.logger.debug("Using ports", {
+      plainText,
+      broker,
+      controller,
+    });
+    return { plainText, broker, controller };
+  }
+
+  /** Generate the HostConfig for a Kafka container using provided ports and this workflow's network name. */
+  private generateHostConfig(ports: KafkaContainerPorts): HostConfig {
+    return {
+      NetworkMode: this.networkName,
+      PortBindings: {
+        [`${LOCAL_KAFKA_REST_PORT}/tcp`]: [
+          {
+            HostIp: "0.0.0.0",
+            HostPort: LOCAL_KAFKA_REST_PORT.toString(),
+          },
+        ],
+        [`${ports.plainText}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: ports.plainText.toString() }],
+        [`${ports.broker}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: ports.broker.toString() }],
+        [`${ports.controller}/tcp`]: [{ HostIp: "0.0.0.0", HostPort: ports.controller.toString() }],
+      },
+    };
+  }
+
+  private generateContainerEnv(brokerContainerName: string, ports: KafkaContainerPorts): string[] {
+    const config: WorkspaceConfiguration = workspace.getConfiguration();
+    // TODO: determine if this needs to be configurable (i.e. for WSL)
+    const kafkaRestHost: string = config.get(LOCAL_KAFKA_REST_HOST, "localhost");
+    return [
+      `KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://${brokerContainerName}:${ports.plainText},PLAINTEXT_HOST://${kafkaRestHost}:${ports.plainText}`,
+      "KAFKA_BROKER_ID=1",
+      "KAFKA_CONTROLLER_LISTENER_NAMES=CONTROLLER",
+      `KAFKA_CONTROLLER_QUORUM_VOTERS=1@${brokerContainerName}:${ports.controller}`,
+      "KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS=0",
+      "KAFKA_INTER_BROKER_LISTENER_NAME=PLAINTEXT",
+      "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT",
+      `KAFKA_LISTENERS=PLAINTEXT://${brokerContainerName}:${ports.broker},CONTROLLER://${brokerContainerName}:${ports.controller},PLAINTEXT_HOST://0.0.0.0:${ports.plainText}`,
+      "KAFKA_LOG_DIRS=/tmp/kraft-combined-logs",
+      "KAFKA_NODE_ID=1",
+      "KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1",
+      "KAFKA_PROCESS_ROLES=broker,controller",
+      `KAFKA_REST_BOOTSTRAP_SERVERS=${brokerContainerName}:${ports.broker}`,
+      "KAFKA_REST_HOST_NAME=rest-proxy",
+      `KAFKA_REST_LISTENERS=http://0.0.0.0:${LOCAL_KAFKA_REST_PORT}`,
+      "KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1",
+      "KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1",
+    ];
+  }
+}
+
+interface KafkaContainerPorts {
+  plainText: number;
+  broker: number;
+  controller: number;
 }
