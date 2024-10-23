dde6e9fbe428d1bfe49935d6f9dd3cdea31a5f78
Dave Shoup <dave.shoup@gmail.com>
2024-10-23 08:36:10 -0400

    remove dedicated launch Kafka+SR command in favor of launch local resources with multi-select quickpick

--- src/commands/docker.ts
+++ src/commands/docker.ts
@@ -1,9 +1,18 @@
-import { CancellationToken, Disposable, ProgressLocation, Uri, env, window } from "vscode";
+import {
+  CancellationToken,
+  Disposable,
+  env,
+  ProgressLocation,
+  QuickPickItem,
+  Uri,
+  window,
+} from "vscode";
 import { registerCommandWithLogging } from ".";
 import { getLocalKafkaImageName, isDockerAvailable } from "../docker/configs";
 import { LocalResourceWorkflow } from "../docker/workflows";
 import { ConfluentLocalWorkflow } from "../docker/workflows/confluent-local";
 import { Logger } from "../logging";
+import { localResourcesQuickPick } from "../quickpicks/localResources";
 
 const logger = new Logger("commands.docker");
 
@@ -15,7 +24,8 @@ async function stopLocalResourcesWithProgress() {
   await runWorkflowWithProgress(false);
 }
 
-/** Run the local resource workflow(s) with a progress notification. */
+/** Prompt the user with a multi-select quickpick, allowing them to choose which resource types to
+ * start. Then run the local resource workflow(s) with a progress notification. */
 async function runWorkflowWithProgress(start: boolean = true) {
   const dockerAvailable = await isDockerAvailable();
   if (!dockerAvailable) {
@@ -33,19 +43,24 @@ async function runWorkflowWithProgress(start: boolean = true) {
     return;
   }
 
-  // TODO(shoup): add multi-select quickpick to determine which resource(s) to start/stop; for now
-  // just default to Kafka
-  const resources = ["Kafka"];
+  // show multi-select quickpick to allow user to choose which resources to launch and determine
+  // how the workflow should be run
+  const resources: QuickPickItem[] = await localResourcesQuickPick();
+  const resourceLabels: string[] = resources.map((resource) => resource.label);
 
   // based on the imageRepo chosen by the user, select the appropriate workflow before running them
   const subworkflows: LocalResourceWorkflow[] = [];
-  if (resources.includes("Kafka")) {
+  if (resourceLabels.includes("Kafka")) {
     const kafkaWorkflow = getKafkaWorkflow();
     if (kafkaWorkflow) subworkflows.push(kafkaWorkflow);
   }
+  if (resourceLabels.includes("Schema Registry")) {
+    const schemaRegistryWorkflow = getSchemaRegistryWorkflow();
+    if (schemaRegistryWorkflow) subworkflows.push(schemaRegistryWorkflow);
+  }
   // add logic for looking up other resources' workflows here
 
-  logger.debug("running local resource workflow(s)", { start, resources });
+  logger.debug("running local resource workflow(s)", { start, resourceLabels });
   window.withProgress(
     {
       location: ProgressLocation.Notification,
@@ -93,4 +108,9 @@ export function getKafkaWorkflow(): LocalResourceWorkflow | undefined {
       return;
   }
   return workflow;
+}
+
+function getSchemaRegistryWorkflow(): LocalResourceWorkflow | undefined {
+  // TODO: implement this once the ConfluentPlatformSchemaRegistryWorkflow is available
+  return;
 }
