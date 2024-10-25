b84cd16f6daebe95584b90224abe4f02b2e6ee7a
Dave Shoup <dave.shoup@gmail.com>
2024-10-25 11:58:48 -0400

    remove unused arg

--- src/docker/workflows/confluent-local.ts
+++ src/docker/workflows/confluent-local.ts
@@ -63,7 +63,6 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
   async start(
     token: CancellationToken,
     progress?: Progress<{ message?: string; increment?: number }>,
-    withSchemaRegistry: boolean = false,
   ): Promise<void> {
     this.progress = progress;
     this.imageTag = getLocalKafkaImageTag();
