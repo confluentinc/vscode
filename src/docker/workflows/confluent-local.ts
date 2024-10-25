0370a24f0dec43ebdccc7e96de591a2e3ba18458
Dave Shoup <dave.shoup@gmail.com>
2024-10-25 11:54:30 -0400

    await stop promises and remove leading slashes

--- src/docker/workflows/confluent-local.ts
+++ src/docker/workflows/confluent-local.ts
@@ -191,6 +191,7 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
       }
       promises.push(this.stopContainer({ id: container.Id, name: container.Names[0] }));
     }
+    await Promise.all(promises);
 
     await this.waitForLocalResourceEventChange();
   }
@@ -345,8 +346,10 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
   }
 
   private async stopContainer(container: LocalResourceContainer): Promise<void> {
+    // names may start with a leading slash, so try to remove it
+    const containerName = container.name.replace(/^\/+/, "");
+    this.progress?.report({ message: `Stopping container "${containerName}"...` });
     // check container status before deleting
-    this.progress?.report({ message: `Stopping container "${container.name}"...` });
     const existingContainer: ContainerInspectResponse | undefined = await getContainer(
       container.id,
     );
@@ -354,7 +357,7 @@ export class ConfluentLocalWorkflow extends LocalResourceWorkflow {
       // assume it was cleaned up some other way
       this.logger.warn("Container not found, skipping stop and delete steps.", {
         id: container.id,
-        name: container.name,
+        name: containerName,
       });
       return;
     }
