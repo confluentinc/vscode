256616ec184cb73dcb4634ae5f5a765b4a8266b0
Dave Shoup <dave.shoup@gmail.com>
2024-10-28 10:02:51 -0400

    fix+cleanup sidecar handle methods (#456)

--- src/sidecar/constants.ts
+++ src/sidecar/constants.ts
@@ -7,14 +7,17 @@ export const SIDECAR_PORT: number = 26636;
 export const SIDECAR_BASE_URL: string =
   process.env.SIDECAR_BASE_URL || `http://127.0.0.1:${SIDECAR_PORT}`;
 
-export const SIDECAR_CURRENT_CONNECTION_ID_HEADER: string = "x-connection-id"; // Header name for the sidecar's current connection ID in the request headers.
-export const WORKSPACE_PROCESS_ID_HEADER: string = "x-workspace-process-id"; // Header name for the workspace's PID in the request headers.
+/** Header used to tell the sidecar which connection ID to use in the request, if applicable. */
+export const SIDECAR_CONNECTION_ID_HEADER: string = "x-connection-id";
 
+/** Header used to specify a given (Kafka, Schema Registry, etc) cluster ID, if applicable */
+export const CLUSTER_ID_HEADER: string = "x-cluster-id";
+
+/** Enable the middleware that emits debug logs for every request to / response from the sidecar. */
 export const ENABLE_REQUEST_RESPONSE_LOGGING: boolean =
   process.env.ENABLE_REQUEST_RESPONSE_LOGGING === "true";
-
-// Header name for the sidecar's PID in the response headers.
+/** Header name for the sidecar's PID in the response headers. */
 export const SIDECAR_PROCESS_ID_HEADER = "x-sidecar-pid";
 
-// OS-independent path to the log file for the sidecar process.
+/** OS-independent path to the log file for the sidecar process. */
 export const SIDECAR_LOGFILE_PATH = join(tmpdir(), "vscode-confluent-sidecar.log");
