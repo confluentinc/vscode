93e48014915a73dd2ca0e5900a307b7509b44bdb
Dave Shoup <dave.shoup@gmail.com>
2024-11-15 12:02:12 -0500

    [v0.21.x] adjust Sentry filter behavior and add rollup output source map base (#605)

--- Gulpfile.js
+++ Gulpfile.js
@@ -146,7 +146,13 @@ export function build(done) {
     context: "globalThis",
   };
   /** @type {import("rollup").OutputOptions} */
-  const extOutput = { dir: DESTINATION, format: "cjs", sourcemap: true, exports: "named" };
+  const extOutput = {
+    dir: DESTINATION,
+    format: "cjs",
+    sourcemap: true,
+    sourcemapBaseUrl: `file://${process.cwd()}/${DESTINATION}/`,
+    exports: "named",
+  };
 
   /** @type {import("rollup").RollupOptions} */
   const webInput = {
