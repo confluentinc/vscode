981a2c2a1f0ee2d25c55ca154a0d360e9048a0ad
James Robinson <jrobinson@confluent.io>
2024-11-18 16:59:19 -0500

    Improve comment.

--- src/storage/resourceLoaderInitialization.ts
+++ src/storage/resourceLoaderInitialization.ts
@@ -4,7 +4,8 @@ import { CCloudResourceLoader } from "./ccloudResourceLoader";
 import { LocalResourceLoader } from "./localResourceLoader";
 import { ResourceLoader } from "./resourceLoader";
 
-/** Construct the singleton resource loaders so they may register their event listeners. */
+/** Construct and register the singleton resource loaders so they may register their event listeners and
+ * so that ResourceLoader.getInstance() will find them. */
 export function constructResourceLoaderSingletons(): Disposable[] {
   ResourceLoader.registerInstance(CCLOUD_CONNECTION_ID, CCloudResourceLoader.getInstance());
   ResourceLoader.registerInstance(LOCAL_CONNECTION_ID, LocalResourceLoader.getInstance());
