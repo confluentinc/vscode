3370f593d39fa7d1d7d26fe88650346b12cf97ee
Noel Cothren <ncothren@confluent.io>
2024-11-20 10:46:22 -0700

    delete unused type, comment, console log

--- src/directConnect.ts
+++ src/directConnect.ts
@@ -20,10 +20,9 @@ export const registerDirectConnectionCommand = (context: ExtensionContext) => {
 const directConnectWebviewCache = new WebviewPanelCache();
 
 export function openDirectConnectionForm(): void {
-  console.log("openDirectConnectionForm");
   // Set up the webview, checking for existing form for this topic
   const [directConnectForm, formExists] = directConnectWebviewCache.findOrCreate(
-    { id: randomUUID(), multiple: false, template: connectionFormTemplate },
+    { id: randomUUID(), multiple: false, template: connectionFormTemplate }, // TODO change the UUID handling when we start allowing Edit
     "direct-connect-form",
     `New Connection`,
     ViewColumn.One,
@@ -40,32 +39,14 @@ export function openDirectConnectionForm(): void {
   async function testConnect(body: any): Promise<{ success: boolean; message: string | null }> {
     console.log(body);
     throw new Error("Not implemented");
-    // if (result.success) {
-    //   console.log("success", result);
-    //   // this.updateValue(input.name, input.value);
-    // } else {
-    //   // this.errorMessage(res.message ?? "Unknown error occurred");
-    //   // this.revertValue(input.name); // FIXME
-    //   // input.classList.add("error");
-    //   console.log("error", result);
-    // }
-    // return result;
   }
 
-  /**
-   * on submit
-   * save info in VSCODE Storage/state - name/id (generate it)
-   * send to backend
-   */
   const processMessage = async (...[type, body]: Parameters<MessageSender>) => {
     switch (type) {
-      case "ValidateInput": {
-        return null satisfies MessageResponse<"ValidateInput">;
-      }
       case "TestConnection":
         return (await testConnect(body)) satisfies MessageResponse<"TestConnection">;
       case "Submit":
-        return null satisfies MessageResponse<"Submit">;
+        return { success: true, message: null } satisfies MessageResponse<"Submit">;
     }
   };
   const disposable = handleWebviewMessage(directConnectForm.webview, processMessage);
