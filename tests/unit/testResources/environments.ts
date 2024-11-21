753bfc7a304df150907634c673ddddf07d369924
Dave Shoup <dave.shoup@gmail.com>
2024-11-21 11:14:02 -0500

    updates for legacy `hasClusters` use

--- tests/unit/testResources/environments.ts
+++ tests/unit/testResources/environments.ts
@@ -1,10 +1,13 @@
 import { CCloudEnvironment } from "../../../src/models/environment";
+import { TEST_CCLOUD_KAFKA_CLUSTER } from "./kafkaCluster";
+import { TEST_CCLOUD_SCHEMA_REGISTRY } from "./schemaRegistry";
 
-export const TEST_CCLOUD_ENVIRONMENT = CCloudEnvironment.create({
+export const TEST_CCLOUD_ENVIRONMENT: CCloudEnvironment = CCloudEnvironment.create({
   id: "env-abc123",
   name: "test-environment",
   streamGovernancePackage: "NONE",
-  hasClusters: true,
+  kafkaClusters: [TEST_CCLOUD_KAFKA_CLUSTER],
+  schemaRegistry: TEST_CCLOUD_SCHEMA_REGISTRY,
 });
 
 // not tied to the CCloud Environment specifically, but used by CCloud Kafka clusters and Schema Registry
