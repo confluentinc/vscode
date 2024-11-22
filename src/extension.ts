928fd65141002d98c2afdf8b10f254dfd3ff191a
Dave Shoup <dave.shoup@gmail.com>
2024-11-22 09:32:28 -0500

    initialize direct resource loader(s) during activation

--- src/extension.ts
+++ src/extension.ts
@@ -214,7 +214,7 @@ async function _activateExtension(
 
   const directConnectionManager = DirectConnectionManager.getInstance();
   context.subscriptions.push(...directConnectionManager.disposables);
-  await directConnectionManager.reconcileSidecarConnections();
+  await directConnectionManager.rehydrateConnections();
 
   // XXX: used for testing; do not remove
   return context;
