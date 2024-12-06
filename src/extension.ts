6904b85b4c94eedbc722cd85ac5bdb910e982980
Dave Shoup <dave.shoup@gmail.com>
2024-12-06 17:17:50 -0500

    migrate connection rehydrating to Resources view loading instead of extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -216,7 +216,6 @@ async function _activateExtension(
 
   const directConnectionManager = DirectConnectionManager.getInstance();
   context.subscriptions.push(...directConnectionManager.disposables);
-  await directConnectionManager.rehydrateConnections();
 
   // XXX: used for testing; do not remove
   return context;
