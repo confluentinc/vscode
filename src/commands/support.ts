4fa156ac4687b7fa99b765f993a8bc477297aefc
Dave Shoup <dave.shoup@gmail.com>
2024-12-16 14:45:47 -0500

    clean up import; remove openLogFile command; fix log download commands

--- src/commands/support.ts
+++ src/commands/support.ts
@@ -2,7 +2,8 @@ import archiver from "archiver";
 import { createWriteStream } from "fs";
 import { homedir } from "os";
 import { join } from "path";
-import * as vscode from "vscode";
+
+import { commands, Disposable, env, Uri, window, workspace } from "vscode";
 import { registerCommandWithLogging } from ".";
 import { EXTENSION_ID } from "../constants";
 import { observabilityContext } from "../context/observability";
@@ -11,17 +12,17 @@ import { SIDECAR_LOGFILE_PATH } from "../sidecar/constants";
 
 const logger = new Logger("commands.support");
 
-const FEEDBACK_URI = vscode.Uri.parse("https://www.surveymonkey.com/r/NYVKQD6");
+const FEEDBACK_URI = Uri.parse("https://www.surveymonkey.com/r/NYVKQD6");
 
 function openWalkthroughCommand() {
-  vscode.commands.executeCommand(
+  commands.executeCommand(
     "workbench.action.openWalkthrough",
     "confluentinc.vscode-confluent#confluent-walkthrough",
   );
 }
 
 function feedbackCommand() {
-  vscode.env.openExternal(FEEDBACK_URI);
+  env.openExternal(FEEDBACK_URI);
 }
 
 /**
@@ -44,7 +45,7 @@ ${contextTable}
 `;
   }
 
-  vscode.commands.executeCommand("vscode.openIssueReporter", {
+  commands.executeCommand("vscode.openIssueReporter", {
     extensionId: EXTENSION_ID,
     // issueTitle: "Issue title",
     // issueBody: "Issue body",
@@ -53,86 +54,67 @@ ${contextTable}
 }
 
 function openSettings() {
-  vscode.commands.executeCommand(
-    "workbench.action.openSettings",
-    "@ext:confluentinc.vscode-confluent",
-  );
-}
-
-/** Open the log file for the Confluent output channel in a read-only editor document. */
-async function openLogFile() {
-  // TODO: check panel state first?
-
-  // VS Code doesn't expose a way to open a non-visible output channel in an editor, so we'll just
-  // open it, use a built-in command to show it in an editor, then close the panel
-  await vscode.commands.executeCommand("confluent.showOutputChannel");
-  vscode.commands.executeCommand("workbench.action.openActiveLogOutputFile");
-
-  // TODO: revert to previous panel state?
-  vscode.commands.executeCommand("workbench.action.closePanel");
+  commands.executeCommand("workbench.action.openSettings", "@ext:confluentinc.vscode-confluent");
 }
 
 /**
  * Convenience function to allow the user to download the Confluent output channel in the form of
  * a .log file.
  */
-function downloadLogs() {
+async function downloadLogs() {
+  // prompt where to save the file, using the home directory as the default
   const defaultPath = join(homedir(), "vscode-confluent.log");
-  vscode.window.showSaveDialog({ defaultUri: vscode.Uri.file(defaultPath) }).then((saveUri) => {
-    if (saveUri) {
-      handleLogFileSave(saveUri, vscode.Uri.parse(LOGFILE_PATH), true);
-    }
-  });
+  const saveUri = await window.showSaveDialog({ defaultUri: Uri.file(defaultPath) });
+  if (saveUri) {
+    await handleLogFileSave(saveUri, Uri.parse(LOGFILE_PATH), true);
+  }
 }
 
 /** Convenience function to allow the user to download the sidecar's .log file. */
 async function downloadSidecarLogs() {
+  // prompt where to save the file, using the home directory as the default
   const defaultPath = join(homedir(), "vscode-confluent-sidecar.log");
-  const saveUri: vscode.Uri | undefined = await vscode.window.showSaveDialog({
-    defaultUri: vscode.Uri.file(defaultPath),
+  const saveUri: Uri | undefined = await window.showSaveDialog({
+    defaultUri: Uri.file(defaultPath),
   });
   if (saveUri) {
-    handleLogFileSave(saveUri, vscode.Uri.parse(SIDECAR_LOGFILE_PATH), true);
+    await handleLogFileSave(saveUri, Uri.parse(SIDECAR_LOGFILE_PATH), true);
   }
 }
 
-function handleLogFileSave(
-  fileUri: vscode.Uri | undefined,
-  sourceUri: vscode.Uri,
-  forSidecar: boolean = false,
-) {
-  if (!fileUri) {
+/** Helper function to handle saving a log file to the given URI. */
+async function handleLogFileSave(fileUri: Uri, sourceUri: Uri, forSidecar: boolean = false) {
+  let logData: Uint8Array;
+  try {
+    logData = await workspace.fs.readFile(sourceUri);
+  } catch (error) {
+    if (error instanceof Error) {
+      const errorMsg = `Error reading log file: ${error.message}`;
+      logger.error(errorMsg);
+      window.showErrorMessage(errorMsg);
+    }
     return;
   }
 
-  let didWrite: boolean = false;
-  vscode.workspace.fs.readFile(sourceUri).then((data) => {
-    try {
-      vscode.workspace.fs.writeFile(fileUri, data);
-      didWrite = true;
-    } catch (error) {
-      if (error instanceof Error) {
-        const errorMsg = `Error writing log file: ${error.message}`;
-        logger.error(errorMsg);
-        vscode.window.showErrorMessage(errorMsg);
-      }
+  try {
+    await workspace.fs.writeFile(fileUri, logData);
+  } catch (error) {
+    if (error instanceof Error) {
+      const errorMsg = `Error writing log file: ${error.message}`;
+      logger.error(errorMsg);
+      window.showErrorMessage(errorMsg);
+    }
+    return;
+  }
+
+  const successMsg = `Confluent extension ${forSidecar ? "sidecar " : ""}log file downloaded successfully.`;
+  logger.debug(successMsg);
+  const openButton = "Open File";
+  window.showInformationMessage(successMsg, openButton).then((value) => {
+    if (value === openButton) {
+      window.showTextDocument(fileUri);
     }
   });
-  if (!didWrite) {
-    return;
-  }
-
-  const openButton = "Open File";
-  vscode.window
-    .showInformationMessage(
-      `Confluent extension ${forSidecar ? "sidecar " : ""}log file downloaded successfully.`,
-      openButton,
-    )
-    .then((value) => {
-      if (value === openButton) {
-        vscode.window.showTextDocument(fileUri);
-      }
-    });
 }
 
 /**
@@ -143,8 +125,8 @@ function handleLogFileSave(
  */
 async function downloadSupportZip() {
   const defaultPath = join(homedir(), `vscode-confluent-support-${new Date().toISOString()}.zip`);
-  const saveUri = await vscode.window.showSaveDialog({
-    defaultUri: vscode.Uri.file(defaultPath),
+  const saveUri = await window.showSaveDialog({
+    defaultUri: Uri.file(defaultPath),
   });
   if (!saveUri) {
     return;
@@ -158,7 +140,7 @@ async function downloadSupportZip() {
   // set up event listeners for write errors & close/finalize
   const closeListener = output.on("close", () => {
     const openButton = "Open";
-    vscode.window
+    window
       .showInformationMessage(
         "Confluent extension support .zip downloaded successfully",
         openButton,
@@ -166,21 +148,22 @@ async function downloadSupportZip() {
       .then((value) => {
         if (value === openButton) {
           // show in OS file explorer, don't try to open it in VS Code
-          vscode.commands.executeCommand("revealFileInOS", saveUri);
+          commands.executeCommand("revealFileInOS", saveUri);
         }
       });
     closeListener.destroy();
   });
-  archive.on("error", (err) => {
-    vscode.window.showErrorMessage(`Error creating zip: ${err.message}`);
+  const errorListener = archive.on("error", (err) => {
+    window.showErrorMessage(`Error creating zip: ${err.message}`);
+    errorListener.destroy();
   });
 
   archive.pipe(output);
 
   // add extension+sidecar log files
-  const extensionLog = await vscode.workspace.fs.readFile(vscode.Uri.parse(LOGFILE_PATH));
+  const extensionLog = await workspace.fs.readFile(Uri.parse(LOGFILE_PATH));
   archive.append(Buffer.from(extensionLog), { name: "vscode-confluent.log" });
-  const sidecarLog = await vscode.workspace.fs.readFile(vscode.Uri.parse(SIDECAR_LOGFILE_PATH));
+  const sidecarLog = await workspace.fs.readFile(Uri.parse(SIDECAR_LOGFILE_PATH));
   archive.append(Buffer.from(sidecarLog), { name: "vscode-confluent-sidecar.log" });
 
   // add observability context
@@ -192,13 +175,12 @@ async function downloadSupportZip() {
   await archive.finalize();
 }
 
-export function registerSupportCommands(): vscode.Disposable[] {
+export function registerSupportCommands(): Disposable[] {
   return [
     registerCommandWithLogging(
       "confluent.support.confluent-walkthrough.launch",
       openWalkthroughCommand,
     ),
-    registerCommandWithLogging("confluent.support.openLogFile", openLogFile),
     registerCommandWithLogging("confluent.support.downloadLogs", downloadLogs),
     registerCommandWithLogging("confluent.support.downloadSidecarLogs", downloadSidecarLogs),
     registerCommandWithLogging("confluent.support.downloadSupportZip", downloadSupportZip),
