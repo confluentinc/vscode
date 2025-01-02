72564f10cfc845c0550b1169cad35f69d36df3e7
Dave Shoup <dave.shoup@gmail.com>
2025-01-02 15:47:16 -0500

    adjust test for obs context markdown table content

--- src/context/observability.test.ts
+++ src/context/observability.test.ts
@@ -1,16 +1,41 @@
 import * as assert from "assert";
-import { version } from "ide-sidecar";
+import { version as sidecarVersion } from "ide-sidecar";
+import { Extension, ExtensionContext, extensions } from "vscode";
+import { EXTENSION_ID } from "../constants";
 import { observabilityContext } from "./observability";
 
 describe("ObservabilityContext", () => {
   it("should convert to markdown table correctly", async () => {
+    let extensionVersion = "";
+    let extensionActivated = false;
+
+    // if another test activated the extension, this will be set
+    const extensionInstance: Extension<ExtensionContext> | undefined =
+      extensions.getExtension(EXTENSION_ID);
+    if (extensionInstance) {
+      extensionActivated = extensionInstance.isActive;
+      if (extensionActivated) {
+        extensionVersion = extensionInstance.packageJSON.version;
+      }
+    }
+
     const table = observabilityContext.toMarkdownTable();
+
     // only check the first few rows of the table since the rest will be adjusted as needed
     const expectedTableHead = `| Key | Value |
-| --- | --- |
-| extensionVersion | "" |
-| extensionActivated | false |
-| sidecarVersion | "${version}" |`;
-    assert.ok(table.startsWith(expectedTableHead));
+| --- | --- |`;
+    assert.ok(table.startsWith(expectedTableHead), `Wrong markdown table head, got:\n${table}`);
+    assert.ok(
+      table.includes(`| extensionVersion | "${extensionVersion}"`),
+      `Wrong extensionVersion line, got:\n${table}\n\nExpected ${extensionVersion}`,
+    );
+    assert.ok(
+      table.includes(`| extensionActivated | ${extensionActivated}`),
+      `Wrong extensionActivated line, got:\n${table}\n\nExpected ${extensionActivated}`,
+    );
+    assert.ok(
+      table.includes(`| sidecarVersion | "${sidecarVersion}" |`),
+      `Wrong sidecarVersion line, got:\n${table}\n\nExpected ${sidecarVersion}`,
+    );
   });
 });
