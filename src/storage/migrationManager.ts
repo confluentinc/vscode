798967f6c954bff2ef4649a9f466be1dcbf71f3b
Dave Shoup <dave.shoup@gmail.com>
2025-01-06 16:09:30 -0500

    move helper functions and add tests for migration version behavior

--- src/storage/migrationManager.ts
+++ src/storage/migrationManager.ts
@@ -1,15 +1,14 @@
 import { StorageManager } from ".";
 import { Logger } from "../logging";
-import { DURABLE_STORAGE_VERSION_KEY, MigrationStorageType } from "./constants";
-import { BaseMigration } from "./migrations/base";
-import { migrations, migrationVersions } from "./migrations/registry";
+import { DURABLE_STORAGE_VERSION_KEY } from "./constants";
+import { executeMigrations } from "./migrations/utils";
 
 // The current storage version for the extension, set in global state, workspace state, and
 // SecretStorage after migrations are run during extension activation.
 // ---
 // This should be incremented whenever a breaking change is made to the global/workspace state or
 // SecretStorage data structures.
-const CODEBASE_STORAGE_VERSION = 2;
+export const CODEBASE_STORAGE_VERSION = 2;
 
 const logger = new Logger("storage.migrationManager");
 
@@ -27,7 +26,7 @@ export async function migrateStorageIfNeeded(manager: StorageManager): Promise<v
 }
 
 /** Check the storage version in **global state**, then migrate if necessary. */
-async function migrateGlobalState(manager: StorageManager): Promise<void> {
+export async function migrateGlobalState(manager: StorageManager): Promise<void> {
   const globalStorageVersion = await manager.getGlobalState<number>(DURABLE_STORAGE_VERSION_KEY);
   if (globalStorageVersion !== CODEBASE_STORAGE_VERSION) {
     logger.debug(
@@ -41,7 +40,7 @@ async function migrateGlobalState(manager: StorageManager): Promise<void> {
 }
 
 /** Check the storage version in **workspace state**, then migrate if necessary. */
-async function migrateWorkspaceState(manager: StorageManager): Promise<void> {
+export async function migrateWorkspaceState(manager: StorageManager): Promise<void> {
   const workspaceStorageVersion = await manager.getWorkspaceState<number>(
     DURABLE_STORAGE_VERSION_KEY,
   );
@@ -57,11 +56,11 @@ async function migrateWorkspaceState(manager: StorageManager): Promise<void> {
 }
 
 /** Check the storage version in **secret storage**, then migrate if necessary. */
-async function migrateSecretStorage(manager: StorageManager): Promise<void> {
+export async function migrateSecretStorage(manager: StorageManager): Promise<void> {
   const secretStorageVersion: string | undefined = await manager.getSecret(
     DURABLE_STORAGE_VERSION_KEY,
   );
-  if (secretStorageVersion !== String(CODEBASE_STORAGE_VERSION)) {
+  if (String(secretStorageVersion) !== String(CODEBASE_STORAGE_VERSION)) {
     logger.debug(
       `secret storage version is incorrect: "${secretStorageVersion}", should be "${CODEBASE_STORAGE_VERSION}"`,
     );
@@ -69,43 +68,5 @@ async function migrateSecretStorage(manager: StorageManager): Promise<void> {
     manager.setSecret(DURABLE_STORAGE_VERSION_KEY, String(CODEBASE_STORAGE_VERSION));
   } else {
     logger.debug(`secret storage version is correct: "${secretStorageVersion}"`);
-  }
-}
-
-/**
- * Determine the migration versions/steps required to move between two storage versions, which may
- * be multiple versions apart.
- */
-function getMigrationVersions(
-  sourceVersion: number | undefined,
-  targetVersion: number,
-): {
-  versions: number[];
-  isUpgrade: boolean;
-} {
-  // either the source version or the lowest migration version (2)
-  const current: number = sourceVersion ?? Math.min(...migrationVersions);
-  const isUpgrade = current < targetVersion;
-  const versions: number[] = isUpgrade
-    ? migrationVersions.filter((v) => v > current && v <= targetVersion)
-    : migrationVersions.filter((v) => v <= current && v > targetVersion).reverse();
-
-  return { versions, isUpgrade };
-}
-
-/** Execute the necessary migrations to move between two storage versions. */
-async function executeMigrations(
-  sourceVersion: number | undefined,
-  targetVersion: number,
-  storageType: MigrationStorageType,
-): Promise<void> {
-  const { versions, isUpgrade } = getMigrationVersions(sourceVersion, targetVersion);
-
-  for (const version of versions) {
-    const migration: BaseMigration = migrations[version];
-    if (!migration) {
-      continue;
-    }
-    await migration.run(isUpgrade, storageType);
   }
 }
