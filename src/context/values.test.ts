4bef099af68675bb28f7ee67be11c38e8cc6cf03
James Robinson <jrobinson@confluent.io>
2025-01-07 14:29:12 -0500

    Resurrect test.

--- src/context/values.test.ts
+++ src/context/values.test.ts
@@ -1,7 +1,31 @@
 import * as assert from "assert";
+import sinon from "sinon";
+import { commands } from "vscode";
 import { ContextValues, getContextValue, setContextValue } from "./values";
 
 describe("ContextValue functions", () => {
+  let sandbox: sinon.SinonSandbox;
+  let executeCommandStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    executeCommandStub = sandbox.stub(commands, "executeCommand");
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("setContextValue() should correctly context value and execute the setContext command", async () => {
+    const key = ContextValues.resourceSelectedForCompare;
+    const value = true;
+
+    await setContextValue(key, value);
+
+    assert.ok(executeCommandStub.calledWith("setContext", key, value));
+    assert.deepStrictEqual(getContextValue<boolean>(key), value);
+  });
+
   it("setContextValue() should throw an error for an untracked context value", async () => {
     await assert.rejects(
       () => setContextValue("invalidKey" as ContextValues, "value"),
