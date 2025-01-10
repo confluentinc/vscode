03ab9481a1ea8882b29cd241de198b150a610d5e
James Robinson <jrobinson@confluent.io>
2025-01-09 22:07:15 -0500

    Revert stray comment.

--- src/models/resource.ts
+++ src/models/resource.ts
@@ -3,7 +3,6 @@ import { IconNames } from "../constants";
 
 /** A uniquely-branded string-type for a connection ID. */
 export type ConnectionId = string & { readonly brand: unique symbol };
-
 // TODO: use other branded resource ID types here
 
 export interface IResourceBase {
