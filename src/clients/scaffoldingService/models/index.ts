698f2586630cec406feb810bd4b49a7e4516eec3
Dave Shoup <dave.shoup@gmail.com>
2025-01-14 18:46:59 -0500

    migrate from the sidecar templates API to the CCloud scaffolding service (#914)

---
+++ src/clients/scaffoldingService/models/index.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./ApplyScaffoldV1TemplateRequest";
+export * from "./ErrorSource";
+export * from "./Failure";
+export * from "./GetScaffoldV1Template200Response";
+export * from "./GetScaffoldV1TemplateCollection200Response";
+export * from "./GlobalObjectReference";
+export * from "./ListMeta";
+export * from "./ModelError";
+export * from "./ObjectMeta";
+export * from "./ObjectReference";
+export * from "./ScaffoldV1Template";
+export * from "./ScaffoldV1TemplateCollection";
+export * from "./ScaffoldV1TemplateCollectionList";
+export * from "./ScaffoldV1TemplateCollectionListDataInner";
+export * from "./ScaffoldV1TemplateCollectionListMetadata";
+export * from "./ScaffoldV1TemplateCollectionMetadata";
+export * from "./ScaffoldV1TemplateCollectionSpec";
+export * from "./ScaffoldV1TemplateList";
+export * from "./ScaffoldV1TemplateListDataInner";
+export * from "./ScaffoldV1TemplateListMetadata";
+export * from "./ScaffoldV1TemplateMetadata";
+export * from "./ScaffoldV1TemplateOption";
+export * from "./ScaffoldV1TemplateSpec";
