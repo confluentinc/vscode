a5a9c62544f62f4da6945902a40b94285c362ce5
Dave Shoup <dave.shoup@gmail.com>
2025-01-24 17:55:19 -0500

    update comment

--- src/schemas/diagnosticCollection.ts
+++ src/schemas/diagnosticCollection.ts
@@ -3,6 +3,6 @@ import { DiagnosticCollection, languages } from "vscode";
 /** The name of the diagnostic collection for JSON validation. */
 const JSON_DIAGNOSTIC_COLLECTION_NAME = "confluent.json";
 
-/** The main diagnostic collection used for any JSON validation done by this extension. */
+/** The main diagnostic collection used for any JSON validation performed by this extension. */
 export const JSON_DIAGNOSTIC_COLLECTION: DiagnosticCollection =
   languages.createDiagnosticCollection(JSON_DIAGNOSTIC_COLLECTION_NAME);
