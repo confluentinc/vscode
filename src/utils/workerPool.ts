b729b42b7de7bb381140f8ccaf3fcead6501539d
Dave Shoup <dave.shoup@gmail.com>
2025-01-29 17:45:10 -0500

    cleanup + enforced ordering + tests

--- src/utils/workerPool.ts
+++ src/utils/workerPool.ts
@@ -23,13 +23,15 @@ export interface ErrorResult extends ExecutionResult<unknown> {
 }
 
 /** Type guard to check if an {@link ExecutionResult} is a {@link SuccessResult}. */
-export function isSuccessResult<R>(result: ExecutionResult<R>): result is SuccessResult<R> {
-  return result.result !== undefined;
+export function isSuccessResult<R>(
+  result: ExecutionResult<R> | undefined,
+): result is SuccessResult<R> {
+  return result?.result !== undefined;
 }
 
 /** Type guard to check if an {@link ExecutionResult} is an {@link ErrorResult}. */
-export function isErrorResult(result: ExecutionResult<unknown>): result is ErrorResult {
-  return result.error !== undefined;
+export function isErrorResult(result: ExecutionResult<unknown> | undefined): result is ErrorResult {
+  return result?.error !== undefined;
 }
 
 interface WorkerPoolOptions {
@@ -38,7 +40,10 @@ interface WorkerPoolOptions {
 }
 
 /**
- * Execute a collection of items in parallel using a worker pool pattern.
+ * Execute a callback against an array of (non-`undefined`) items using a worker pool pattern.
+ * Results will be returned in the same order as the input items, with each result containing either
+ * a result or an error.
+ *
  * @param items Array of items to process
  * @param callable Async function to execute for each item
  * @param options Worker pool options
@@ -49,16 +54,22 @@ interface WorkerPoolOptions {
 export async function executeInWorkerPool<T, R>(
   items: T[],
   callable: (item: T) => Promise<R>,
-  options: WorkerPoolOptions = { maxWorkers: 1 },
+  options: WorkerPoolOptions = { maxWorkers: 2 },
   progress?: Progress<{
     message?: string;
     increment?: number;
   }>,
   token?: CancellationToken,
 ): Promise<ExecutionResult<R>[]> {
-  // the workers themselves don't return anything; leave that to the inner function to push to results
-  const workers: Promise<void>[] = [];
-  const results: ExecutionResult<R>[] = [];
+  // maxWorkers must be at least 2 and no more than 100
+  options.maxWorkers = Math.max(2, Math.min(options.maxWorkers, 100));
+  logger.debug("worker pool execution started", {
+    taskName: options.taskName,
+    maxWorkers: options.maxWorkers,
+    itemCount: items.length,
+  });
+  // `undefined` placeholders for results to preserve ordering
+  const results: ExecutionResult<R>[] = new Array(items.length).fill(undefined);
 
   const totalCount = items.length;
   const progressTick = { increment: 100 / totalCount };
@@ -68,22 +79,27 @@ export async function executeInWorkerPool<T, R>(
 
   async function worker(): Promise<void> {
     while (currentIndex < totalCount && !token?.isCancellationRequested) {
+      // store the current index in a separate variable so we can use it to set the result/error and
+      // preserve ordering of the original items
+      const taskIndex = currentIndex;
       const item: T | undefined = items[currentIndex++];
       if (!item) {
-        // no more items to process
-        return;
+        // no support for passing `undefined` items into the callback
+        continue;
       }
 
       try {
         const result: R = await callable(item);
-        // XXX: only enable for local debugging because this can get very noisy
-        // logger.debug("worker pool task finished", {
+        // XXX: only enable for local debugging/testing because this can get very noisy
+        // logger.info("worker pool task finished", {
+        //   result,
         //   currentIndex,
         //   totalCount,
         //   taskName: options.taskName,
         // });
         resultCount++;
-        results.push({ result });
+        // don't .push() directly since we want to preserve the order of the original items
+        results[taskIndex] = { result };
       } catch (error) {
         errorCount++;
         logError(error, "workerPool", {
@@ -93,22 +109,25 @@ export async function executeInWorkerPool<T, R>(
           totalCount: totalCount.toString(),
         });
         if (error instanceof Error) {
-          results.push({ error });
+          results[taskIndex] = { error };
         }
+        // no re-throwing here; let the callers handle that if needed
       }
 
       if (progress) {
         // update any attached progress reporters (e.g. vscode.window.withProgress)
         progress.report(progressTick);
-        // TODO: include configurable `message`?
       }
     }
   }
 
+  // set up the initial workers; no less than 2 and no more than the total number of items provided
+  // (the workers themselves don't return anything; leave that to the inner function to set results)
+  const workers: Promise<void>[] = [];
   for (let i = 0; i < Math.min(options.maxWorkers, totalCount); i++) {
     workers.push(worker());
   }
-
+  // wait for all workers to finish their task loops
   await Promise.all(workers);
 
   if (token?.isCancellationRequested) {
