4a9480be2371f71bd311e269088619fdfd2132d6
Dave Shoup <dave.shoup@gmail.com>
2025-01-31 18:32:09 -0500

    delegate "container-search" to data class methods

--- src/viewProviders/filtering.ts
+++ src/viewProviders/filtering.ts
@@ -1,5 +1,4 @@
 import { Environment } from "../models/environment";
-import { KafkaCluster } from "../models/kafkaCluster";
 import { ContainerTreeItem } from "../models/main";
 import { ISearchable } from "../models/resource";
 
@@ -14,63 +13,21 @@ export function filterSearchableItems(items: ISearchable[], searchStr: string): 
   const filteredItems: ISearchable[] = [];
 
   items.forEach((item) => {
-    const itemMatches = item.searchableText().toLowerCase().includes(searchStr);
-
-    if (item instanceof Environment) {
-      if (itemMatches) {
-        // something about the environment matched on its own
-        filteredItems.push(item);
-      } else {
-        // include any nested Kafka Clusters or Schema Registries in search filtering
-        const kafkaMatches = item.kafkaClusters.some((kafkaCluster) =>
-          kafkaCluster.searchableText().toLowerCase().includes(searchStr),
-        );
-        if (kafkaMatches) {
-          item.kafkaClusters = filterSearchableItems(
-            item.kafkaClusters,
-            searchStr,
-          ) as KafkaCluster[];
-        } else {
-          // remove all Kafka Clusters if none match
-          item.kafkaClusters = [];
-        }
-
-        const schemaRegistryMatches = item.schemaRegistry
-          ?.searchableText()
-          .toLowerCase()
-          .includes(searchStr);
-        if (!schemaRegistryMatches) {
-          // remove it if it doesn't match
-          item.schemaRegistry = undefined;
-        }
-
-        if (kafkaMatches || schemaRegistryMatches) {
-          filteredItems.push(item);
-        }
-      }
-      return;
-    }
-
-    if (item instanceof ContainerTreeItem) {
-      if (itemMatches) {
-        // something about the container matched on its own
-        filteredItems.push(item);
-      } else {
-        // recursively filter children (e.g. "Confluent Cloud" > Environments > Kafka Clusters)
-        const childrenMatches = filterSearchableItems(item.children, searchStr);
-        if (childrenMatches.length > 0) {
-          // don't return all children, just the ones that matched
-          item.children = childrenMatches;
-          filteredItems.push(item);
-        }
+    // check any container-like items for nested matches since they may need to also filter their
+    // children (which calls back into this function)
+    if (item instanceof Environment || item instanceof ContainerTreeItem) {
+      const containerLike: (Environment | ContainerTreeItem<any>) | undefined =
+        item.searchContainer(searchStr);
+      if (containerLike) {
+        filteredItems.push(containerLike);
       }
       return;
     }
 
     // TODO: add Topic/Schema match logic here
 
-    // non-parent type, no need to check nested items
-    if (itemMatches) {
+    // not a "container-like" item, so just check its own searchableText
+    if (item.searchableText().toLowerCase().includes(searchStr)) {
       filteredItems.push(item);
     }
   });
