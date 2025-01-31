a0b8891b9f30afdaa5b4a3c5b31b7dd6e4a9bbc0
Dave Shoup <dave.shoup@gmail.com>
2025-01-31 15:02:08 -0500

    return partial items if a subset of children match

--- src/viewProviders/filtering.ts
+++ src/viewProviders/filtering.ts
@@ -1,4 +1,5 @@
 import { Environment } from "../models/environment";
+import { KafkaCluster } from "../models/kafkaCluster";
 import { ContainerTreeItem } from "../models/main";
 import { ISearchable } from "../models/resource";
 
@@ -16,25 +17,52 @@ export function filterSearchableItems(items: ISearchable[], searchStr: string): 
     const itemMatches = item.searchableText().toLowerCase().includes(searchStr);
 
     if (item instanceof Environment) {
-      // include any nested Kafka Clusters or Schema Registries in search filtering
-      const kafkaMatches = item.kafkaClusters.some((kafkaCluster) =>
-        kafkaCluster.searchableText().toLowerCase().includes(searchStr),
-      );
-      const schemaRegistryMatches = item.schemaRegistry
-        ?.searchableText()
-        .toLowerCase()
-        .includes(searchStr);
-      if (itemMatches || kafkaMatches || schemaRegistryMatches) {
+      if (itemMatches) {
+        // something about the environment matched on its own
         filteredItems.push(item);
+      } else {
+        // include any nested Kafka Clusters or Schema Registries in search filtering
+        const kafkaMatches = item.kafkaClusters.some((kafkaCluster) =>
+          kafkaCluster.searchableText().toLowerCase().includes(searchStr),
+        );
+        if (kafkaMatches) {
+          item.kafkaClusters = filterSearchableItems(
+            item.kafkaClusters,
+            searchStr,
+          ) as KafkaCluster[];
+        } else {
+          // remove all Kafka Clusters if none match
+          item.kafkaClusters = [];
+        }
+
+        const schemaRegistryMatches = item.schemaRegistry
+          ?.searchableText()
+          .toLowerCase()
+          .includes(searchStr);
+        if (!schemaRegistryMatches) {
+          // remove it if it doesn't match
+          item.schemaRegistry = undefined;
+        }
+
+        if (kafkaMatches || schemaRegistryMatches) {
+          filteredItems.push(item);
+        }
       }
       return;
     }
 
     if (item instanceof ContainerTreeItem) {
-      // recursively filter children (e.g. "Confluent Cloud" > Environments > Kafka Clusters)
-      const childrenMatches = filterSearchableItems(item.children, searchStr);
-      if (itemMatches || childrenMatches.length > 0) {
+      if (itemMatches) {
+        // something about the container matched on its own
         filteredItems.push(item);
+      } else {
+        // recursively filter children (e.g. "Confluent Cloud" > Environments > Kafka Clusters)
+        const childrenMatches = filterSearchableItems(item.children, searchStr);
+        if (childrenMatches.length > 0) {
+          // don't return all children, just the ones that matched
+          item.children = childrenMatches;
+          filteredItems.push(item);
+        }
       }
       return;
     }
