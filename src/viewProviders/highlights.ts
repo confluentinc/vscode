fb9d5646fb3af1dea49f28d5e6feaaff53013f3c
Dave Shoup <dave.shoup@gmail.com>
2025-01-31 10:04:13 -0500

    support multi-matches; add tests

--- src/viewProviders/highlights.ts
+++ src/viewProviders/highlights.ts
@@ -1,11 +1,19 @@
-/** Given a label and a substring, return a range of indices to highlight the label. */
+/** Returns a range of indices to provide the `highlights` for a `TreeViewLabel` given a label. */
 export function createHighlightRanges(label: string, substring: string): [number, number][] {
   if (!label || !substring) {
     return [];
   }
-  const searchIndex = label.toLowerCase().indexOf(substring.toLowerCase());
-  if (searchIndex >= 0) {
-    return [[searchIndex, searchIndex + substring.length]];
+
+  const matches: [number, number][] = [];
+  // case-insensitive search
+  const lowerLabel = label.toLowerCase();
+  const lowerSubstring = substring.toLowerCase();
+  let searchIndex = 0;
+  // find all occurrences of substring in label, not just the first
+  while ((searchIndex = lowerLabel.indexOf(lowerSubstring, searchIndex)) >= 0) {
+    matches.push([searchIndex, searchIndex + substring.length]);
+    // move search position past current match before looking for next match
+    searchIndex += substring.length;
   }
-  return [];
+  return matches;
 }
