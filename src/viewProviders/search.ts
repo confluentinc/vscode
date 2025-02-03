883eab3ac53a648a36da2e18f7231d8f23a70172
Dave Shoup <dave.shoup@gmail.com>
2025-02-03 15:01:11 -0500

    refactor recursive matching into general recursive callback function; add file decoration provider

--- src/viewProviders/search.ts
+++ src/viewProviders/search.ts
@@ -1,3 +1,4 @@
+import { FileDecoration, FileDecorationProvider, ThemeColor, Uri } from "vscode";
 import { ISearchable, isSearchable } from "../models/resource";
 
 /** Check if an item matches the provided search string. */
@@ -18,17 +19,54 @@ export function filterItems(items: ISearchable[], searchStr: string): ISearchabl
   return items.filter((item) => matchesOrHasMatchingChild(item, searchStr));
 }
 
+/** Traverse tree nodes, calling the provided callback function for each matching element. */
+export function traverseMatches(
+  item: ISearchable,
+  searchStr: string,
+  callback: (item: ISearchable) => void,
+): void {
+  if (itemMatchesSearch(item, searchStr)) {
+    callback(item);
+  }
+  item.children?.forEach((child) => traverseMatches(child, searchStr, callback));
+}
+
 /** Determine whether an item directly matches the search string, or if its children do. */
 export function matchesOrHasMatchingChild(item: ISearchable, searchStr: string): boolean {
-  // check if the item is a direct match
-  if (itemMatchesSearch(item, searchStr)) {
-    return true;
-  }
-  // recursively check if any child matches
-  if (item.children?.length) {
-    return item.children.some((child) =>
-      matchesOrHasMatchingChild(child as ISearchable, searchStr),
-    );
-  }
-  return false;
+  if (!searchStr) return true;
+  let hasMatch = false;
+  traverseMatches(item, searchStr, () => {
+    hasMatch = true;
+  });
+  return hasMatch;
 }
+
+/** Count total number of elements matching the search string */
+export function countMatchingElements(item: ISearchable, searchStr: string): number {
+  if (!searchStr) return 0;
+  let count = 0;
+  traverseMatches(item, searchStr, () => {
+    count++;
+  });
+  return count;
+}
+
+/** Uri scheme to use when a view's tree item can be decorated to look like a search result. */
+export const SEARCH_DECORATION_URI_SCHEME = "search-match";
+/**
+ * File decoration provider that adds a visual indicator to search results for {@link Uri}s using
+ * the {@link SEARCH_DECORATION_URI_SCHEME}.
+ */
+export const SEARCH_DECORATION_PROVIDER: FileDecorationProvider = {
+  provideFileDecoration: (uri: Uri): FileDecoration | undefined => {
+    if (uri.scheme === SEARCH_DECORATION_URI_SCHEME) {
+      const decoration = new FileDecoration(
+        "●",
+        "Matches search",
+        new ThemeColor("list.highlightForeground"),
+      );
+      decoration.propagate = true;
+      return decoration;
+    }
+  },
+};
