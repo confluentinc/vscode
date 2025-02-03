4772eaa75d6ab640d5748b2aaefd7931e7f13d44
Dave Shoup <dave.shoup@gmail.com>
2025-02-03 17:49:01 -0500

    set KafkaTopicTreeItem `id`

--- src/models/topic.ts
+++ src/models/topic.ts
@@ -59,6 +59,7 @@ export class KafkaTopicTreeItem extends vscode.TreeItem {
     super(resource.name);
 
     // internal properties
+    this.id = resource.uniqueId;
     this.resource = resource;
     this.contextValue = `${this.resource.connectionType.toLowerCase()}-kafka-topic`;
     if (this.resource.hasSchema) {
