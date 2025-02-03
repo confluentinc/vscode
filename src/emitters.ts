e0492c40134a32edbb3b1cffe169b15d2fcc663c
Dave Shoup <dave.shoup@gmail.com>
2025-02-03 09:02:16 -0500

    push these into follow-on branches

--- src/emitters.ts
+++ src/emitters.ts
@@ -42,7 +42,3 @@ export const connectionStable = new vscode.EventEmitter<ConnectionId>();
 
 /** The user set/unset a filter for the Resources view. */
 export const resourceSearchSet = new vscode.EventEmitter<string | null>();
-/** The user set/unset a filter for the Topics view. */
-export const topicSearchSet = new vscode.EventEmitter<string | null>();
-/** The user set/unset a filter for the Schemas view. */
-export const schemaSearchSet = new vscode.EventEmitter<string | null>();
