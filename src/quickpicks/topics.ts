5ecfb3138ac52adb98d17109c0c0f9102cc6d7eb
James Robinson <jrobinson@confluent.io>
2025-02-03 16:13:16 -0500

    Simplify. No need to call getSubjects() at all, because loader.getTopicsForCluster() will have done this already.

--- src/quickpicks/topics.ts
+++ src/quickpicks/topics.ts
@@ -1,11 +1,8 @@
 import { ProgressOptions, ThemeColor, ThemeIcon, window } from "vscode";
 import { IconNames } from "../constants";
 import { ResourceLoader } from "../loaders";
-import { Logger } from "../logging";
 import { KafkaCluster } from "../models/kafkaCluster";
 import { KafkaTopic } from "../models/topic";
-
-const logger = new Logger("quickpicks.topics");
 
 export async function topicQuickPick(
   cluster: KafkaCluster,
@@ -18,27 +15,12 @@ export async function topicQuickPick(
   return window.withProgress(options, async () => {
     const loader = ResourceLoader.getInstance(cluster.connectionId);
 
-    const [subjects, topics]: [string[], KafkaTopic[]] = await Promise.all([
-      loader.getSubjects(cluster.environmentId!, forceRefresh),
-      loader.getTopicsForCluster(cluster, forceRefresh),
-    ]);
-
-    logger.debug(
-      `Loaded ${subjects.length} subjects and ${topics.length} topics for cluster ${cluster.name}`,
-    );
-
-    // Poor person's subject -> topic name mapping, matching only via TopicNameStrategy.
-    const subjectSet = new Set(subjects.map((subject) => subject.replace(/-key$|-value$/, "")));
+    const topics: KafkaTopic[] = await loader.getTopicsForCluster(cluster, forceRefresh);
 
     const choices = topics.map((topic) => {
-      const hasSchema = subjectSet.has(topic.name);
-      if (hasSchema) {
-        // Found a schema for this topic, remove it from the set to speed up future lookups
-        subjectSet.delete(topic.name);
-      }
       return {
         label: topic.name,
-        iconPath: hasSchema
+        iconPath: topic.hasSchema
           ? new ThemeIcon(IconNames.TOPIC)
           : new ThemeIcon(
               IconNames.TOPIC_WITHOUT_SCHEMA,
