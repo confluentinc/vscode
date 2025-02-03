8c86cc8caceacaed8386c972b8222a9540f55dfc
James Robinson <james@relatable.dev>
2025-02-03 15:55:38 -0500

    Wean schema subject quickpick off of getSubjects() / `GET /schemas` route (#1005)
    * Quickpick for schema registry subject names (i.e. when uploading a new schema) now based off of `GET /subjects` route results.
    * No more need for schemaSubjectQuickPick() defaultSubject argument.

--- src/loaders/loaderUtils.test.ts
+++ src/loaders/loaderUtils.test.ts
@@ -1,8 +1,13 @@
 import assert from "assert";
-import { TEST_LOCAL_KAFKA_CLUSTER } from "../../tests/unit/testResources";
+import * as sinon from "sinon";
+import {
+  TEST_LOCAL_KAFKA_CLUSTER,
+  TEST_LOCAL_SCHEMA_REGISTRY,
+} from "../../tests/unit/testResources";
 import { createTestTopicData } from "../../tests/unit/testUtils";
 import { TopicData } from "../clients/kafkaRest/models";
 import * as loaderUtils from "../loaders/loaderUtils";
+import * as sidecar from "../sidecar";
 
 // as from fetchTopics() result.
 export const topicsResponseData: TopicData[] = [
@@ -12,7 +17,7 @@ export const topicsResponseData: TopicData[] = [
   createTestTopicData(TEST_LOCAL_KAFKA_CLUSTER.id, "topic4", ["READ", "WRITE"]),
 ];
 
-describe("correlateTopicsWithSchemaSubjects() test", () => {
+describe("loaderUtils correlateTopicsWithSchemaSubjects() test", () => {
   it("should correlate topics with schema subjects as strings", () => {
     // topic 1-3 will be correlated with schema subjects, topic 4 will not.
     const subjects: string[] = ["topic1-value", "topic2-key", "topic3-Foo"];
@@ -27,5 +32,45 @@ describe("correlateTopicsWithSchemaSubjects() test", () => {
     assert.ok(results[1].hasSchema);
     assert.ok(results[2].hasSchema);
     assert.ok(!results[3].hasSchema);
+  });
+});
+
+describe("loaderUtils fetchSubjects() tests", () => {
+  let sandbox: sinon.SinonSandbox;
+
+  let listSubjectsStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+
+    listSubjectsStub = sandbox.stub();
+
+    const mockSubjectsV1Api = {
+      list: listSubjectsStub,
+    };
+
+    let getSidecarStub: sinon.SinonStub;
+    getSidecarStub = sandbox.stub(sidecar, "getSidecar");
+
+    const mockHandle = {
+      getSubjectsV1Api: () => {
+        return mockSubjectsV1Api;
+      },
+    };
+    getSidecarStub.resolves(mockHandle);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("should return subjects sorted", async () => {
+    const subjectsRaw = ["subject2", "subject3", "subject1"];
+    listSubjectsStub.resolves(subjectsRaw);
+
+    const subjects = await loaderUtils.fetchSubjects(TEST_LOCAL_SCHEMA_REGISTRY);
+
+    // be sure to test against a wholly separate array, 'cause .sort() is in-place.
+    assert.deepStrictEqual(subjects, ["subject1", "subject2", "subject3"]);
   });
 });
