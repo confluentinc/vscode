873ba773413fdf8ddc103938dd50833f97a579b2
Dave Shoup <dave.shoup@gmail.com>
2025-02-21 10:40:52 -0500

    remove event processor

--- src/telemetry/eventProcessors.ts
+++ src/telemetry/eventProcessors.ts
@@ -1,7 +1,6 @@
 import * as Sentry from "@sentry/node";
 import { env, workspace } from "vscode";
 import { observabilityContext } from "../context/observability";
-import { extractErrorInformation } from "../errors";
 
 /** Helper function to make sure the user has Telemetry ON before sending Sentry error events */
 export function checkTelemetrySettings(event: Sentry.Event) {
@@ -17,13 +16,4 @@ export function checkTelemetrySettings(event: Sentry.Event) {
 export function includeObservabilityContext(event: Sentry.Event): Sentry.Event {
   event.extra = { ...event.extra, ...observabilityContext.toRecord() };
   return event;
-}
-
-export async function handleError(event: Sentry.Event): Sentry.Event | null {
-  if (event.exception) {
-    const errorInfo = await extractErrorInformation(event.exception);
-    if (errorInfo) {
-      event.exception = errorInfo.e;
-    }
-  }
 }
