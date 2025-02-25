7812448d69c880a8dae85af8d70db4aa15fcca2f
James Robinson <james@relatable.dev>
2025-02-25 11:52:35 -0500

    Additional model tests as inidicated by coverage report. (#1076)

---
+++ src/models/kafkaCluster.test.ts
@@ -0,0 +1,36 @@
+import * as assert from "assert";
+import {
+  TEST_CCLOUD_KAFKA_CLUSTER,
+  TEST_LOCAL_KAFKA_CLUSTER,
+} from "../../tests/unit/testResources/kafkaCluster";
+import { createKafkaClusterTooltip, LocalKafkaCluster } from "./kafkaCluster";
+
+describe("createKafkaClusterTooltip()", () => {
+  it("should return the correct tooltip for a Confluent Cloud Kafka cluster", () => {
+    const tooltipString = createKafkaClusterTooltip(TEST_CCLOUD_KAFKA_CLUSTER).value;
+    assert.ok(tooltipString.includes(`Name: \`${TEST_CCLOUD_KAFKA_CLUSTER.name}\``));
+    assert.ok(tooltipString.includes("Open in Confluent Cloud"));
+    assert.ok(tooltipString.includes("URI:"));
+  });
+
+  it("Should handle nameless cluster (??)", () => {
+    const cluster = LocalKafkaCluster.create({
+      ...TEST_LOCAL_KAFKA_CLUSTER,
+      id: "local-kafka-cluster-abc123",
+      name: "",
+    });
+
+    const tooltipString = createKafkaClusterTooltip(cluster).value;
+    assert.ok(!tooltipString.includes("Name:"));
+  });
+
+  it("Should handle URI-less cluster (??)", () => {
+    const cluster = LocalKafkaCluster.create({
+      ...TEST_LOCAL_KAFKA_CLUSTER,
+      uri: undefined,
+    });
+
+    const tooltipString = createKafkaClusterTooltip(cluster).value;
+    assert.ok(!tooltipString.includes("URI:"));
+  });
+});
