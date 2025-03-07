831f0e2de40d59e76636496a160da82a2ae811c9
Dave Shoup <dave.shoup@gmail.com>
2025-03-07 18:46:27 -0500

    initial client + listener setup

--- src/featureFlags/client.ts
+++ src/featureFlags/client.ts
@@ -1,21 +1,56 @@
-import * as LaunchDarkly from "launchdarkly-node-client-sdk";
+import {
+  LDElectronMainClient,
+  LDFlagValue,
+  LDUser,
+  initializeInMain,
+} from "launchdarkly-electron-client-sdk";
+import { env } from "vscode";
 import { Logger } from "../logging";
-import { FeatureFlag } from "./constants";
+import { handleFlagChanges } from "./changes";
+import { FEATURE_FLAG_DEFAULTS, FeatureFlags } from "./constants";
 
-const logger = new Logger("featureFlags");
+const logger = new Logger("featureFlags.client");
 
-// TODO: fix the context
-let context: LaunchDarkly.LDContext = {
-  kind: "user",
-  key: "user-key-123abc",
+// use the client ID fetched from vault at build time for production releases, otherwise use any
+// local test ID set in .env
+const clientSideId: string | undefined =
+  process.env.NODE_ENV !== "production"
+    ? process.env.TEST_LAUNCHDARKLY_CLIENT_ID
+    : process.env.LAUNCHDARKLY_CLIENT_ID;
+
+/**
+ * Singleton LaunchDarkly client.
+ * @see https://launchdarkly.com/docs/sdk/client-side/node-js#initialize-the-client
+ *
+ * However, we're using the Electron client SDK instead of the Node.js SDK:
+ * @see https://launchdarkly.com/docs/sdk/client-side/electron#why-use-this-instead-of-the-nodejs-sdk
+ */
+let client: LDElectronMainClient | undefined = undefined;
+
+/** Initial user context, only updated during CCloud auth via {@link LDElectronMainClient.identify}. */
+let user: LDUser = {
+  key: `${env.uriScheme}-user`,
+  anonymous: true,
 };
 
-export const FeatureFlagMap: Map<FeatureFlag, any> = new Map();
+/** Returns the LaunchDarkly client. If it fails to initialize, it will log an error and return
+ * undefined and any feature flag lookups will return the local defaults. */
+export function getLaunchDarklyClient(): LDElectronMainClient | undefined {
+  if (!clientSideId) {
+    logger.error("LaunchDarkly client side ID is not set");
+    return;
+  }
+  if (client) {
+    return client;
+  }
 
-export async function getLDClient() {
-  const client = LaunchDarkly.initialize("client-side-id-123abc", context);
+  const options = {
+    streaming: true, // Necessary in order for live flag updating to work
+  };
+
   try {
-    await client.waitForInitialization(5);
+    client = initializeInMain(clientSideId, user, options);
+    setEventListeners(client);
   } catch (e) {
     if (e instanceof Error) {
       logger.error("LaunchDarkly client failed to initialize:", e.message);
@@ -23,22 +58,40 @@ export async function getLDClient() {
     return;
   }
 
-  client.on("initialized", () => {
-    FeatureFlagMap.set(
-      FeatureFlag.GLOBAL_ENABLED,
-      client.variation(FeatureFlag.GLOBAL_ENABLED, true) as boolean,
-    );
-    FeatureFlagMap.set(
-      FeatureFlag.GLOBAL_NOTICES,
-      client.variation(FeatureFlag.GLOBAL_NOTICES, []) as string[],
-    );
-    FeatureFlagMap.set(
-      FeatureFlag.SEGMENT_ENABLE,
-      client.variation(FeatureFlag.SEGMENT_ENABLE, true) as boolean,
-    );
-    FeatureFlagMap.set(
-      FeatureFlag.CCLOUD_ENABLE,
-      client.variation(FeatureFlag.CCLOUD_ENABLE, true) as boolean,
-    );
+  return client;
+}
+
+/**
+ * Sets up the event listeners for the LaunchDarkly client.
+ * @param client The LaunchDarkly client.
+ */
+function setEventListeners(client: LDElectronMainClient): void {
+  client.on("ready", () => {
+    logger.debug("client ready event, setting flags...");
+    // set starting values
+    for (const [key, defaultValue] of Object.entries(FEATURE_FLAG_DEFAULTS)) {
+      const actualValue: LDFlagValue = client.variation(key, defaultValue);
+      FeatureFlags[key] = actualValue;
+    }
+    logger.debug("client ready, flags set:", FeatureFlags);
   });
+
+  client.on("failed", (err) => {
+    logger.error("failed event:", err);
+  });
+
+  client.on("error", (err) => {
+    logger.error("error event:", err);
+  });
+
+  // this is the main one we care about after the client is ready:
+  client.on("change", handleFlagChanges);
+}
+
+/** Sets the {@link FeatureFlags} with default values from {@link FEATURE_FLAG_DEFAULTS}. */
+export function setFlagDefaults(): void {
+  for (const [flag, defaultValue] of Object.entries(FEATURE_FLAG_DEFAULTS)) {
+    FeatureFlags[flag] = defaultValue;
+  }
+  logger.debug("defaults set:", FeatureFlags);
 }
