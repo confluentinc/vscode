a76688dd02ecba1eea5fd888d1f59b8974bb0d63
Dave Shoup <dave.shoup@gmail.com>
2025-03-10 11:39:10 -0400

    cleanup and keep track of rotated names for this extension instance

--- src/logging.ts
+++ src/logging.ts
@@ -2,7 +2,6 @@ import { tmpdir } from "os";
 import { createStream, Generator, RotatingFileStream } from "rotating-file-stream";
 import { LogOutputChannel, window } from "vscode";
 
-export const LOGFILE_NAME = `vscode-confluent-${process.pid}.log`;
 /**
  * Default path to store downloadable log files for this extension instance.
  *
@@ -13,13 +12,15 @@ export const LOGFILE_DIR = tmpdir();
 
 /** Max size of any log file written to disk.
  * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#size */
-const MAX_LOG_FILE_SIZE = "20K"; // 10MB max file size
+const MAX_LOGFILE_SIZE = "15K"; // 10MB max file size
+
 /** Number of log files to keep.
  * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#maxfiles */
-const MAX_LOG_FILES = 3; // only keep 3 log files at a time
-/** How often log files should rotate if they don't exceed {@link MAX_LOG_FILE_SIZE}.
+const MAX_LOGFILES = 3; // only keep 3 log files at a time
+
+/** How often log files should rotate if they don't exceed {@link MAX_LOGFILE_SIZE}.
  * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#interval */
-const LOG_ROTATION_INTERVAL = "1d"; // rotate log files daily
+const LOGFILE_ROTATION_INTERVAL = "1d"; // rotate log files daily
 
 /**
  * Main "Confluent" output channel.
@@ -153,13 +154,14 @@ export class Logger {
 /** Single stream to allow rotating-file-stream to keep track of file sizes and rotation timing. */
 let logFileStream: RotatingFileStream | undefined;
 
+/** Creates a new rotating log file stream if it doesn't already exist. */
 export function getLogFileStream(): RotatingFileStream {
   if (!logFileStream) {
     logFileStream = createStream(rotatingFilenameGenerator, {
-      size: MAX_LOG_FILE_SIZE,
-      maxSize: MAX_LOG_FILE_SIZE,
-      maxFiles: MAX_LOG_FILES,
-      interval: LOG_ROTATION_INTERVAL,
+      size: MAX_LOGFILE_SIZE,
+      maxSize: MAX_LOGFILE_SIZE,
+      maxFiles: MAX_LOGFILES,
+      interval: LOGFILE_ROTATION_INTERVAL,
       path: LOGFILE_DIR,
       encoding: "utf-8",
     });
@@ -167,20 +169,40 @@ export function getLogFileStream(): RotatingFileStream {
   return logFileStream;
 }
 
-function pad(num: number) {
+/**
+ * Pads a number with leading zero if it's less than 10.
+ * @param num - The number to pad.
+ * @returns The padded number as a string.
+ */
+function pad(num: number): string {
   return (num > 9 ? "" : "0") + num;
 }
 
+/** Set of log file names that have already been created for this extension instance. */
+const ROTATED_LOGFILE_NAMES = new Set<string>();
+
+/** The name of the currently active log file, including time/index prefixing. */
+export let CURRENT_LOGFILE_NAME: string;
+
+/** The name of the log file, without any time/index prefixing. */
+const LOGFILE_NAME = `vscode-confluent-${process.pid}.log`;
+
+/**
+ * Generates a rotating filename based on the current date and time, with an optional index.
+ * The format is `YYYYMMDD-HHMM-index-logfile.log`.
+ * @param time - The date/time to use for the filename. If not provided, defaults to the current date/time.
+ * @param index - An optional index to append to the filename.
+ * @returns The generated filename.
+ */
 const rotatingFilenameGenerator: Generator = (time: number | Date, index?: number): string => {
-  if (!time) return LOGFILE_NAME;
-
-  if (!(time instanceof Date)) {
-    time = new Date(time);
-  }
-  const month = time.getFullYear() + "" + pad(time.getMonth() + 1);
-  const day = pad(time.getDate());
-  const hour = pad(time.getHours());
-  const minute = pad(time.getMinutes());
-
-  return `${month}/${month}${day}-${hour}${minute}-${index}-${LOGFILE_NAME}`;
+  const currentTime = new Date();
+  const year = currentTime.getFullYear();
+  const month = pad(currentTime.getMonth() + 1);
+  const day = pad(currentTime.getDate());
+  const hour = pad(currentTime.getHours());
+  const minute = pad(currentTime.getMinutes());
+  const newFileName = `${year}-${month}-${day}-${hour}${minute}-${index ?? 0}-${LOGFILE_NAME}`;
+  ROTATED_LOGFILE_NAMES.add(newFileName);
+  CURRENT_LOGFILE_NAME = newFileName;
+  return newFileName;
 };
