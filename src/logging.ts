750293021fa492fbc8764aa95ad42e86a337d17f
Dave Shoup <dave.shoup@gmail.com>
2025-03-10 14:45:21 -0400

    cleanup and comments

--- src/logging.ts
+++ src/logging.ts
@@ -3,26 +3,6 @@ import { createStream, RotatingFileStream } from "rotating-file-stream";
 import { LogOutputChannel, window } from "vscode";
 
 /**
- * Default path to store downloadable log files for this extension instance.
- *
- * The main difference between this and the ExtensionContext.logUri (where LogOutputChannel lines
- * are written) is this will include ALL log levels, not just the ones enabled in the output channel.
- */
-export const LOGFILE_DIR = tmpdir();
-
-/** Max size of any log file written to disk.
- * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#size */
-const MAX_LOGFILE_SIZE = "15K"; // 10MB max file size
-
-/** Number of log files to keep.
- * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#maxfiles */
-const MAX_LOGFILES = 3; // only keep 3 log files at a time
-
-/** How often log files should rotate if they don't exceed {@link MAX_LOGFILE_SIZE}.
- * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#interval */
-const LOGFILE_ROTATION_INTERVAL = "1d"; // rotate log files daily
-
-/**
  * Main "Confluent" output channel.
  * @remarks We're using a {@link LogOutputChannel} instead of an `OutputChannel`
  * because it includes timestamps and colored log levels in the output by default.
@@ -134,7 +114,11 @@ export class Logger {
 
     return new Promise<void>((resolve, reject) => {
       try {
-        const stream = getLogFileStream();
+        const stream: RotatingFileStream = getLogFileStream();
+        if (stream.closed) {
+          resolve();
+          return;
+        }
         stream.write(formattedMessage, (error) => {
           if (error) {
             console.error("Error writing to log file:", error);
@@ -151,18 +135,46 @@ export class Logger {
   }
 }
 
+/**
+ * Default path to store downloadable log files for this extension instance.
+ *
+ * The main difference between this and the ExtensionContext.logUri (where LogOutputChannel lines
+ * are written) is this will include ALL log levels, not just the ones enabled in the output channel.
+ */
+export const LOGFILE_DIR = tmpdir();
+
+/** The name of the currently active log file, including time/index prefixing. */
+export let CURRENT_LOGFILE_NAME: string;
+
+/** Set of log file names that have already been created for this extension instance. */
+export const ROTATED_LOGFILE_NAMES: string[] = [];
+
+/** Max size of any log file written to disk.
+ * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#size */
+const MAX_LOGFILE_SIZE = "10K"; // 10MB max file size
+
+/** Number of log files to keep.
+ * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#maxfiles */
+const MAX_LOGFILES = 3; // only keep 3 **rotated** log files at a time for this extension instance
+
+/** How often log files should rotate if they don't exceed {@link MAX_LOGFILE_SIZE}.
+ * @see https://github.com/iccicci/rotating-file-stream?tab=readme-ov-file#interval */
+const LOGFILE_ROTATION_INTERVAL = "1d"; // rotate log files daily
+
 /** Single stream to allow rotating-file-stream to keep track of file sizes and rotation timing. */
 let logFileStream: RotatingFileStream | undefined;
 
 /** Creates a new rotating log file stream if it doesn't already exist. */
 export function getLogFileStream(): RotatingFileStream {
   if (!logFileStream) {
+    // don't use the `maxSize` option since that will interfere with proper rotation and cleanup by
+    // immediately removing the created rotated log file
     logFileStream = createStream(rotatingFilenameGenerator, {
       size: MAX_LOGFILE_SIZE,
-      maxSize: MAX_LOGFILE_SIZE,
       maxFiles: MAX_LOGFILES,
       interval: LOGFILE_ROTATION_INTERVAL,
       path: LOGFILE_DIR,
+      history: `vscode-confluent-${process.pid}.history.log`,
       encoding: "utf-8",
     });
   }
@@ -170,39 +182,36 @@ export function getLogFileStream(): RotatingFileStream {
 }
 
 /**
- * Pads a number with leading zero if it's less than 10.
- * @param num - The number to pad.
- * @returns The padded number as a string.
- */
-function pad(num: number): string {
-  return (num > 9 ? "" : "0") + num;
-}
-
-/** Set of log file names that have already been created for this extension instance. */
-const ROTATED_LOGFILE_NAMES = new Set<string>();
-
-/** The name of the currently active log file, including time/index prefixing. */
-export let CURRENT_LOGFILE_NAME: string;
-
-/** The name of the log file, without any time/index prefixing. */
-const LOGFILE_NAME = `vscode-confluent-${process.pid}.log`;
-
-/**
- * Generates a rotating filename based on the current date and time, with an optional index.
- * The format is `YYYYMMDD-HHMM-index-logfile.log`.
- * @param time - The date/time to use for the filename. If not provided, defaults to the current date/time.
- * @param index - An optional index to append to the filename.
- * @returns The generated filename.
+ * Generates a rotating filename based on the index, to be used in the `RotatingFileStream`'s
+ * internal operations.
+ *
+ * The currently-active logfile will always end in `0.log`, and as new files are created, the older
+ * files will be removed until the max number of files is reached.
+ *
+ * Example for a max of 3 log files:
+ * First set of rotations:
+ * - `vscode-confluent-1234.0.log` (current log file)
+ * - `vscode-confluent-1234.1.log` (oldest log file)
+ * - `vscode-confluent-1234.2.log`
+ * - `vscode-confluent-1234.3.log` (new log file)
+ *
+ * The next rotation will remove the oldest file and create a new one:
+ * - `vscode-confluent-1234.0.log` (current log file)
+ * - (`vscode-confluent-1234.1.log` is deleted)
+ * - `vscode-confluent-1234.2.log`
+ * - `vscode-confluent-1234.3.log`
+ * - `vscode-confluent-1234.4.log` (new log file)
  */
 function rotatingFilenameGenerator(time: number | Date, index?: number): string {
-  const currentTime = new Date();
-  const year = currentTime.getFullYear();
-  const month = pad(currentTime.getMonth() + 1);
-  const day = pad(currentTime.getDate());
-  const hour = pad(currentTime.getHours());
-  const minute = pad(currentTime.getMinutes());
-  const newFileName = `${year}-${month}-${day}-${hour}${minute}-${index ?? 0}-${LOGFILE_NAME}`;
-  ROTATED_LOGFILE_NAMES.add(newFileName);
+  const maybefileIndex = index !== undefined ? `.${index}` : "";
+  // use process.pid to keep the log file names unique across multiple extension instances
+  const newFileName = `vscode-confluent-${process.pid}${maybefileIndex}.log`;
+  ROTATED_LOGFILE_NAMES.push(newFileName);
+  if (ROTATED_LOGFILE_NAMES.length > MAX_LOGFILES) {
+    // remove the oldest log file from the array
+    // (RotatingFileStream will handle the actual file deletion)
+    ROTATED_LOGFILE_NAMES.shift();
+  }
   CURRENT_LOGFILE_NAME = newFileName;
   return newFileName;
 }
