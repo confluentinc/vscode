d0051c731d76f27d8c2cf0f7cebd7003ed76a05d
Dave Shoup <dave.shoup@gmail.com>
2025-03-19 17:06:25 -0400

    add disabled versions flag

--- src/featureFlags/constants.ts
+++ src/featureFlags/constants.ts
@@ -1,8 +1,10 @@
 import { LDFlagSet } from "launchdarkly-electron-client-sdk";
 
 export enum FeatureFlag {
-  /** Is this extension version enabled at all? */
+  /** Is this extension enabled at all? */
   GLOBAL_ENABLED = "ide.global.enabled",
+  /** Are any specific versions of the extension disabled? */
+  GLOBAL_DISABLED_VERSIONS = "ide.global.disabledVersions",
   /** Are there any notices for this version that need to be shown to users? */
   GLOBAL_NOTICES = "ide.global.notices",
   /** Is Segment enabled? */
@@ -15,10 +17,12 @@ export enum FeatureFlag {
  * reachable. */
 export const FEATURE_FLAG_DEFAULTS: LDFlagSet = {
   [FeatureFlag.GLOBAL_ENABLED]: true,
+  [FeatureFlag.GLOBAL_DISABLED_VERSIONS]: [],
   [FeatureFlag.GLOBAL_NOTICES]: [],
   [FeatureFlag.SEGMENT_ENABLE]: true,
   [FeatureFlag.CCLOUD_ENABLE]: true,
 };
 
-/** Feature flags and their values, subject to change based on the responses from LaunchDarkly. */
+/** Feature flags and their current values
+ * (These may change based on the responses from LaunchDarkly.) */
 export const FeatureFlags: LDFlagSet = {};
