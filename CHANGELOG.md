4bb9fab537edec2b90837412134ab197cda2927f
Dave Shoup <dave.shoup@gmail.com>
2025-03-19 16:30:29 -0400

    🍒 [main] fix: failing to fetch preferences when updating the extension (#1299)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ All notable changes to this extension will be documented in this file.
 ### Fixed
 
 - Improved resolving newly created direct connection connectivity state.
+- Fixed an issue where the extension would fail to sync CCloud authentication related user settings
+  to the sidecar process when updating the extension.
 
 ## 1.0.1
 
--- src/preferences/updates.test.ts
+++ src/preferences/updates.test.ts
@@ -1,20 +1,29 @@
 import * as assert from "assert";
-import sinon from "sinon";
-import {
-  Preferences,
-  PreferencesResourceApi,
-  PreferencesSpec,
-  ResponseError,
-} from "../clients/sidecar";
+import * as sinon from "sinon";
+import { workspace } from "vscode";
+import { Preferences, PreferencesResourceApi, PreferencesSpec } from "../clients/sidecar";
+import * as errors from "../errors";
 import * as sidecar from "../sidecar";
+import {
+  DEFAULT_SSL_PEM_PATHS,
+  DEFAULT_TRUST_ALL_CERTIFICATES,
+  SSL_PEM_PATHS,
+  SSL_VERIFY_SERVER_CERT_DISABLED,
+} from "./constants";
 import * as updates from "./updates";
+import { loadPreferencesFromWorkspaceConfig } from "./updates";
 
 describe("preferences/updates", function () {
   let sandbox: sinon.SinonSandbox;
   let mockClient: sinon.SinonStubbedInstance<PreferencesResourceApi>;
+  let getConfigurationStub: sinon.SinonStub;
+  let logErrorStub: sinon.SinonStub;
 
   beforeEach(function () {
     sandbox = sinon.createSandbox();
+
+    logErrorStub = sandbox.stub(errors, "logError");
+
     // create the stubs for the sidecar + service client
     const mockSidecarHandle: sinon.SinonStubbedInstance<sidecar.SidecarHandle> =
       sandbox.createStubInstance(sidecar.SidecarHandle);
@@ -22,49 +31,97 @@ describe("preferences/updates", function () {
     mockSidecarHandle.getPreferencesApi.returns(mockClient);
     // stub the getSidecar function to return the mock sidecar handle
     sandbox.stub(sidecar, "getSidecar").resolves(mockSidecarHandle);
+
+    // stub the WorkspaceConfiguration
+    getConfigurationStub = sandbox.stub();
+    sandbox.stub(workspace, "getConfiguration").returns({
+      get: getConfigurationStub,
+      has: sandbox.stub(),
+      update: sandbox.stub(),
+      inspect: sandbox.stub(),
+    });
   });
 
   afterEach(function () {
     sandbox.restore();
   });
 
-  it("should fetch preferences successfully", async function () {
-    const mockPreferences: Preferences = { spec: {} } as Preferences;
-    mockClient.gatewayV1PreferencesGet.resolves(mockPreferences);
+  it("loadDefaultPreferences() should load default preferences from the workspace configuration", async function () {
+    // default values, no user changes
+    const tlsPemPaths: string[] = DEFAULT_SSL_PEM_PATHS;
+    const trustAllCerts = DEFAULT_TRUST_ALL_CERTIFICATES;
 
-    const result = await updates.fetchPreferences();
+    getConfigurationStub.withArgs(SSL_PEM_PATHS, DEFAULT_SSL_PEM_PATHS).returns(tlsPemPaths);
+    getConfigurationStub
+      .withArgs(SSL_VERIFY_SERVER_CERT_DISABLED, DEFAULT_TRUST_ALL_CERTIFICATES)
+      .returns(trustAllCerts);
 
-    assert.strictEqual(result, mockPreferences);
+    const result: PreferencesSpec = loadPreferencesFromWorkspaceConfig();
+
+    assert.deepStrictEqual(result, {
+      tls_pem_paths: DEFAULT_SSL_PEM_PATHS,
+      trust_all_certificates: DEFAULT_TRUST_ALL_CERTIFICATES,
+    });
   });
 
-  it("should rethrow if fetchPreferences fails with ResponseError", async function () {
-    const mockError = new ResponseError(
-      new Response(null, { status: 500, statusText: "Internal Server Error" }),
-    );
-    mockClient.gatewayV1PreferencesGet.rejects(mockError);
+  it("loadDefaultPreferences() should load preferences with custom values", async function () {
+    // simulate user changing from the default values
+    const tlsPemPaths: string[] = ["path/to/custom.pem"];
+    const trustAllCerts = true;
 
-    await assert.rejects(updates.fetchPreferences(), mockError);
-  });
+    getConfigurationStub.withArgs(SSL_PEM_PATHS, DEFAULT_SSL_PEM_PATHS).returns(tlsPemPaths);
+    getConfigurationStub
+      .withArgs(SSL_VERIFY_SERVER_CERT_DISABLED, DEFAULT_TRUST_ALL_CERTIFICATES)
+      .returns(trustAllCerts);
 
-  it("should rethrow if fetchPreferences fails with non-ResponseError", async function () {
-    const mockError = new Error("Some error");
-    mockClient.gatewayV1PreferencesGet.rejects(mockError);
+    const result: PreferencesSpec = loadPreferencesFromWorkspaceConfig();
 
-    await assert.rejects(updates.fetchPreferences(), mockError);
+    assert.deepStrictEqual(result, {
+      tls_pem_paths: tlsPemPaths,
+      trust_all_certificates: trustAllCerts,
+    });
   });
 
   it("should update preferences successfully", async function () {
-    const mockPreferences: Preferences = { spec: {} } as Preferences;
-    mockClient.gatewayV1PreferencesGet.resolves(mockPreferences);
-    const mockUpdatedSpec: PreferencesSpec = { tls_pem_paths: ["path/to/pem"] };
-    const mockUpdatedPreferences: Preferences = {
-      ...mockPreferences,
-      spec: mockUpdatedSpec,
+    // simulate user changing from the default values
+    const tlsPemPaths: string[] = ["path/to/custom.pem"];
+    const trustAllCerts = true;
+
+    getConfigurationStub.withArgs(SSL_PEM_PATHS, DEFAULT_SSL_PEM_PATHS).returns(tlsPemPaths);
+    getConfigurationStub
+      .withArgs(SSL_VERIFY_SERVER_CERT_DISABLED, DEFAULT_TRUST_ALL_CERTIFICATES)
+      .returns(trustAllCerts);
+
+    const fakePreferences: Preferences = {
+      api_version: "gateway/v1",
+      kind: "Preferences",
+      spec: {
+        tls_pem_paths: tlsPemPaths,
+        trust_all_certificates: trustAllCerts,
+      },
     };
-    mockClient.gatewayV1PreferencesPut.resolves(mockUpdatedPreferences);
+    mockClient.gatewayV1PreferencesPut.resolves(fakePreferences);
 
-    await updates.updatePreferences(mockUpdatedSpec);
+    await updates.updatePreferences();
 
-    assert.ok(mockClient.gatewayV1PreferencesPut.calledOnce);
+    sinon.assert.calledWithExactly(mockClient.gatewayV1PreferencesPut, {
+      Preferences: fakePreferences,
+    });
+  });
+
+  it("updatePreferences() should log and not re-throw errors when updating preferences", async function () {
+    const errorMessage = "Failed to update preferences";
+    mockClient.gatewayV1PreferencesPut.rejects(new Error(errorMessage));
+
+    await updates.updatePreferences();
+
+    sinon.assert.calledOnce(mockClient.gatewayV1PreferencesPut);
+    sinon.assert.calledWithExactly(
+      logErrorStub,
+      sinon.match.instanceOf(Error),
+      "updating preferences",
+      {},
+      true,
+    );
   });
 });
