5672bb131bed3655dda25e5c9de429e4555440f6
James Robinson <jrobinson@confluent.io>
2025-03-20 14:40:36 -0400

    Migrate helpers to utils subpackage.

--- src/commands/schemas.ts
+++ src/commands/schemas.ts
@@ -12,6 +12,10 @@ import { schemaTypeQuickPick } from "../quickpicks/schemas";
 import { hashed, logUsage, UserEvent } from "../telemetry/events";
 import { getSchemasViewProvider } from "../viewProviders/schemas";
 import { uploadSchemaForSubjectFromfile, uploadSchemaFromFile } from "./schemaUpload";
+import {
+  getDeleteSchemaVersionPrompt,
+  getSchemaDeletionValidatorAndPlaceholder,
+} from "./utils/schemas";
 
 const logger = new Logger("commands.schemas");
 
@@ -300,81 +304,6 @@ async function deleteSchemaVersionCommand(schema: Schema) {
     subject_hash: hashed(schema.subject),
     schema_version: schema.version,
   });
-}
-
-/**
- * Determine the prompt to show when deleting a schema version.
- * @param hardDeletion Is a hard delete going to be performed?
- * @param schema The Schema/version to delete.
- * @param loader ResourceLoader to use to get the schema group.
- * @returns Customized prompt for the delete action.
- */
-async function getDeleteSchemaVersionPrompt(
-  hardDeletion: boolean,
-  schema: Schema,
-  loader: ResourceLoader,
-): Promise<string> {
-  const schemaGroup = await loader.getSchemasForSubject(schema.environmentId!, schema.subject);
-  const isOnlyVersion = schemaGroup.length === 1;
-
-  const deleteVerb = (hardDeletion ? "hard" : "soft") + " delete";
-  let prompt: string;
-
-  if (isOnlyVersion) {
-    prompt = `Are you sure you want to ${deleteVerb} the only version of subject ${schema.subject}?`;
-  } else {
-    const isLatestVersion = schema.version === schemaGroup[0].version;
-    if (isLatestVersion) {
-      prompt = `Are you sure you want to ${deleteVerb} the latest version of subject ${schema.subject}? Version ${schemaGroup[1].version} will become the latest.`;
-    } else {
-      prompt = `Are you sure you want to ${deleteVerb} version ${schema.version} of subject ${schema.subject}?`;
-    }
-  }
-
-  return prompt;
-}
-
-type ValidationFunction = (input: string) => vscode.InputBoxValidationMessage | undefined;
-/**
- * Returns a pair:
- *  [0]: Function that can be used to validate the confirmation input for a schema version for deletion.
- *  [1]: The string to show in the input box as a placeholder.
- * @param version The version to confirm.
- * @param hardDeletion Whether this is for a hard deletion or not.
- * @returns pair of [validation function, prompt string]
- */
-function getSchemaDeletionValidatorAndPlaceholder(
-  version: number,
-  hardDeletion: boolean,
-): [ValidationFunction, string] {
-  let validator: ValidationFunction;
-  let prompt: string;
-
-  if (hardDeletion) {
-    prompt = `Enter "hard v${version}" to confirm, escape to cancel.`;
-    validator = (input: string) => {
-      if (input === `hard v${version}`) {
-        return;
-      }
-      return {
-        message: `Enter "hard v${version}" to confirm hard deletion, escape to cancel.`,
-        severity: vscode.InputBoxValidationSeverity.Error,
-      };
-    };
-  } else {
-    prompt = `Enter "v${version}" to confirm, escape to cancel.`;
-    validator = (input: string) => {
-      if (input === `v${version}`) {
-        return;
-      }
-      return {
-        message: `Enter "v${version}" to confirm, escape to cancel.`,
-        severity: vscode.InputBoxValidationSeverity.Error,
-      };
-    };
-  }
-
-  return [validator, prompt];
 }
 
 /**
