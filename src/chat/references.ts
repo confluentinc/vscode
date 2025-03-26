6a9525483d0e08287e4d6c315097dd5c9ba6c548
Dave Shoup <dave.shoup@gmail.com>
2025-03-26 15:59:45 -0400

    update URI parsing and content loading from ChatPromptReference

--- src/chat/references.ts
+++ src/chat/references.ts
@@ -1,7 +1,13 @@
-import { readFile } from "fs/promises";
-import { ChatPromptReference, LanguageModelChatMessage, Uri } from "vscode";
+import {
+  ChatPromptReference,
+  LanguageModelChatMessage,
+  Location,
+  Range,
+  TextDocument,
+  Uri,
+  workspace,
+} from "vscode";
 import { Logger } from "../logging";
-import { PARTICIPANT_ID } from "./constants";
 
 const logger = new Logger("chat.references");
 
@@ -12,8 +18,10 @@ export async function parseReferences(
   const referenceMessages: LanguageModelChatMessage[] = [];
 
   for (const reference of references) {
-    const referenceMessage = await handleReference(reference);
-    referenceMessages.push(referenceMessage);
+    const referenceMessage: LanguageModelChatMessage | undefined = await handleReference(reference);
+    if (referenceMessage) {
+      referenceMessages.push(referenceMessage);
+    }
   }
 
   return referenceMessages;
@@ -22,25 +30,33 @@ export async function parseReferences(
 /** Handle a single reference from the user's chat message. */
 export async function handleReference(
   reference: ChatPromptReference,
-): Promise<LanguageModelChatMessage> {
+): Promise<LanguageModelChatMessage | undefined> {
   logger.debug("handling reference:", reference);
 
   switch (reference.id) {
-    case "file": {
-      const fileUri = Uri.from(reference.value as Uri);
-      const fileContent = await readFile(fileUri.fsPath, "utf8");
-      return LanguageModelChatMessage.User(
-        `${fileUri.path}:\n\n\`\`\`\n${fileContent}\n\`\`\``,
-        "user",
-      );
+    // custom URI (message preview, schema definitions, etc), editor, or file
+    case "vscode.implicit.viewport":
+    case "vscode.untitled":
+    case "vscode.file": {
+      let uri: Uri;
+      let range: Range | undefined;
+      if (reference.value instanceof Uri) {
+        uri = reference.value;
+      } else if (reference.value instanceof Location) {
+        // may be a selection in a document
+        uri = reference.value.uri;
+        range = reference.value.range;
+      } else {
+        logger.error("reference type not yet supported:", reference);
+        return;
+      }
+
+      const title = uri.path.split("/").pop() || "Untitled";
+      const document: TextDocument = await workspace.openTextDocument(uri);
+      const content: string = document.getText(range);
+      // TODO: clean up this formatting:
+      return LanguageModelChatMessage.User(`#file:${title}:\n\n\`\`\`\n${content}\n\`\`\``, "user");
     }
-    case "copilot.selection":
-      return LanguageModelChatMessage.User(`${reference.value}`, "user");
     // TODO: handle other reference types
-    default:
-      return LanguageModelChatMessage.Assistant(
-        "I don't know how to handle this reference",
-        PARTICIPANT_ID,
-      );
   }
 }
