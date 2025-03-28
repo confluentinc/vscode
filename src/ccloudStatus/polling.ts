11c51f20301212a8c15f512ce69b1af9a9d67d65
Dave Shoup <dave.shoup@gmail.com>
2025-03-28 10:53:41 -0400

    remove CCloudNotice interface and pass CCloudStatusSummary directly for status bar item update

--- src/ccloudStatus/polling.ts
+++ src/ccloudStatus/polling.ts
@@ -1,11 +1,8 @@
-import { MarkdownString } from "vscode";
 import { Logger } from "../logging";
 import { updateCCloudStatus } from "../statusBar/ccloudItem";
-import { CCloudNotice } from "../statusBar/types";
-import { titleCase } from "../utils";
 import { IntervalPoller } from "../utils/timing";
 import { fetchCCloudStatus } from "./api";
-import { CCloudStatusSummary, Incident, ScheduledMaintenance, StatusUpdate } from "./types";
+import { CCloudStatusSummary } from "./types";
 
 const logger = new Logger("ccloudStatus.polling");
 
@@ -55,62 +52,6 @@ export async function refreshCCloudStatus() {
     numMaintenances: notices.filter((n) => n.type === "maintenance").length,
   });
   // update the status bar item with the latest incidents/maintenance notices
-  updateCCloudStatus(notices);
+  updateCCloudStatus(status);
   logger.debug("CCloud status bar item updated");
-}
-
-/**
- * Converts a {@link CCloudStatusSummary} object to an array of {@link CCloudNotice} for displaying in
- * the CCloud status bar item.
- */
-export function convertStatusToNotices(status: CCloudStatusSummary): CCloudNotice[] {
-  const notices: CCloudNotice[] = [];
-
-  if (status.incidents) {
-    status.incidents.forEach((incident: Incident) => {
-      notices.push({
-        type: "incident",
-        message: formatStatusUpdate(incident),
-      });
-    });
-  }
-
-  if (status.scheduled_maintenances) {
-    status.scheduled_maintenances.forEach((maintenance: ScheduledMaintenance) => {
-      notices.push({
-        type: "maintenance",
-        message: formatStatusUpdate(maintenance),
-      });
-    });
-  }
-
-  return notices;
-}
-
-/** Creates a markdown formatted string for an {@link Incident} or {@link ScheduledMaintenance}. */
-export function formatStatusUpdate(item: Incident | ScheduledMaintenance): string {
-  let message = new MarkdownString(`[${item.name}](${item.shortlink})`);
-
-  const latestUpdate: StatusUpdate | undefined = item.incident_updates
-    .sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
-    .shift();
-  if (latestUpdate) {
-    const date = new Date(latestUpdate.updated_at);
-    // e.g. "Jan 01, 12:00 UTC" similar to https://status.confluent.cloud/
-    const dateStr = !isNaN(date.getTime())
-      ? date.toLocaleString("en-US", {
-          month: "short",
-          day: "2-digit",
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-          timeZone: "UTC",
-        })
-      : "Unknown date";
-    message.appendMarkdown(
-      `\n   - _${dateStr} UTC_: **${titleCase(latestUpdate.status)}** - ${latestUpdate.body}`,
-    );
-  }
-
-  return message.value;
 }
--- src/statusBar/types.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Basic notice structure for the CCloud status bar item. The `type` will affect the background
- * color used, and the `message` will be displayed in the status bar item tooltip.
- */
-export interface CCloudNotice {
-  type: "maintenance" | "incident";
-  message: string;
-}
