6f1b403f17f61fbfe2245051581ead7fdf0b67a0
Dave Shoup <dave.shoup@gmail.com>
2025-04-02 13:54:33 -0400

    add basic `FlinkArtifact*` models

---
+++ src/models/flinkArtifact.ts
@@ -0,0 +1,72 @@
+import { ThemeIcon, TreeItem, TreeItemCollapsibleState } from "vscode";
+import { ConnectionType } from "../clients/sidecar";
+import { IconNames } from "../constants";
+import { IdItem } from "./main";
+import { ConnectionId, EnvironmentId, IResourceBase, ISearchable } from "./resource";
+
+export class FlinkArtifact implements IResourceBase, IdItem, ISearchable {
+  connectionId!: ConnectionId;
+  connectionType!: ConnectionType;
+  iconName: IconNames = IconNames.FLINK_ARTIFACT;
+
+  environmentId!: EnvironmentId;
+  computePoolId!: string;
+
+  name!: string; // display_name
+  status!: string;
+
+  provider!: string; // cloud
+  region!: string;
+
+  // TODO: add more properties as needed
+
+  constructor(
+    props: Pick<
+      FlinkArtifact,
+      | "connectionId"
+      | "connectionType"
+      | "environmentId"
+      | "computePoolId"
+      | "name"
+      | "status"
+      | "provider"
+      | "region"
+    >,
+  ) {
+    this.connectionId = props.connectionId;
+    this.connectionType = props.connectionType;
+    this.environmentId = props.environmentId;
+    this.computePoolId = props.computePoolId;
+    this.name = props.name;
+    this.status = props.status;
+    this.provider = props.provider;
+    this.region = props.region;
+  }
+
+  get id(): string {
+    return `${this.connectionId}-${this.computePoolId}-${this.name}`;
+  }
+
+  searchableText(): string {
+    return `${this.name} ${this.status}`;
+  }
+}
+
+export class FlinkArtifactTreeItem extends TreeItem {
+  resource: FlinkArtifact;
+
+  constructor(resource: FlinkArtifact) {
+    super(resource.name, TreeItemCollapsibleState.None);
+
+    // internal properties
+    this.id = `${resource.connectionId}-${resource.computePoolId}-${resource.name}`;
+    this.resource = resource;
+    this.contextValue = `${resource.connectionType.toLowerCase()}-flink-artifact`;
+
+    // user-facing properties
+    this.iconPath = new ThemeIcon(resource.iconName);
+    this.description = resource.status;
+
+    // TODO: add tooltip
+  }
+}
