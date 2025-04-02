2ab252a8f66b9e96f0e14574a75ff5fa08e0ff8a
Dave Shoup <dave.shoup@gmail.com>
2025-04-02 16:41:34 -0400

    fix search context setting ahead of actual search-implementation branch; early exit from getChildren without focused pool

--- src/viewProviders/flinkArtifacts.ts
+++ src/viewProviders/flinkArtifacts.ts
@@ -1,19 +1,28 @@
 import { Disposable, TreeDataProvider, TreeItem } from "vscode";
 import { ConnectionType } from "../clients/sidecar";
 import { CCLOUD_CONNECTION_ID } from "../constants";
+import { ContextValues, setContextValue } from "../context/values";
+import { currentFlinkArtifactsPoolChanged } from "../emitters";
+import { Logger } from "../logging";
 import { FlinkArtifact, FlinkArtifactTreeItem } from "../models/flinkArtifact";
 import { FlinkComputePool } from "../models/flinkComputePool";
 import { EnvironmentId } from "../models/resource";
 import { BaseViewProvider } from "./base";
+
+const logger = new Logger("viewProviders.flinkArtifacts");
 
 export class FlinkArtifactsViewProvider
   extends BaseViewProvider<FlinkComputePool, FlinkArtifact>
   implements TreeDataProvider<FlinkArtifact>
 {
   viewId = "confluent-flink-artifacts";
+  searchContextValue = ContextValues.flinkArtifactsSearchApplied;
 
   async getChildren(): Promise<FlinkArtifact[]> {
     const children: FlinkArtifact[] = [];
+    if (!this.computePool) {
+      return children;
+    }
 
     // TODO: replace this with real data
     const fakeArtifact = new FlinkArtifact({
@@ -43,7 +52,31 @@ export class FlinkArtifactsViewProvider
   }
 
   setEventListeners(): Disposable[] {
-    return [];
+    const poolChangedSub: Disposable = currentFlinkArtifactsPoolChanged.event(
+      async (pool: FlinkComputePool | null) => {
+        logger.debug(
+          `currentFlinkArtifactsPool event fired, ${pool ? "refreshing" : "resetting"}.`,
+          { pool },
+        );
+        this.setSearch(null); // reset search when pool changes
+        if (!pool) {
+          this.reset();
+        } else {
+          setContextValue(ContextValues.flinkArtifactsPoolSelected, true);
+          this.resource = pool;
+          await this.updateTreeViewDescription();
+          this.refresh();
+        }
+      },
+    );
+    return [poolChangedSub];
+  }
+
+  async reset() {
+    logger.debug("resetting view");
+    setContextValue(ContextValues.flinkArtifactsPoolSelected, false);
+    this.resource = null;
+    await this.updateTreeViewDescription();
   }
 
   get computePool(): FlinkComputePool | null {
--- src/viewProviders/flinkStatements.ts
+++ src/viewProviders/flinkStatements.ts
@@ -1,19 +1,28 @@
 import { Disposable, TreeDataProvider, TreeItem } from "vscode";
 import { ConnectionType } from "../clients/sidecar";
 import { CCLOUD_CONNECTION_ID } from "../constants";
+import { ContextValues, setContextValue } from "../context/values";
+import { currentFlinkStatementsPoolChanged } from "../emitters";
+import { Logger } from "../logging";
 import { FlinkComputePool } from "../models/flinkComputePool";
 import { FlinkStatement, FlinkStatementTreeItem } from "../models/flinkStatement";
 import { EnvironmentId } from "../models/resource";
 import { BaseViewProvider } from "./base";
+
+const logger = new Logger("viewProviders.flinkStatements");
 
 export class FlinkStatementsViewProvider
   extends BaseViewProvider<FlinkComputePool, FlinkStatement>
   implements TreeDataProvider<FlinkStatement>
 {
   viewId = "confluent-flink-statements";
+  searchContextValue = ContextValues.flinkStatementsSearchApplied;
 
   async getChildren(): Promise<FlinkStatement[]> {
     const children: FlinkStatement[] = [];
+    if (!this.computePool) {
+      return children;
+    }
 
     // TODO: replace this with real data
     const fakeStatement = new FlinkStatement({
@@ -46,7 +55,31 @@ export class FlinkStatementsViewProvider
   }
 
   setEventListeners(): Disposable[] {
-    return [];
+    const poolChangedSub: Disposable = currentFlinkStatementsPoolChanged.event(
+      async (pool: FlinkComputePool | null) => {
+        logger.debug(
+          `currentFlinkStatementsPool event fired, ${pool ? "refreshing" : "resetting"}.`,
+          { pool },
+        );
+        this.setSearch(null); // reset search when pool changes
+        if (!pool) {
+          this.reset();
+        } else {
+          setContextValue(ContextValues.flinkStatementsPoolSelected, true);
+          this.resource = pool;
+          await this.updateTreeViewDescription();
+          this.refresh();
+        }
+      },
+    );
+    return [poolChangedSub];
+  }
+
+  async reset() {
+    logger.debug("resetting view");
+    setContextValue(ContextValues.flinkStatementsPoolSelected, false);
+    this.resource = null;
+    await this.updateTreeViewDescription();
   }
 
   get computePool(): FlinkComputePool | null {
