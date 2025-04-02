c1330d5c4f7ed8b0e26307b9b21fa6bc3c02288d
Dave Shoup <dave.shoup@gmail.com>
2025-04-02 09:38:41 -0400

    initial functionality with fake data

--- src/viewProviders/flinkStatements.ts
+++ src/viewProviders/flinkStatements.ts
@@ -1,4 +1,8 @@
 import { Disposable, TreeDataProvider, TreeItem } from "vscode";
+import { ConnectionType } from "../clients/sidecar";
+import { CCLOUD_CONNECTION_ID } from "../constants";
+import { FlinkStatement, FlinkStatementTreeItem } from "../models/flinkStatement";
+import { EnvironmentId } from "../models/resource";
 import { BaseViewProvider } from "./base";
 
 export class FlinkStatementsViewProvider
@@ -7,14 +11,37 @@ export class FlinkStatementsViewProvider
 {
   viewId = "confluent-flink-statements";
 
-  private static instance: FlinkStatementsViewProvider;
-
   async getChildren(): Promise<FlinkStatement[]> {
-    return [];
+    const children: FlinkStatement[] = [];
+
+    // TODO: replace this with real data
+    const fakeStatement = new FlinkStatement({
+      connectionId: CCLOUD_CONNECTION_ID,
+      connectionType: ConnectionType.Ccloud,
+      environmentId: "env1" as EnvironmentId,
+      computePoolId: "pool1",
+      name: "statement1",
+      status: "running",
+    });
+    children.push(
+      fakeStatement,
+      new FlinkStatement({
+        ...fakeStatement,
+        name: "statement2",
+        status: "failed",
+      }),
+      new FlinkStatement({
+        ...fakeStatement,
+        name: "statement3",
+        status: "stopped",
+      }),
+    );
+
+    return this.filterChildren(undefined, children);
   }
 
   getTreeItem(element: FlinkStatement): TreeItem {
-    return new TreeItem(element.name);
+    return new FlinkStatementTreeItem(element);
   }
 
   setEventListeners(): Disposable[] {
