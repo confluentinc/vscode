d9c199f485326b46716f7a13b016f5107709562a
James Robinson <james@relatable.dev>
2025-04-02 09:24:48 -0400

    Generate client for flink artifacts APIs. (#1394)

---
+++ src/clients/flinkArtifacts/models/index.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./ArtifactV1FlinkArtifact";
+export * from "./ArtifactV1FlinkArtifactList";
+export * from "./ArtifactV1FlinkArtifactListDataInner";
+export * from "./ArtifactV1FlinkArtifactListMetadata";
+export * from "./ArtifactV1FlinkArtifactMetadata";
+export * from "./ArtifactV1FlinkArtifactVersion";
+export * from "./ArtifactV1FlinkArtifactVersionUploadSource";
+export * from "./ArtifactV1PresignedUrl";
+export * from "./ArtifactV1PresignedUrlRequest";
+export * from "./ArtifactV1PresignedUrlRequestMetadata";
+export * from "./ArtifactV1UploadSourcePresignedUrl";
+export * from "./ArtifactV1UploadSourcePresignedUrlMetadata";
+export * from "./CreateArtifactV1FlinkArtifact201Response";
+export * from "./CreateArtifactV1FlinkArtifactRequest";
+export * from "./CreateArtifactV1FlinkArtifactRequestUploadSource";
+export * from "./ErrorSource";
+export * from "./Failure";
+export * from "./GetArtifactV1FlinkArtifact200Response";
+export * from "./ListMeta";
+export * from "./ModelError";
+export * from "./ObjectMeta";
+export * from "./ObjectReference";
+export * from "./PresignedUploadUrlArtifactV1PresignedUrl200Response";
+export * from "./PresignedUploadUrlArtifactV1PresignedUrlRequest";
