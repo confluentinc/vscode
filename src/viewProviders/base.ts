527bf271d6e0a73f51a6a6cd726d10f69806fdc2
Dave Shoup <dave.shoup@gmail.com>
2025-04-04 09:59:29 -0400

    fix .reset() and clean up; add BaseViewProvider method tests

--- src/viewProviders/base.ts
+++ src/viewProviders/base.ts
@@ -20,6 +20,12 @@ import { logUsage, UserEvent } from "../telemetry/events";
 import { titleCase } from "../utils";
 import { filterItems, itemMatchesSearch } from "./search";
 
+/**
+ * Base class for all tree view providers handling a primary resource type.
+ * @template P The type of the "parent" resource that can be "focused" in the view to determine which
+ * resources will be shown. (Example: `KafkaCluster`, `SchemaRegistry`, `FlinkComputePool`)
+ * @template T The primary resource(s) that will be shown in the view.
+ */
 export abstract class BaseViewProvider<
   P extends IResourceBase & IdItem & ISearchable & { environmentId: EnvironmentId },
   T extends IResourceBase & IdItem & ISearchable & { environmentId: EnvironmentId },
@@ -42,6 +48,10 @@ export abstract class BaseViewProvider<
   }
 
   protected treeView!: TreeView<T>;
+  /**
+   * The id of the view associated with this provider, set in `package.json` and used to register {@linkcode treeView} to the provider instance.
+   */
+  abstract viewId: string;
 
   /** The parent {@link Environment} of the focused resource.  */
   environment: Environment | null = null;
@@ -67,9 +77,8 @@ export abstract class BaseViewProvider<
   /** Count of all items returned from `getChildren()`. */
   totalItemCount: number = 0;
 
-  /** The id of the view associated with this provider, set in package.json. */
-  abstract viewId: string;
-
+  // NOTE: this is usually private/protected with a singleton pattern, but needs to be public for
+  // the subclasses to be called with .getInstance() properly
   public constructor() {
     if (!getExtensionContext()) {
       // getChildren() will fail without the extension context
@@ -106,12 +115,7 @@ export abstract class BaseViewProvider<
   /** Set up event listeners for this view provider. */
   private setEventListeners(): Disposable[] {
     const ccloudConnectedSub: Disposable = ccloudConnected.event((connected: boolean) => {
-      if (this.resource && isCCloud(this.resource)) {
-        // any transition of CCloud connection state should reset the tree view if we're focused on
-        // a CCloud parent resource
-        this.logger.debug("ccloudConnected event fired, resetting view", { connected });
-        this.reset();
-      }
+      this.handleCCloudConnectionChange(connected);
     });
 
     return [ccloudConnectedSub, ...this.setCustomEventListeners()];
@@ -129,6 +133,74 @@ export abstract class BaseViewProvider<
   /** Convenience method to revert this view to its original state. */
   async reset(): Promise<void> {
     this.logger.debug("reset() called, clearing tree view");
+    this.environment = null;
+    this.resource = null;
+
+    this.treeView.description = undefined;
+    this.treeView.message = undefined;
+
+    this.setSearch(null);
+    // TODO: update this to adjust associated context value for focused resource(s)
+
+    this.refresh();
+  }
+
+  /** Callback for  */
+  handleCCloudConnectionChange(connected: boolean) {
+    if (this.resource && isCCloud(this.resource)) {
+      // any transition of CCloud connection state should reset the tree view if we're focused on
+      // a CCloud parent resource
+      this.logger.debug("ccloudConnected event fired, resetting view", { connected });
+      this.reset();
+    }
+  }
+
+  /**
+   * Update the tree view description to show the currently-focused {@linkcode resource}'s parent
+   * {@link Environment} name and the resource ID.
+   *
+   * Reassigns {@linkcode environment} to the parent {@link Environment} of the {@linkcode resource}.
+   * */
+  async updateTreeViewDescription(): Promise<void> {
+    const subLogger = this.logger.withCallpoint("updateTreeViewDescription");
+
+    const focusedResource = this.resource;
+    if (!focusedResource) {
+      subLogger.debug("called with no focused resource, clearing view description");
+      this.treeView.description = "";
+      this.environment = null;
+      return;
+    }
+
+    subLogger.debug(
+      `called with ${focusedResource.constructor.name}, checking for environments...`,
+    );
+    const loader = ResourceLoader.getInstance(focusedResource.connectionId);
+    const envs: Environment[] = await loader.getEnvironments();
+    const parentEnv: Environment | undefined = envs.find(
+      (env) => env.id === focusedResource.environmentId,
+    );
+    this.environment = parentEnv ?? null;
+    if (parentEnv) {
+      subLogger.debug("found environment, setting view description");
+      this.treeView.description = `${parentEnv.name} | ${focusedResource.id}`;
+    } else {
+      subLogger.debug(`couldn't find parent environment for ${focusedResource.constructor.name}`);
+      this.treeView.description = "";
+    }
+  }
+
+  /** Update internal state when the {@link itemSearchString search string} is set or unset. */
+  setSearch(searchString: string | null): void {
+    // set/unset the filter so any calls to getChildren() will filter appropriately
+    this.itemSearchString = searchString;
+    if (this.searchContextValue) {
+      // set context value to toggle between "search" and "clear search" actions
+      setContextValue(this.searchContextValue, searchString !== null);
+    }
+    // clear from any previous search filter
+    this.searchMatches = new Set();
+    this.totalItemCount = 0;
   }
 
   /** Filter results from any {@link itemSearchString search string} applied to the current view. */
@@ -175,53 +247,5 @@ export abstract class BaseViewProvider<
     });
 
     return children;
-  }
-
-  /**
-   * Update the tree view description to show the currently-focused {@linkcode resource}'s parent
-   * {@link Environment} name and the resource ID.
-   *
-   * Reassigns {@linkcode environment} to the parent {@link Environment} of the {@linkcode resource}.
-   * */
-  async updateTreeViewDescription(): Promise<void> {
-    const subLogger = this.logger.withCallpoint("updateTreeViewDescription");
-
-    const focusedResource = this.resource;
-    if (!focusedResource) {
-      subLogger.debug("called with no focused resource, clearing view description");
-      this.treeView.description = "";
-      this.environment = null;
-      return;
-    }
-
-    subLogger.debug(
-      `called with ${focusedResource.constructor.name}, checking for environments...`,
-    );
-    const loader = ResourceLoader.getInstance(focusedResource.connectionId);
-    const envs: Environment[] = await loader.getEnvironments();
-    const parentEnv: Environment | undefined = envs.find(
-      (env) => env.id === focusedResource.environmentId,
-    );
-    this.environment = parentEnv ?? null;
-    if (parentEnv) {
-      subLogger.debug("found environment, setting view description");
-      this.treeView.description = `${parentEnv.name} | ${focusedResource.id}`;
-    } else {
-      subLogger.debug(`couldn't find parent environment for ${focusedResource.constructor.name}`);
-      this.treeView.description = "";
-    }
-  }
-
-  /** Update internal state when the search string is set or unset. */
-  setSearch(searchString: string | null): void {
-    // set/unset the filter so any calls to getChildren() will filter appropriately
-    this.itemSearchString = searchString;
-    if (this.searchContextValue) {
-      // set context value to toggle between "search" and "clear search" actions
-      setContextValue(this.searchContextValue, searchString !== null);
-    }
-    // clear from any previous search filter
-    this.searchMatches = new Set();
-    this.totalItemCount = 0;
   }
 }
