a9a69c44c918126c0b0c03c6266606d4a60405df
Dave Shoup <dave.shoup@gmail.com>
2025-04-04 09:41:42 -0400

    add flink statement fixture

---
+++ tests/unit/testResources/flinkStatement.ts
@@ -0,0 +1,14 @@
+import { ConnectionType } from "../../../src/clients/sidecar";
+import { CCLOUD_CONNECTION_ID } from "../../../src/constants";
+import { FlinkStatement } from "../../../src/models/flinkStatement";
+import { TEST_CCLOUD_ENVIRONMENT_ID } from "./environments";
+import { TEST_CCLOUD_FLINK_COMPUTE_POOL } from "./flinkComputePool";
+
+export const TEST_CCLOUD_FLINK_STATEMENT = new FlinkStatement({
+  connectionId: CCLOUD_CONNECTION_ID,
+  connectionType: ConnectionType.Ccloud,
+  environmentId: TEST_CCLOUD_ENVIRONMENT_ID,
+  computePoolId: TEST_CCLOUD_FLINK_COMPUTE_POOL.id,
+  id: "statement0",
+  status: "RUNNING",
+} as FlinkStatement);
