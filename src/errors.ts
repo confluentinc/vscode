ecac2ebe863e6c6dc47b985b5ece843ce52ec33e
Cerchie <lcerchie@confluent.io>
2025-04-11 08:40:47 -0700

    but of cleanup

--- src/errors.ts
+++ src/errors.ts
@@ -160,7 +160,7 @@ export async function logError(
   logger.error(logErrorMessage, { ...errorContext, ...sentryContext });
   // TODO: follow up to reuse EventHint type for capturing tags and other more fine-grained data
   if (Object.keys(sentryContext).length) {
-    sentryCaptureException(e, {
+    sentryCaptureException(wrappedError, {
       captureContext: {
         ...sentryContext,
         contexts: {
