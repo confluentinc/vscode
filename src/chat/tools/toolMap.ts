dee2efb0e9d514884772d727552d1940b8ae6ada
Dave Shoup <dave.shoup@gmail.com>
2025-04-11 11:08:49 -0400

    remove debuggery

--- src/chat/tools/toolMap.ts
+++ src/chat/tools/toolMap.ts
@@ -3,9 +3,7 @@ import { LanguageModelTool } from "vscode";
 const TOOL_MAP = new Map<string, LanguageModelTool<any>>();
 
 export function setToolMap(toolId: string, tool: LanguageModelTool<any>) {
-  console.log("here");
   TOOL_MAP.set(toolId, tool);
-  console.log("there");
 }
 
 export function getToolMap() {
