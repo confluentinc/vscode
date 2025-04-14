415de856a570f42bf341279bba72a2f44b83d751
James Robinson <jrobinson@confluent.io>
2025-04-14 16:15:14 -0400

    Fix docstring.

--- src/utils/workerPool.ts
+++ src/utils/workerPool.ts
@@ -47,9 +47,8 @@ interface WorkerPoolOptions {
  * Results will be returned in the same order as the input items, with each result containing either
  * a result or an error.
  *
- * @param items Array of items to process
- * @param context Optional singular context to pass to the callable
  * @param callable Async function to execute for each item
+ * @param items Array of items to process
  * @param options Worker pool options
  * @param progress Optional progress reporter
  * @param token Optional cancellation token
