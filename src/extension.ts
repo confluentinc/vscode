d97d487b95e694c67fe2509db647b534f7c6c70b
James Robinson <james@relatable.dev>
2025-04-15 13:41:13 -0400

    Offer "view in confluent cloud" for Flink statements. (#1481)

--- src/extension.ts
+++ src/extension.ts
@@ -307,6 +307,7 @@ async function setupContextValues() {
     "ccloud-kafka-topic",
     "ccloud-kafka-topic-with-schema",
     "ccloud-schema-registry",
+    "ccloud-flink-statement",
   ]);
   // allow for easier matching using "in" clauses for our Resources/Topics/Schemas views
   const viewsWithResources = setContextValue(ContextValues.VIEWS_WITH_RESOURCES, [
