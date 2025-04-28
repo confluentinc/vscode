65680ad5b9bc915eedf5e300077d4430bf3a4a62
Dave Shoup <dave.shoup@gmail.com>
2025-04-28 19:57:55 -0400

    update storage keys/enums

--- src/storage/constants.ts
+++ src/storage/constants.ts
@@ -27,14 +27,17 @@ export enum WorkspaceStorageKeys {
   /** What (Schema) URI was chosen first to diff against? */
   DIFF_BASE_URI = "diffs.selectedResource",
 
-  /** URI annotation facility, setURIMetadata() and the like.*/
+  /** Map of URIs to their associated metadata record/objects. */
   URI_METADATA = "uriMetadata",
 }
 
 /** Keys for use within URI Metadata dicts */
 export enum UriMetadataKeys {
-  SCHEMA_REGISTRY_ID = "schemaRegistryId",
-  SCHEMA_SUBJECT = "schemaSubject",
+  CCLOUD_ORG_ID = "ccloudOrgId",
+  CCLOUD_PROVIDER = "ccloudProvider",
+  CCLOUD_REGION = "ccloudRegion",
+  COMPUTE_POOL_ID = "computePoolId",
+  ENVIRONMENT_ID = "environmentId",
 }
 
 export enum SecretStorageKeys {
