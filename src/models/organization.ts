f9da133e68938afa334ec83afb32fa366301d988
Rohit Sanjay <rsanjay@confluent.io>
2025-04-28 10:38:18 -0700

    fix test

--- src/models/organization.ts
+++ src/models/organization.ts
@@ -1,7 +1,8 @@
 import { Data, type Require as Enforced } from "dataclass";
+import { OrganizationId } from "./resource";
 
 export class CCloudOrganization extends Data {
-  id!: Enforced<string>;
+  id!: Enforced<OrganizationId>;
   name!: Enforced<string>;
   current!: Enforced<boolean>;
   jit_enabled!: Enforced<boolean>;
