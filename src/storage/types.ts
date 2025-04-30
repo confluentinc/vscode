26e7b1e29bb31b7a02db6a1a8f47a26e2355117a
Dave Shoup <dave.shoup@gmail.com>
2025-04-30 14:33:17 -0400

    remove old types; use UriMetadata partial record type

--- src/storage/types.ts
+++ src/storage/types.ts
@@ -1,4 +1,7 @@
 import { UriMetadataKeys } from "./constants";
 
+/** Record that uses any {@link UriMetadataKeys} value for its keys.  */
+export type UriMetadata = Partial<Record<UriMetadataKeys, any>>;
+
 /** Map of stringified resource {@link Uri}s to their associated metadata objects. */
-export type UriMetadataMap = Map<string, Record<UriMetadataKeys, any>>;
+export type UriMetadataMap = Map<string, UriMetadata>;
