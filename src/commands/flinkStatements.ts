f3d0ae2217f14a452f9bb5e294d467300d760da8
James Robinson <james@relatable.dev>
2025-04-30 10:52:12 -0400

    Describe user local timezone to Flink when submitting new statements. (#1613)

--- src/commands/flinkStatements.ts
+++ src/commands/flinkStatements.ts
@@ -16,6 +16,7 @@ import {
   determineFlinkStatementName,
   FlinkSpecProperties,
   IFlinkStatementSubmitParameters,
+  localTimezoneOffset,
   submitFlinkStatement,
 } from "./utils/flinkStatements";
 
@@ -101,8 +102,7 @@ export async function submitFlinkStatementCommand(): Promise<void> {
     properties: new FlinkSpecProperties({
       currentDatabase,
       currentCatalog: computePool.environmentId,
-      // TODO get at the user's local timezone
-      // localTimezone: "GMT-04:00",
+      localTimezone: localTimezoneOffset(),
     }),
   };
 
---
+++ src/commands/utils/flinkStatements.test.ts
@@ -0,0 +1,37 @@
+import * as assert from "assert";
+import { localTimezoneOffset } from "./flinkStatements";
+
+describe("commands/utils/flinkStatements.ts localTimezoneOffset()", function () {
+  let originalTimezone: string | undefined;
+
+  beforeEach(() => {
+    originalTimezone = process.env.TZ;
+  });
+  afterEach(() => {
+    if (originalTimezone) {
+      process.env.TZ = originalTimezone;
+    } else {
+      delete process.env.TZ;
+    }
+  });
+
+  it("Should extract offset relative to GMT", async function () {
+    // Pin down timezone. May still be DST or not though based on the datetime.
+    // (Can only do this once; internals of Date will cache the timezone offset?)
+    process.env.TZ = "America/Los_Angeles";
+
+    // This should be GMT-0700 for EDT or GMT-0800 for EST
+    const offset = localTimezoneOffset();
+    assert.strictEqual(offset.startsWith("GMT"), true, "Offset should end with GMT");
+    // Fourth character should be + or -
+    assert.strictEqual(offset[3] === "+" || offset[3] === "-", true, "Offset should be + or -");
+
+    // Remainder will be either 0700 or 0800
+    assert.strictEqual(
+      offset.slice(4, 6) === "07" || offset.slice(4, 6) === "08",
+      true,
+      "Offset should be 0700 or 0800",
+    );
+    assert.strictEqual(offset.slice(6, 8) === "00", true, "Offset should be 00");
+  });
+});
