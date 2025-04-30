9bc2316ee72dca298cefd77c5930f76154e9e946
Dave Shoup <dave.shoup@gmail.com>
2025-04-30 12:31:30 -0400

    add org ID const for testing

--- tests/unit/testResources/organization.ts
+++ tests/unit/testResources/organization.ts
@@ -2,8 +2,9 @@ import { randomUUID } from "crypto";
 import { CCloudOrganization } from "../../../src/models/organization";
 import { OrganizationId } from "../../../src/models/resource";
 
+export const TEST_CCLOUD_ORGANIZATION_ID = randomUUID() as OrganizationId;
 export const TEST_CCLOUD_ORGANIZATION = CCloudOrganization.create({
-  id: randomUUID() as OrganizationId,
+  id: TEST_CCLOUD_ORGANIZATION_ID,
   current: true,
   name: "test-ccloud-org",
   jit_enabled: false,
