68ace2e00bb90caaa566ba8ae85dd3e7f6358589
Rohit Sanjay <rsanjay@confluent.io>
2025-05-01 11:30:45 -0700

    add new gulp command and write some docs

--- Gulpfile.js
+++ Gulpfile.js
@@ -34,6 +34,7 @@ const IS_WINDOWS = process.platform === "win32";
 
 export const ci = parallel(check, build, lint);
 export const test = series(clean, testBuild, testRun);
+export const e2e = series(clean, build, testBuild, e2eRun);
 export const liveTest = series(clean, build, testBuild);
 liveTest.description =
   "Rebuild the out/ directory after codebase or test suite changes for live breakpoint debugging.";
@@ -719,7 +720,19 @@ function coverage(options) {
 }
 
 export function functional(done) {
-  const result = spawnSync("npx", ["playwright", "test"], { stdio: "inherit", shell: IS_WINDOWS });
+  const result = spawnSync("npx", ["playwright", "test", "src"], {
+    stdio: "inherit",
+    shell: IS_WINDOWS,
+  });
+  if (result.error) throw result.error;
+  return done(result.status);
+}
+
+export function e2eRun(done) {
+  const result = spawnSync("npx", ["playwright", "test", "tests/e2e"], {
+    stdio: "inherit",
+    shell: IS_WINDOWS,
+  });
   if (result.error) throw result.error;
   return done(result.status);
 }
