6819890a36e92409d653548f6b7e424ab50fee6c
Dave Shoup <dave.shoup@gmail.com>
2025-05-01 18:58:11 -0400

    show notification or automatically update depending on update-configs

--- src/commands/documents.ts
+++ src/commands/documents.ts
@@ -4,9 +4,17 @@ import { uriMetadataSet } from "../emitters";
 import { getCurrentOrganization } from "../graphql/organizations";
 import { Logger } from "../logging";
 import { CCloudFlinkComputePool } from "../models/flinkComputePool";
-import { KafkaCluster } from "../models/kafkaCluster";
+import { CCloudKafkaCluster, KafkaCluster } from "../models/kafkaCluster";
 import { CCloudOrganization } from "../models/organization";
-import { showErrorNotificationWithButtons } from "../notifications";
+import {
+  showErrorNotificationWithButtons,
+  showInfoNotificationWithButtons,
+} from "../notifications";
+import {
+  UPDATE_DEFAULT_DATABASE_FROM_LENS,
+  UPDATE_DEFAULT_POOL_ID_FROM_LENS,
+} from "../preferences/constants";
+import { updateDefaultFlinkDatabaseId, updateDefaultFlinkPoolId } from "../preferences/updates";
 import { flinkComputePoolQuickPick } from "../quickpicks/flinkComputePools";
 import { flinkDatabaseQuickpick } from "../quickpicks/kafkaClusters";
 import { UriMetadataKeys } from "../storage/constants";
@@ -14,6 +22,9 @@ import { getResourceManager } from "../storage/resourceManager";
 import { UriMetadata } from "../storage/types";
 
 const logger = new Logger("commands.documents");
+
+/** Possible values for user settings controlling whether or not to update the default Flink resource IDs. */
+export type NeverAskAlways = "never" | "ask" | "always";
 
 export async function setCCloudOrgForUriCommand(uri?: Uri) {
   if (!(uri instanceof Uri)) {
@@ -82,6 +93,30 @@ export async function setCCloudComputePoolForUriCommand(uri?: Uri) {
   };
   await getResourceManager().setUriMetadata(uri, metadata);
   uriMetadataSet.fire(uri);
+
+  // check user settings to see if we should ask to update the default compute pool ID or
+  // just do it automatically. (if set to "never" or any other value, we won't ask and won't do it)
+  const shouldUpdateDefaultPoolId: NeverAskAlways = workspace
+    .getConfiguration()
+    .get(UPDATE_DEFAULT_POOL_ID_FROM_LENS, "ask");
+  if (shouldUpdateDefaultPoolId === "ask") {
+    await showInfoNotificationWithButtons(
+      `Set default Flink compute pool to "${pool.id}" ("${pool.name}")?`,
+      {
+        Yes: async () => {
+          await updateDefaultFlinkPoolId(pool);
+        },
+        "Change Notification Settings": () => {
+          void commands.executeCommand(
+            "workbench.action.openSettings",
+            `@id:${UPDATE_DEFAULT_POOL_ID_FROM_LENS}`,
+          );
+        },
+      },
+    );
+  } else if (shouldUpdateDefaultPoolId === "always") {
+    await updateDefaultFlinkPoolId(pool);
+  }
 }
 
 export async function setCCloudCatalogDatabaseForUriCommand(
@@ -127,6 +162,30 @@ export async function setCCloudCatalogDatabaseForUriCommand(
   };
   await getResourceManager().setUriMetadata(uri, metadata);
   uriMetadataSet.fire(uri);
+
+  // check user settings to see if we should ask to update the default compute pool ID or
+  // just do it automatically. (if set to "never" or any other value, we won't ask and won't do it)
+  const shouldUpdateDefaultDatabaseId: NeverAskAlways = workspace
+    .getConfiguration()
+    .get(UPDATE_DEFAULT_DATABASE_FROM_LENS, "ask");
+  if (shouldUpdateDefaultDatabaseId === "ask") {
+    await showInfoNotificationWithButtons(
+      `Set default Flink database to "${database.id}" ("${database.name}")?`,
+      {
+        Yes: async () => {
+          await updateDefaultFlinkDatabaseId(database as CCloudKafkaCluster);
+        },
+        "Change Notification Settings": () => {
+          void commands.executeCommand(
+            "workbench.action.openSettings",
+            `@id:${UPDATE_DEFAULT_DATABASE_FROM_LENS}`,
+          );
+        },
+      },
+    );
+  } else if (shouldUpdateDefaultDatabaseId === "always") {
+    await updateDefaultFlinkDatabaseId(database as CCloudKafkaCluster);
+  }
 }
 
 export function registerDocumentCommands(): Disposable[] {
