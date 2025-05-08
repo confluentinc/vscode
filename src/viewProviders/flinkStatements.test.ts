4d98bdef9f5b37e4d0b246ae786fba8b9ba59281
James Robinson <jrobinson@confluent.io>
2025-05-08 09:58:35 -0400

    Remove redundant word

--- src/viewProviders/flinkStatements.test.ts
+++ src/viewProviders/flinkStatements.test.ts
@@ -179,7 +179,7 @@ describe("FlinkStatementsViewProvider", () => {
       assert.strictEqual(computePool, null);
     });
 
-    it("returns returns null if resource set to an environment", () => {
+    it("returns null if resource set to an environment", () => {
       viewProvider["resource"] = TEST_CCLOUD_ENVIRONMENT;
       const computePool = viewProvider.computePool;
       assert.strictEqual(computePool, null);
