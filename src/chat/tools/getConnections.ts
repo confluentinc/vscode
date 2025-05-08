0089607617b8eecb5dc84a95421aec8c94d6578b
Dave Shoup <dave.shoup@gmail.com>
2025-05-08 15:05:03 -0400

    connection summarizing formatting adjustments

--- src/chat/tools/getConnections.ts
+++ src/chat/tools/getConnections.ts
@@ -6,7 +6,9 @@ import {
   LanguageModelToolCallPart,
   LanguageModelToolInvocationOptions,
   LanguageModelToolResult,
+  MarkdownString,
 } from "vscode";
+import { CCLOUD_SIGN_IN_BUTTON_LABEL } from "../../authn/constants";
 import {
   Connection,
   ConnectionsList,
@@ -15,6 +17,7 @@ import {
 } from "../../clients/sidecar";
 import { Logger } from "../../logging";
 import { getSidecar } from "../../sidecar";
+import { titleCase } from "../../utils";
 import { summarizeConnection } from "../summarizers/connections";
 import { BaseLanguageModelTool, TextOnlyToolResultPart } from "./base";
 
@@ -38,17 +41,56 @@ export class GetConnectionsTool extends BaseLanguageModelTool<IGetConnectionsPar
     // use the Connections API to get the list of connections
     const sidecar = await getSidecar();
     const client: ConnectionsResourceApi = sidecar.getConnectionsResourceApi();
-    const connections: ConnectionsList = await client.gatewayV1ConnectionsGet();
+    const connectionsList: ConnectionsList = await client.gatewayV1ConnectionsGet();
 
+    // ...then handle all of the text-formatting to tell the model only the information that's needed
     const connectionStrings: LanguageModelTextPart[] = [];
-    connections.data.forEach((connection: Connection) => {
-      // filter connections by type
-      if (params.connectionType && connection.spec.type !== params.connectionType) {
-        return;
+    if (connectionsList.data.length) {
+      // group by type to keep summaries concise
+      const connectionsMap: Map<ConnectionType, Connection[]> = new Map();
+      connectionsList.data.forEach((connection: Connection) => {
+        const type: ConnectionType = connection.spec.type!;
+        if (!connectionsMap.has(type)) {
+          connectionsMap.set(type, []);
+        }
+        connectionsMap.get(type)?.push(connection);
+      });
+
+      // give each connection type its own header
+      for (const [type, connections] of connectionsMap.entries()) {
+        if (params.connectionType && type !== params.connectionType) {
+          // if the model is filtering by connection type, skip any other types
+          continue;
+        }
+        const plural = connections.length === 1 ? "" : "s";
+        let connectionGroupSummary = new MarkdownString(
+          `## ${titleCase(type)} Connection${plural} (${connections.length})`,
+        );
+        // then summarize each connection
+        connections.forEach((connection: Connection) => {
+          const connectionSummary: string = summarizeConnection(connection);
+          connectionGroupSummary = connectionGroupSummary.appendMarkdown(
+            `\n\n${connectionSummary}`,
+          );
+        });
+        connectionStrings.push(new LanguageModelTextPart(connectionGroupSummary.value));
       }
-      const connectionSummary: string = summarizeConnection(connection);
-      connectionStrings.push(new LanguageModelTextPart(connectionSummary));
-    });
+    } else {
+      // show placeholders for signing in to CCloud, starting local resources, or connecting directly
+      const ccloudButton = `[${CCLOUD_SIGN_IN_BUTTON_LABEL}](command:confluent.connections.ccloud.signIn)`;
+      const localResourcesButton = `[Start Local Resources](command:confluent.docker.startLocalResources)`;
+      const directConnectionButton = `[Connect Directly](command:confluent.connections.direct)`;
+
+      const noConnectionsMarkdown = new MarkdownString(`No connections found.`)
+        .appendMarkdown(`\n\n- Connect to Confluent Cloud by signing in:\n\n${ccloudButton}`)
+        .appendMarkdown(
+          `\n\n- Start local resources (Kafka, Schema Registry, etc.) by running:\n\n${localResourcesButton}`,
+        )
+        .appendMarkdown(
+          `\n\n- Connect directly to a Kafka cluster or Schema registry with:\n\n${directConnectionButton}`,
+        );
+      connectionStrings.push(new LanguageModelTextPart(noConnectionsMarkdown.value));
+    }
 
     // TODO(shoup): remove later
     logger.debug(`connectionStrings:\n\n${connectionStrings.map((part) => part.value).join("\n")}`);
