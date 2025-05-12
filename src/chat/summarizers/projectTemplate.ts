b40f551ee6e08cb159b88afd1ea4dc6ef2c076f5
Dave Shoup <dave.shoup@gmail.com>
2025-05-12 14:41:05 -0400

    add help text for required fields

--- src/chat/summarizers/projectTemplate.ts
+++ src/chat/summarizers/projectTemplate.ts
@@ -1,5 +1,9 @@
 import { MarkdownString } from "vscode";
-import { ScaffoldV1Template, ScaffoldV1TemplateSpec } from "../../clients/scaffoldingService";
+import {
+  ScaffoldV1Template,
+  ScaffoldV1TemplateOption,
+  ScaffoldV1TemplateSpec,
+} from "../../clients/scaffoldingService";
 
 export function summarizeProjectTemplate(template: ScaffoldV1Template): string {
   const spec: ScaffoldV1TemplateSpec = template.spec!;
@@ -11,8 +15,23 @@ export function summarizeProjectTemplate(template: ScaffoldV1Template): string {
 }
 
 export function summarizeTemplateOptions(template: ScaffoldV1Template): string {
-  let summary = new MarkdownString().appendCodeblock(
-    JSON.stringify(template.spec!.options, null, 2),
-  );
+  const options: Record<string, ScaffoldV1TemplateOption> | undefined = template.spec!.options;
+  if (options === undefined) {
+    return "(No options available.)";
+  }
+
+  let summary = new MarkdownString().appendCodeblock(JSON.stringify(options, null, 2), "json");
+  // iterate through each field and mark it as required if its min_length is greater than 0
+  const requiredFields = [];
+  for (const [field, properties] of Object.entries(options)) {
+    if (properties.min_length && properties.min_length > 0) {
+      requiredFields.push(field);
+    }
+  }
+  if (requiredFields.length > 0) {
+    summary.appendMarkdown(
+      `\n\n**Required fields:** ${requiredFields.map((field) => `\`${field}\``).join(", ")}`,
+    );
+  }
   return summary.value;
 }
