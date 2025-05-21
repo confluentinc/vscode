0433b3ce7c7bdf4301dfa4fe77b23b5120d1ff74
Dave Shoup <dave.shoup@gmail.com>
2025-05-21 10:14:15 -0400

    update org quickpick to use QuickPickItemWithValue

--- src/quickpicks/organizations.ts
+++ src/quickpicks/organizations.ts
@@ -1,34 +1,39 @@
-import * as vscode from "vscode";
+import { ThemeIcon, window } from "vscode";
 import { IconNames } from "../constants";
 import { getOrganizations } from "../graphql/organizations";
 import { CCloudOrganization } from "../models/organization";
+import { QuickPickItemWithValue } from "./types";
 
+/**
+ * Displays a quickpick for selecting a CCloud organization.
+ *
+ * @returns The selected {@link CCloudOrganization} or `undefined` if no selection was made.
+ */
 export async function organizationQuickPick(): Promise<CCloudOrganization | undefined> {
   const ccloudOrganizations: CCloudOrganization[] = await getOrganizations();
   if (ccloudOrganizations.length === 0) {
-    vscode.window.showInformationMessage("No organizations available.");
-    return undefined;
+    window.showInformationMessage("No organizations available.");
+    return;
   }
 
-  let organizationItems: vscode.QuickPickItem[] = [];
+  let organizationItems: QuickPickItemWithValue<CCloudOrganization>[] = [];
   ccloudOrganizations.sort((a, b) => a.name.localeCompare(b.name));
   ccloudOrganizations.forEach((organization: CCloudOrganization) => {
     organizationItems.push({
       label: organization.name,
       description: organization.id,
-      iconPath: new vscode.ThemeIcon(
+      iconPath: new ThemeIcon(
         organization.current ? IconNames.CURRENT_RESOURCE : IconNames.ORGANIZATION,
       ),
+      value: organization,
     });
   });
 
-  const chosenOrganizationItem: vscode.QuickPickItem | undefined =
-    await vscode.window.showQuickPick(organizationItems, {
+  const chosenOrganizationItem: QuickPickItemWithValue<CCloudOrganization> | undefined =
+    await window.showQuickPick(organizationItems, {
       placeHolder: "Select an organization",
       ignoreFocusOut: true,
     });
 
-  return chosenOrganizationItem
-    ? ccloudOrganizations.find((org) => org.id === chosenOrganizationItem.description)
-    : undefined;
+  return chosenOrganizationItem?.value;
 }
