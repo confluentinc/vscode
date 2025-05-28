814e029f565dc39548f5bc6316ecb4d4dc711520
Dave Shoup <dave.shoup@gmail.com>
2025-05-28 21:45:15 +0000

    MINOR: create single log output channel for Flink SQL language server (#1919)

---
+++ src/flinkSql/logging.ts
@@ -0,0 +1,16 @@
+import { LogOutputChannel, window } from "vscode";
+
+let languageServerOutputChannel: LogOutputChannel | undefined;
+
+/** Get the {@link LogOutputChannel} for the Flink SQL language server, creating it if it doesn't already exist. */
+export function getFlinkSQLLanguageServerOutputChannel(): LogOutputChannel {
+  if (!languageServerOutputChannel) {
+    languageServerOutputChannel = window.createOutputChannel(
+      "Confluent Flink SQL Language Server",
+      {
+        log: true,
+      },
+    );
+  }
+  return languageServerOutputChannel;
+}
