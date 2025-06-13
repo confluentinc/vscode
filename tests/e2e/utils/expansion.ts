057da44d71a0d53a5d1f0fdba9cded883a86c2cd
Dave Shoup <dave.shoup@gmail.com>
2025-06-13 19:50:54 -0400

    add util for handling collapsible states / aria-expanded

--- tests/e2e/utils/expansion.ts
+++ tests/e2e/utils/expansion.ts
@@ -1,8 +1,21 @@
-import { Locator, expect } from "@playwright/test";
+import { Locator } from "@playwright/test";
 
-/** Check if an element with `aria-expanded` is currently expanded. */
+export const EXPANDABLE_ATTRIBUTE = "aria-expanded";
+
+/** Whether an element has the `aria-expanded` attribute, indicating it can be expanded or collapsed.*/
+export async function isExpandable(locator: Locator): Promise<boolean> {
+  const hasAriaExpanded: string | null = await locator.getAttribute(EXPANDABLE_ATTRIBUTE);
+  return hasAriaExpanded !== null;
+}
+
+/** Whether or not an element with `aria-expanded` is currently expanded. */
 export async function isExpanded(locator: Locator): Promise<boolean> {
-  const ariaExpanded: string | null = await locator.getAttribute("aria-expanded");
+  if (!(await isExpandable(locator))) {
+    throw new Error(
+      `Element "${await locator.textContent()}" is not expandable, as it has no "${EXPANDABLE_ATTRIBUTE}" attribute.`,
+    );
+  }
+  const ariaExpanded: string | null = await locator.getAttribute(EXPANDABLE_ATTRIBUTE);
   return ariaExpanded === "true";
 }
 
@@ -10,7 +23,10 @@ export async function isExpanded(locator: Locator): Promise<boolean> {
 export async function expand(locator: Locator): Promise<void> {
   if (!(await isExpanded(locator))) {
     await locator.click();
-    await expect(locator).toHaveAttribute("aria-expanded", "true");
+    // make sure the element is expanded after clicking
+    if (!(await isExpanded(locator))) {
+      throw new Error(`Element "${await locator.textContent()}" could not be expanded.`);
+    }
   }
 }
 
@@ -18,6 +34,9 @@ export async function expand(locator: Locator): Promise<void> {
 export async function collapse(locator: Locator): Promise<void> {
   if (await isExpanded(locator)) {
     await locator.click();
-    await expect(locator).toHaveAttribute("aria-expanded", "false");
+    // make sure the element is collapsed after clicking
+    if (await isExpanded(locator)) {
+      throw new Error(`Element "${await locator.textContent()}" could not be collapsed.`);
+    }
   }
 }
