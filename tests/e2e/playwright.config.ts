28e41db7f1d872da1140764d26d1d5597a6a08bd
Dave Shoup <dave.shoup@gmail.com>
2025-06-17 09:25:59 -0400

    initial updates to fix test startup

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "@playwright/test";
 import { configDotenv } from "dotenv";
-import { globSync } from "glob";
 import path from "path";
 import { fileURLToPath } from "url";
-import { VSCodeTestOptions, VSCodeWorkerOptions } from "vscode-test-playwright";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,10 +10,9 @@ configDotenv({
   path: path.join(__dirname, "..", "..", ".env"),
 });
 
-const vsix: string = globSync(path.resolve(__dirname, "..", "..", "out", "*.vsix")).at(0) as string;
 const vscodeVersion = process.env.VSCODE_VERSION ?? "stable";
 
-export default defineConfig<VSCodeTestOptions, VSCodeWorkerOptions>({
+export default defineConfig({
   testDir: path.join(__dirname, "specs"),
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -26,19 +23,13 @@ export default defineConfig<VSCodeTestOptions, VSCodeWorkerOptions>({
   },
   reporter: "html",
   use: {
-    // We run the tests against the bundled VSIX file.
-    extensions: [vsix],
-    // The test VS Code window instance is launched with the `out` directory
-    // opened. This lets us load fixture files (the fixtures are copied over in the Gulp task `e2e`)
-    // Make sure to run `gulp bundle` if you changed the extension `src` code.
-    baseDir: path.join(__dirname, "..", "..", "out"),
-    // Enables Playwright tracing to help with debugging. Super useful!
-    vscodeTrace: "on",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
   projects: [
     {
       name: vscodeVersion,
-      use: { vscodeVersion },
     },
   ],
 });
