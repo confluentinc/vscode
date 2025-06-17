6bd9356271df9061051fda30fe896355b247152f
Dave Shoup <dave.shoup@gmail.com>
2025-06-16 20:23:06 -0400

    fix topics view label; add nav icon methods

--- tests/e2e/objects/views/TopicsView.ts
+++ tests/e2e/objects/views/TopicsView.ts
@@ -8,12 +8,43 @@ export class TopicsView extends View {
   // private to use `TopicsView.from(page)` instead of `new TopicsView(page)` since we aren't
   // creating a "new" topics view object, just accessing it from the existing page
   private constructor(page: Page) {
-    super(page, /Topics - .* \| .* Section/);
+    super(page, /Topics.*Section/);
   }
 
   /** Get the Topics view for the given {@link Page page} */
   static from(page: Page): TopicsView {
     return new TopicsView(page);
+  }
+
+  /** Click the "Search" nav action in the view title area. */
+  async clickSearch(): Promise<void> {
+    await this.clickNavAction("Search");
+  }
+
+  /** Click the "Create Topic" nav action in the view title area. */
+  async clickCreateTopic(): Promise<void> {
+    await this.clickNavAction("Create Topic");
+  }
+
+  /**
+   * Click the "Select Kafka Cluster" nav action in the view title area, which will show a
+   * quickpick with a list of Kafka cluster items.
+   */
+  async clickSelectKafkaCluster(): Promise<void> {
+    await this.clickNavAction("Select Kafka Cluster");
+  }
+
+  /** Click the "Refresh" nav action in the view title area. */
+  async clickRefresh(): Promise<void> {
+    await this.clickNavAction("Refresh");
+  }
+
+  /**
+   * Click the "More Actions..." nav action in the view title area, which will open a right-click
+   * context menu.
+   */
+  async clickMoreActions(): Promise<void> {
+    await this.clickNavAction("More Actions...");
   }
 
   /**
