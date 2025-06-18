1458209bcfadbf345900b182afc31e073eec143b
Dave Shoup <dave.shoup@gmail.com>
2025-06-18 13:57:11 -0400

    simplify quickpickitem methods

--- tests/e2e/objects/quickInputs/QuickpickItem.ts
+++ tests/e2e/objects/quickInputs/QuickpickItem.ts
@@ -2,30 +2,32 @@ import { Locator, Page } from "@playwright/test";
 
 /** Represents a single item in a VS Code quickpick. */
 export class QuickpickItem {
-  private readonly page: Page;
-  readonly locator: Locator;
-
-  constructor(page: Page, element: Locator) {
-    this.page = page;
-    this.locator = element;
-  }
-
-  async click(): Promise<void> {
-    await this.locator.click();
-  }
-
-  async isVisible(): Promise<boolean> {
-    return await this.locator.isVisible();
-  }
+  constructor(
+    public readonly page: Page,
+    public readonly locator: Locator,
+  ) {}
 
   /** Checks if this item is currently enabled (not disabled). */
   async isEnabled(): Promise<boolean> {
-    const disabled = await this.locator.getAttribute("aria-disabled");
+    const disabled: string | null = await this.locator.getAttribute("aria-disabled");
     return disabled !== "true";
   }
 
   get icon(): Locator {
     return this.locator.locator(".quick-input-list-icon");
+  }
+
+  /**
+   * Parse the codicon class name for the icon element.
+   * For example, if the class is `codicon-account`, this will return `account`.
+   */
+  async iconId(): Promise<string> {
+    const className: string | null = await this.icon.getAttribute("class");
+    if (!className) {
+      return "";
+    }
+    const match: RegExpMatchArray | null = className.match(/codicon-(\w+)/);
+    return match ? match[1] : "";
   }
 
   get label(): Locator {
