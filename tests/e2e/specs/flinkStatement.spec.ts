858c28ea02a052d6ebc3fd57de7a73654a2fcc48
Dave Shoup <dave.shoup@gmail.com>
2025-07-03 18:42:46 -0400

    skip Flink tests until they're updated/fixed

--- tests/e2e/specs/flinkStatement.spec.ts
+++ tests/e2e/specs/flinkStatement.spec.ts
@@ -9,57 +9,66 @@ import {
   verifyStatementStatus,
 } from "./utils/flinkStatement";
 
-test.describe("Flink statements and statement results viewer", () => {
-  let webview: FrameLocator;
-
-  test.beforeEach(async ({ page, electronApp }) => {
-    // Open the extension
-    await openConfluentExtension(page);
-
-    // Login to Confluent Cloud
-    await login(page, electronApp, process.env.E2E_USERNAME!, process.env.E2E_PASSWORD!);
-  });
-
-  test.afterEach(async () => {
-    // Stop the statement
-    await stopStatement(webview);
-  });
-
-  const testCases = [
-    {
-      name: "SELECT statement",
-      fileName: "select.flink.sql",
-      eventualExpectedStatus: "RUNNING",
-      expectedStats: "Showing 1..100 of 200 results (total: 200).",
+test.describe.fixme(
+  "Flink statements and statement results viewer",
+  {
+    annotation: {
+      type: "issue",
+      description: "https://github.com/confluentinc/vscode/issues/2112",
     },
-    {
-      name: "EXPLAIN statement",
-      fileName: "explain.flink.sql",
-      eventualExpectedStatus: "COMPLETED",
-      expectedStats: "Showing 1..1 of 1 results (total: 1).",
-    },
-    {
-      name: "DESCRIBE statement",
-      fileName: "describe.flink.sql",
-      eventualExpectedStatus: "COMPLETED",
-      expectedStats: "Showing 1..5 of 5 results (total: 5).",
-    },
-  ];
+  },
+  () => {
+    let webview: FrameLocator;
 
-  for (const testCase of testCases) {
-    test(`should submit Flink statement - ${testCase.name}`, async ({ page }) => {
-      // Submit the statement
-      await submitFlinkStatement(page, testCase.fileName);
+    test.beforeEach(async ({ page, electronApp }) => {
+      // Open the extension
+      await openConfluentExtension(page);
 
-      webview = page.locator("iframe").contentFrame().locator("iframe").contentFrame();
-
-      // Wait for statement to run and verify status
-      await verifyStatementStatus(webview, testCase.eventualExpectedStatus);
-
-      // Verify results stats
-      await verifyResultsStats(webview, testCase.expectedStats);
-
-      // TODO: Verify results are correct for each test case
+      // Login to Confluent Cloud
+      await login(page, electronApp, process.env.E2E_USERNAME!, process.env.E2E_PASSWORD!);
     });
-  }
-});
+
+    test.afterEach(async () => {
+      // Stop the statement
+      await stopStatement(webview);
+    });
+
+    const testCases = [
+      {
+        name: "SELECT statement",
+        fileName: "select.flink.sql",
+        eventualExpectedStatus: "RUNNING",
+        expectedStats: "Showing 1..100 of 200 results (total: 200).",
+      },
+      {
+        name: "EXPLAIN statement",
+        fileName: "explain.flink.sql",
+        eventualExpectedStatus: "COMPLETED",
+        expectedStats: "Showing 1..1 of 1 results (total: 1).",
+      },
+      {
+        name: "DESCRIBE statement",
+        fileName: "describe.flink.sql",
+        eventualExpectedStatus: "COMPLETED",
+        expectedStats: "Showing 1..5 of 5 results (total: 5).",
+      },
+    ];
+
+    for (const testCase of testCases) {
+      test(`should submit Flink statement - ${testCase.name}`, async ({ page }) => {
+        // Submit the statement
+        await submitFlinkStatement(page, testCase.fileName);
+
+        webview = page.locator("iframe").contentFrame().locator("iframe").contentFrame();
+
+        // Wait for statement to run and verify status
+        await verifyStatementStatus(webview, testCase.eventualExpectedStatus);
+
+        // Verify results stats
+        await verifyResultsStats(webview, testCase.expectedStats);
+
+        // TODO: Verify results are correct for each test case
+      });
+    }
+  },
+);
