876b103e0a42a63eed335bbcb51c0a3d70974e52
Dave Shoup <dave.shoup@gmail.com>
2025-07-07 17:31:40 -0400

    add only other inline action available even though it isn't used yet

--- tests/e2e/objects/views/viewItems/SubjectItem.ts
+++ tests/e2e/objects/views/viewItems/SubjectItem.ts
@@ -1,6 +1,11 @@
 import { ViewItem } from "./ViewItem";
 
 export class SubjectItem extends ViewItem {
+  /** Click the "View Latest Schema" inline action. */
+  async clickViewLatestSchema(): Promise<void> {
+    await this.clickInlineAction("View Latest Schema");
+  }
+
   /** Click the "Evolve Latest Schema" inline action to open the schema evolution workflow. */
   async clickEvolveLatestSchema(): Promise<void> {
     await this.clickInlineAction("Evolve Latest Schema");
