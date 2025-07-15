41d8dee4d77a0db2d22b1f058b398a27a2f77f28
Dave Shoup <dave.shoup@gmail.com>
2025-07-15 14:24:41 +0000

    chore(CI): set up pipeline for running E2E tests manually (#2163)

--- tests/e2e/tags.ts
+++ tests/e2e/tags.ts
@@ -1,5 +1,11 @@
 /** {@see https://playwright.dev/docs/test-annotations#tag-tests} */
 export enum Tag {
   Smoke = "@smoke",
-  CriticalUserJourney = "@critical-user-journey",
+
+  /** Tests that require a CCloud connection to be set up and authenticated */
+  CCloud = "@ccloud",
+  /** Tests that require a direct connection to be set up */
+  Direct = "@direct",
+  /** Tests that require a local connection to be set up */
+  Local = "@local", // unused until https://github.com/confluentinc/vscode/issues/2140 is done
 }
