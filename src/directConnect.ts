8dbdbdc78c2ad8804714d0d895c145a36173f70f
Noel Cothren <ncothren@confluent.io>
2025-07-18 14:27:57 -0600

    don't await user response to close form

--- src/directConnect.ts
+++ src/directConnect.ts
@@ -109,12 +109,12 @@ export function openDirectConnectionForm(connection: CustomConnectionSpec | null
     } else {
       result.success = true;
       // save and close the form
+      directConnectForm.dispose();
       await showInfoNotificationWithButtons(`New Connection Created`, {
         "Edit Connection": () => {
           commands.executeCommand("confluent.connections.direct.edit", newConnection.id);
         },
       });
-      directConnectForm.dispose();
     }
     return result;
   }
