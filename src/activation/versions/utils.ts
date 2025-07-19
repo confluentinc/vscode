e9081ce93a6a26fd985995af435e83a7d9df21bd
Dave Shoup <dave.shoup@gmail.com>
2025-07-18 22:45:03 -0400

    remove unnecessary function and associated tests

--- src/activation/versions/utils.ts
+++
@@ -1,6 +0,0 @@
-import { SHOW_NEW_INSTALL_OR_UPDATE_NOTIFICATIONS } from "../../extensionSettings/constants";
-
-/** Check if the user has enabled notifications for new or updated extension activations. */
-export function canShowNewOrUpdatedExtensionNotifications(): boolean {
-  return SHOW_NEW_INSTALL_OR_UPDATE_NOTIFICATIONS.value ?? true;
-}
