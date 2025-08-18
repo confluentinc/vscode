6be088cba0510b837b780973b54d05633cd2b5d3
Stefan Sprenger <ssprenger@confluent.io>
2025-08-18 13:54:23 +0200

    fix: Don't clear diagnostics when saving documents
    We clear diagnostics in the listener for document change events. Prior to this change, we cleared diagnostics upon saving documents because saving a document generates a document change event.
    
    This change updates the event listener, so that we clear diagnostics only if a document change event updated the content of the document, which is not the case when saving it.
    
    Fixes #2350

--- src/flinkSql/flinkLanguageClientManager.ts
+++ src/flinkSql/flinkLanguageClientManager.ts
@@ -248,7 +248,11 @@ export class FlinkLanguageClientManager implements Disposable {
         }
         if (
           this.openFlinkSqlDocuments.has(uriString) &&
-          this.languageClient?.diagnostics?.has(event.document.uri)
+          this.languageClient?.diagnostics?.has(event.document.uri) &&
+          // Make sure the change updated the content of the document. Otherwise, clearing diagnostics
+          // won't make sense. We'll, for instance, see document change events without content changes
+          // when saving the document.
+          event.contentChanges.length > 0
         ) {
           this.clearDiagnostics(event.document.uri);
         } else {
