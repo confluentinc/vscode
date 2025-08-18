54855484b274738a437cc75cc3d0a064b4d7f2f7
Stefan Sprenger <128683+flippingbits@users.noreply.github.com>
2025-08-18 09:47:36 +0200

    [v1.5.x] Trigger diagnostics when opening document (#2348)
    * fix: Trigger diagnostics when opening document
    
    Upon opening a new document, send the `textDocument/didChange` event to the server
    to trigger diagnostics. This change can be reverted once the CCloud Flink Language Server
    has been updated to run diagnostics on the `textDocument/didOpen` event.
    
    Fixes #2343
    
    * chore: Improve function signature
    
    * chore: Argh, adding missing semicolon
    
    * chore: Incorporate reviewer feedback

--- src/flinkSql/flinkLanguageClientManager.ts
+++ src/flinkSql/flinkLanguageClientManager.ts
@@ -156,6 +156,30 @@ export class FlinkLanguageClientManager implements Disposable {
     }
   }
 
+  /**
+   * Simulates a document change to trigger diagnostics on the server side. Is needed to get diagnostics
+   * when opening a new document. Can be removed when the CCloud Flink Language Service has been updated.
+   * @param doc The document to simulate changes for.
+   * @returns A promise that resolves when the notification has been sent.
+   */
+  public async simulateDocumentChangeToTriggerDiagnostics(doc: TextDocument): Promise<void> {
+    if (!this.languageClient) {
+      logger.info("Can't simulate document change for non-existing language client.");
+      return;
+    }
+    await this.languageClient.sendNotification("textDocument/didChange", {
+      textDocument: {
+        uri: doc.uri.toString() || "",
+        version: doc.version,
+      },
+      contentChanges: [
+        {
+          text: doc.getText(),
+        },
+      ],
+    });
+  }
+
   private registerListeners(): void {
     // Listen for changes to metadata
     // Codelens compute pool affects connection, catalog/db will be sent as LSP workspace config
@@ -192,6 +216,7 @@ export class FlinkLanguageClientManager implements Disposable {
         ) {
           logger.trace("Active editor changed to Flink SQL file, initializing language client");
           await this.maybeStartLanguageClient(editor.document.uri);
+          await this.simulateDocumentChangeToTriggerDiagnostics(editor.document);
         }
       }),
     );
@@ -208,6 +233,7 @@ export class FlinkLanguageClientManager implements Disposable {
           } else {
             logger.trace("Initializing language client for changed active Flink SQL document");
             await this.maybeStartLanguageClient(doc.uri);
+            await this.simulateDocumentChangeToTriggerDiagnostics(doc);
           }
         }
       }),
@@ -546,7 +572,7 @@ export class FlinkLanguageClientManager implements Disposable {
             action: "client_initialized",
             compute_pool_id: computePoolId,
           });
-          this.notifyConfigChanged();
+          await this.notifyConfigChanged();
         }
       } catch (error) {
         let msg = "Error in maybeStartLanguageClient";
