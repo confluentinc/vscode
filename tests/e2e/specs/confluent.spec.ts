1c62b3d799b593f5a2ebfc2e84de9ecf945e78e0
Dave Shoup <dave.shoup@gmail.com>
2025-08-20 09:39:03 -0400

    TEMPORARY test the tests

--- tests/e2e/specs/confluent.spec.ts
+++ tests/e2e/specs/confluent.spec.ts
@@ -1,26 +1,28 @@
-import { stubAllDialogs } from "electron-playwright-helpers";
 import { test } from "../baseTest";
 import { Tag } from "../tags";
 import { openConfluentExtension } from "./utils/confluent";
 import { login } from "./utils/confluentCloud";
 
-test.describe(() => {
+test.describe.only(() => {
   test(
     "should load the extension properly",
     { tag: [Tag.Smoke] },
     async ({ page, electronApp }) => {
-      await stubAllDialogs(electronApp);
       await openConfluentExtension(page);
     },
   );
 
   test(
-    "sign in to confluent cloud",
-    { tag: [Tag.Smoke, Tag.CCloud] },
+    "should load the extension properly in another test",
+    { tag: [Tag.Smoke] },
     async ({ page, electronApp }) => {
       await openConfluentExtension(page);
-
-      await login(page, electronApp, process.env.E2E_USERNAME!, process.env.E2E_PASSWORD!);
     },
   );
+
+  test("sign in to confluent cloud", { tag: [Tag.CCloud] }, async ({ page, electronApp }) => {
+    await openConfluentExtension(page);
+
+    await login(page, electronApp, process.env.E2E_USERNAME!, process.env.E2E_PASSWORD!);
+  });
 });
