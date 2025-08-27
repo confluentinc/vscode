94c0b81094440ddad5428ccef6857b81f91d8d1c
Stefan Sprenger <ssprenger@confluent.io>
2025-08-27 18:59:29 +0200

    chore: Copilot feedback

--- src/webview/direct-connect-form.ts
+++ src/webview/direct-connect-form.ts
@@ -89,11 +89,7 @@ class DirectConnectFormViewModel extends ViewModel {
 
   // We must use a specific client ID suffix for connecting to WarpStream by K8s port-forwarding
   warpStreamPortForwardingEnabled = this.derive(() => {
-    if (this.spec()?.kafka_cluster?.client_id_suffix?.toString() === "ws_host_override=localhost") {
-      return true;
-    } else {
-      return false;
-    }
+    return this.spec()?.kafka_cluster?.client_id_suffix?.toString() === "ws_host_override=localhost";
   });
 
   // SSL enabled is true by default. If this is undefined it means the user never set/saved it
