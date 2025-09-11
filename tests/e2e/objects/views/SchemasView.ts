60f5a6276e08db5a1871979cdf8e098387471665
Dave Shoup <dave.shoup@gmail.com>
2025-09-11 10:57:31 -0400

    add utility methods for common view operations

--- tests/e2e/objects/views/SchemasView.ts
+++ tests/e2e/objects/views/SchemasView.ts
@@ -1,5 +1,13 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
+import { ConnectionType } from "../../utils/connections";
+import { Quickpick } from "../quickInputs/Quickpick";
+import { ResourcesView } from "./ResourcesView";
 import { View } from "./View";
+
+export enum SelectSchemaRegistry {
+  FromResourcesView = "Schema Registry action from the Resources view",
+  FromSchemasViewButton = "Schemas view nav action",
+}
 
 /**
  * Object representing the "Schemas"
@@ -64,5 +72,41 @@ export class SchemasView extends View {
   get schemaVersions(): Locator {
     // we don't use `this.subjects` because these are sibling elements to subjects in the DOM
     return this.body.locator("[role='treeitem'][aria-level='2']");
+  }
+
+  /**
+   * Once a connection is established, load schema subjects into the view using the specified
+   * {@link SelectSchemaRegistry entrypoint}.
+   *
+   * If using the {@link SelectSchemaRegistry.FromSchemasViewButton "Select Schema Registry" nav action}
+   * entrypoint, you can optionally provide a `registryLabel` to select a specific registry from the
+   * quickpick list. If not provided, the first registry in the list will be selected.
+   */
+  async loadSchemaSubjects(
+    connectionType: ConnectionType,
+    entrypoint: SelectSchemaRegistry,
+    registryLabel?: string | RegExp,
+  ): Promise<void> {
+    switch (entrypoint) {
+      case SelectSchemaRegistry.FromResourcesView: {
+        const resourcesView = new ResourcesView(this.page);
+        const registry = await resourcesView.getSchemaRegistry(connectionType);
+        await registry.click();
+        break;
+      }
+      case SelectSchemaRegistry.FromSchemasViewButton: {
+        await this.clickSelectSchemaRegistry();
+        const schemaRegistryQuickpick = new Quickpick(this.page);
+        await expect(schemaRegistryQuickpick.locator).toBeVisible();
+        await expect(schemaRegistryQuickpick.items).not.toHaveCount(0);
+        const registryItem = registryLabel
+          ? schemaRegistryQuickpick.items.filter({ hasText: registryLabel }).first()
+          : schemaRegistryQuickpick.items.first();
+        await registryItem.click();
+        break;
+      }
+      default:
+        throw new Error(`Unsupported entrypoint: ${entrypoint}`);
+    }
   }
 }
