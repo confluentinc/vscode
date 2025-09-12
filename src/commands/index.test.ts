eb5b3b718cf76b94f56d7372b0307966ea5c56b2
James Robinson <jrobinson@confluent.io>
2025-09-12 15:33:37 -0400

    Enforce that all registered commands must also be mentioned one way or another in contributes.menus.commandPalette.

--- src/commands/index.test.ts
+++ src/commands/index.test.ts
@@ -4,45 +4,55 @@ import { ConnectionType } from "../clients/sidecar";
 import { ConnectionId, IResourceBase } from "../models/resource";
 import { titleCase } from "../utils";
 
-describe("getCommandArgsContext", () => {
-  it("should handle no args", () => {
-    const result = getCommandArgsContext([]);
-
-    assert.deepStrictEqual(result, {});
-  });
-
-  it("should handle undefined first arg", () => {
-    const result = getCommandArgsContext([undefined]);
-
-    assert.deepStrictEqual(result, {});
-  });
-
-  it("should include 'resourceConnectionType' when the first arg implements IResourceBase", () => {
-    const fakeResource: IResourceBase = {
-      connectionId: "abc123" as ConnectionId,
-      connectionType: ConnectionType.Direct,
-    };
-
-    const result = getCommandArgsContext([fakeResource]);
-
-    assert.deepStrictEqual(result, {
-      resourceConnectionType: fakeResource.connectionType,
+describe("commands/index.ts", () => {
+  describe("registerCommandWithLogging", () => {
+    it("should throw if command name does not start with 'confluent.'", () => {
+      assert.throws(() => {
+        getCommandArgsContext(["not-a-valid-command"]);
+      }, /must start with "confluent."/);
     });
   });
 
-  for (const idField of RESOURCE_ID_FIELDS) {
-    const expectedField = `resource${titleCase(idField)}`;
+  describe("getCommandArgsContext", () => {
+    it("should handle no args", () => {
+      const result = getCommandArgsContext([]);
 
-    it(`should include '${expectedField}' if '${idField}' exists on the first arg`, () => {
-      const fakeResource = {
-        [idField]: "abc123",
+      assert.deepStrictEqual(result, {});
+    });
+
+    it("should handle undefined first arg", () => {
+      const result = getCommandArgsContext([undefined]);
+
+      assert.deepStrictEqual(result, {});
+    });
+
+    it("should include 'resourceConnectionType' when the first arg implements IResourceBase", () => {
+      const fakeResource: IResourceBase = {
+        connectionId: "abc123" as ConnectionId,
+        connectionType: ConnectionType.Direct,
       };
 
       const result = getCommandArgsContext([fakeResource]);
 
       assert.deepStrictEqual(result, {
-        [expectedField]: fakeResource[idField],
+        resourceConnectionType: fakeResource.connectionType,
       });
     });
-  }
+
+    for (const idField of RESOURCE_ID_FIELDS) {
+      const expectedField = `resource${titleCase(idField)}`;
+
+      it(`should include '${expectedField}' if '${idField}' exists on the first arg`, () => {
+        const fakeResource = {
+          [idField]: "abc123",
+        };
+
+        const result = getCommandArgsContext([fakeResource]);
+
+        assert.deepStrictEqual(result, {
+          [expectedField]: fakeResource[idField],
+        });
+      });
+    }
+  });
 });
