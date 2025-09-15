9d97e72296b8690fa365991b22b8363f354ebf5e
Lucia Cerchie <luciacerchie@gmail.com>
2025-09-15 14:09:19 -0700

    (UDFs) Artifacts: add logic to catch large files and notify user  (#2518)
    * add logic to catch large files

--- src/commands/flinkArtifacts.ts
+++ src/commands/flinkArtifacts.ts
@@ -39,7 +39,7 @@ export async function uploadArtifactCommand(): Promise<void> {
 
     const uploadUrl = await getPresignedUploadUrl(request);
 
-    if (!uploadUrl.upload_id) {
+    if (!uploadUrl) {
       throw new Error("Upload ID is missing from the presigned URL response.");
     }
 
