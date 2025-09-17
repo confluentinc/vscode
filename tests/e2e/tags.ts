bfcd184f357f61588a5bccdf13daf15c18d7c8ec
Dave Shoup <dave.shoup@gmail.com>
2025-09-17 08:53:47 -0400

    add setupLocalConnection util and use in existing tests' dimensions

--- tests/e2e/tags.ts
+++ tests/e2e/tags.ts
@@ -7,5 +7,5 @@ export enum Tag {
   /** Tests that require a direct connection to be set up */
   Direct = "@direct",
   /** Tests that require a local connection to be set up */
-  Local = "@local", // unused until https://github.com/confluentinc/vscode/issues/2140 is done
+  Local = "@local",
 }
