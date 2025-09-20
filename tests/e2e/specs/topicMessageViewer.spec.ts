64bf432bc33ab876a8423ba31af9f5b9de589702
Dave Shoup <dave.shoup@gmail.com>
2025-09-19 23:32:26 -0400

    update fixture use and create+delete topic

--- tests/e2e/specs/topicMessageViewer.spec.ts
+++ tests/e2e/specs/topicMessageViewer.spec.ts
@@ -1,7 +1,11 @@
 import { expect } from "@playwright/test";
 import { test } from "../baseTest";
 import { ConnectionType } from "../connectionTypes";
-import { SelectKafkaCluster, TopicsView } from "../objects/views/TopicsView";
+import {
+  DEFAULT_CCLOUD_TOPIC_REPLICATION_FACTOR,
+  SelectKafkaCluster,
+  TopicsView,
+} from "../objects/views/TopicsView";
 import { TopicItem } from "../objects/views/viewItems/TopicItem";
 import { MessageViewerWebview } from "../objects/webviews/MessageViewerWebview";
 import { Tag } from "../tags";
@@ -12,28 +16,35 @@ import { Tag } from "../tags";
  *
  * Test flow:
  * 1. Set up connection (CCloud, Direct, or Local)
- * 2. Select a Kafka cluster with topics
+ * 2. Select a Kafka cluster
  *    a. Pick from the Resources view, or
  *    b. Pick from the Topics view nav action
- * 3. Topics view should have at least one topic item listed
+ * 3. Create a topic
  * 4. Click on the `confluent-new-message` icon (envelope with magnifying glass) to open the topic
  *    message viewer
  * 5. View should open with basic webview components, even if messages aren't (yet) available
  */
 
 test.describe("Topics Listing & Message Viewer", () => {
+  let topicName: string = "e2e-topic-message-viewer";
+
+  test.afterEach(async ({ page }) => {
+    const topicView = new TopicsView(page);
+    await topicView.deleteTopic(topicName);
+  });
+
   // test dimensions:
-  const connectionTypes: Array<[ConnectionType, Tag]> = [
-    [ConnectionType.Ccloud, Tag.CCloud],
-    [ConnectionType.Direct, Tag.Direct],
-    [ConnectionType.Local, Tag.Local],
+  const connectionTypes: Array<[ConnectionType, Tag, number]> = [
+    [ConnectionType.Ccloud, Tag.CCloud, DEFAULT_CCLOUD_TOPIC_REPLICATION_FACTOR],
+    [ConnectionType.Direct, Tag.Direct, DEFAULT_CCLOUD_TOPIC_REPLICATION_FACTOR],
+    [ConnectionType.Local, Tag.Local, 1],
   ];
   const entrypoints = [
     SelectKafkaCluster.FromResourcesView,
     SelectKafkaCluster.FromTopicsViewButton,
   ];
 
-  for (const [connectionType, connectionTag] of connectionTypes) {
+  for (const [connectionType, connectionTag, replicationFactor] of connectionTypes) {
     test.describe(`${connectionType} connection`, { tag: [connectionTag] }, () => {
       // tell the `setupConnection` fixture which connection type to create
       test.use({ connectionType });
@@ -41,15 +52,24 @@ test.describe("Topics Listing & Message Viewer", () => {
       for (const entrypoint of entrypoints) {
         test(`should select a Kafka cluster from the ${entrypoint}, list topics, and open message viewer`, async ({
           page,
-          setupConnection,
+          connectionItem,
         }) => {
+          // ensure connection has resources available to work with
+          await expect(connectionItem.locator).toHaveAttribute("aria-expanded", "true");
+
+          // create a new topic
           const topicsView = new TopicsView(page);
           await topicsView.loadTopics(connectionType, entrypoint);
+          await topicsView.createTopic(topicName, 1, replicationFactor);
 
-          // now the Topics view should show at least one topic item
-          await expect(topicsView.topics).not.toHaveCount(0);
-          const firstTopic = new TopicItem(page, topicsView.topics.first());
-          const messageViewer: MessageViewerWebview = await firstTopic.clickViewMessages();
+          // verify it shows up in the Topics view
+          let targetTopic = topicsView.topicsWithoutSchemas.filter({ hasText: topicName });
+          await targetTopic.scrollIntoViewIfNeeded();
+          await expect(targetTopic).toBeVisible();
+
+          // open the message viewer for the topic
+          const topic = new TopicItem(page, targetTopic);
+          const messageViewer: MessageViewerWebview = await topic.clickViewMessages();
 
           // the message viewer webview should now be visible in the editor area
           await expect(messageViewer.messageViewerSettings).toBeVisible();
