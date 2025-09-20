47ef61b213d2336a97fa88a8aba3c0591581c269
Dave Shoup <dave.shoup@gmail.com>
2025-09-19 22:33:08 -0400

    block until notifications resolve

--- tests/e2e/objects/views/viewItems/KafkaClusterItem.ts
+++ tests/e2e/objects/views/viewItems/KafkaClusterItem.ts
@@ -1,3 +1,6 @@
+import { expect } from "@playwright/test";
+import { Notification } from "../../notifications/Notification";
+import { NotificationArea } from "../../notifications/NotificationArea";
 import { ViewItem } from "./ViewItem";
 
 export class KafkaClusterItem extends ViewItem {
@@ -9,5 +12,13 @@ export class KafkaClusterItem extends ViewItem {
   /** Copy the Kafka cluster's bootstrap servers to the clipboard via the right-click context menu. */
   async copyBootstrapServers(): Promise<void> {
     await this.rightClickContextMenuAction("Copy Bootstrap Server(s)");
+    // don't resolve until the "Copied ... to clipboard." info notification appears and is dismissed
+    const notificationArea = new NotificationArea(this.page);
+    const copyNotifications = notificationArea.infoNotifications.filter({
+      hasText: /Copied ".*" to clipboard./,
+    });
+    await expect(copyNotifications).toHaveCount(1);
+    const notification = new Notification(this.page, copyNotifications.first());
+    await notification.dismiss();
   }
 }
