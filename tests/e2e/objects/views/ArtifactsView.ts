14dc4ba45639035dc223b32f34242d73d66152a3
Cerchie <lcerchie@confluent.io>
2025-10-14 11:30:37 -0700

    select correct cluster

--- tests/e2e/objects/views/ArtifactsView.ts
+++ tests/e2e/objects/views/ArtifactsView.ts
@@ -1,6 +1,5 @@
 import { expect, Locator, Page } from "@playwright/test";
 import { ConnectionType } from "../../connectionTypes";
-import { Quickpick } from "../quickInputs/Quickpick";
 import { ResourcesView } from "./ResourcesView";
 import { View } from "./View";
 import { KafkaClusterItem } from "./viewItems/KafkaClusterItem";
@@ -9,6 +8,10 @@ export enum SelectFlinkDatabase {
   FromResourcesView = "Flink database action from the Resources view",
   FromArtifactsViewButton = "Artifacts view nav action",
 }
+
+const TEST_ENV_NAME = "flink-testing-env";
+const TEST_COMPUTE_POOL_NAME = "azure-pool";
+const TEST_KAFKA_CLUSTER_NAME = "azure-cluster";
 /**
  * Object representing the "Artifacts tree view"
  * {@link https://code.visualstudio.com/api/ux-guidelines/views#tree-views view} in the "Confluent"
@@ -43,34 +46,52 @@ export class ArtifactsView extends View {
     await expect(this.body).toBeVisible();
   }
 
+  get ccloudKafkaClusters(): Locator {
+    // Search from the entire page instead of just this view's tree items
+    return this.page
+      .locator('[role="treeitem"]')
+      .filter({ has: this.page.locator(".codicon-confluent-kafka-cluster") })
+      .and(this.page.locator("[aria-level='3'][aria-label^='CCLOUD connection: Kafka Cluster']"));
+  }
+
   async loadArtifacts(
     connectionType: ConnectionType,
     entrypoint: SelectFlinkDatabase,
     clusterLabel?: string | RegExp,
   ): Promise<void> {
+    const resourcesView = new ResourcesView(this.page);
+    // First, expand the CCloud env
+    await expect(resourcesView.ccloudEnvironments).not.toHaveCount(0);
+    await resourcesView.ccloudEnvironments.getByText(TEST_ENV_NAME).click();
+    // TODO: add second entrypoint option to select from the Artifacts view button
     switch (entrypoint) {
       case SelectFlinkDatabase.FromResourcesView: {
-        const resourcesView = new ResourcesView(this.page);
-        const cluster = await resourcesView.getKafkaCluster(connectionType);
-        const clusterItem = new KafkaClusterItem(this.page, cluster.first());
+        let kafkaClusters: Locator;
+        kafkaClusters = this.ccloudKafkaClusters;
+
+        const kafkaClusterItem = kafkaClusters.filter({ hasText: clusterLabel });
+        await expect(kafkaClusterItem).toBeVisible();
+        const clusterItem = new KafkaClusterItem(this.page, kafkaClusterItem);
         await clusterItem.selectAsFlinkDatabase();
-        break;
-      }
-      case SelectFlinkDatabase.FromArtifactsViewButton: {
-        await this.clickSelectKafkaClusterAsFlinkDatabase();
-        const kafkaClusterQuickpick = new Quickpick(this.page);
-        await expect(kafkaClusterQuickpick.locator).toBeVisible();
-        await expect(kafkaClusterQuickpick.items).not.toHaveCount(0);
-        const clusterItem = clusterLabel
-          ? kafkaClusterQuickpick.items.filter({ hasText: clusterLabel }).first()
-          : kafkaClusterQuickpick.items.first();
-        await clusterItem.click();
+
+        await expect(this.header).toHaveAttribute("aria-expanded", "true");
+        await expect(this.body).toBeVisible();
+        await expect(async () => {
+          const artifactCount = await this.artifacts.count();
+          const hasLoadingIndicator = await this.body
+            .locator('.loading, .spinner, [aria-label*="loading"]')
+            .isVisible();
+
+          // Either we have artifacts loaded OR we're still in a loading state
+          // This ensures we don't exit while artifacts are still being fetched
+          expect(artifactCount > 0 || !hasLoadingIndicator).toBeTruthy();
+          // timeout 0 means we wait indefinitely for one of the conditions to be true
+        }).toPass({ timeout: 0 });
+
         break;
       }
       default:
         throw new Error(`Unsupported entrypoint: ${entrypoint}`);
     }
-    await expect(this.header).toHaveAttribute("aria-expanded", "true");
-    await expect(this.body).toBeVisible();
   }
 }
--- tests/e2e/specs/flinkArtifact.spec.ts
+++ tests/e2e/specs/flinkArtifact.spec.ts
@@ -1,11 +1,9 @@
 import { expect } from "@playwright/test";
 import { test } from "../baseTest";
 import { ConnectionType } from "../connectionTypes";
-import { ArtifactsView } from "../objects/views/ArtifactsView";
+import { ArtifactsView, SelectFlinkDatabase } from "../objects/views/ArtifactsView";
 import { Tag } from "../tags";
-
-import { SelectFlinkDatabase } from "../objects/views/ArtifactsView";
-
+// TODO: Refactor with custom locators
 test.describe("Flink Artifacts", { tag: [Tag.CCloud, Tag.FlinkArtifacts] }, () => {
   test.use({ connectionType: ConnectionType.Ccloud });
 
@@ -18,8 +16,14 @@ test.describe("Flink Artifacts", { tag: [Tag.CCloud, Tag.FlinkArtifacts] }, () =
     // Use existing page object models for common interactions
     const artifactsView = new ArtifactsView(page);
     // NOTE: might have to be done differently, but right here all I want is to confirm the tab exists
-    await artifactsView.loadArtifacts(ConnectionType.Ccloud, SelectFlinkDatabase.FromResourcesView);
+    await artifactsView.loadArtifacts(
+      ConnectionType.Ccloud,
+      SelectFlinkDatabase.FromResourcesView,
+      "azure-cluster",
+    );
+    await artifactsView.clickArtifactsView();
     await expect(artifactsView.header).toHaveAttribute("aria-expanded", "true");
-    await expect(artifactsView.body).toBeVisible();
+    // Check that we have artifacts loaded (at least one)
+    await expect(artifactsView.artifacts.first()).toBeVisible();
   });
 });
