ee9536e3bbc66f48c37f7ba2cf65cbd0eeebc7ed
Noel Cothren <ncothren@confluent.io>
2025-10-15 14:36:14 -0600

    clean up & simplify

--- src/commands/flinkArtifacts.ts
+++ src/commands/flinkArtifacts.ts
@@ -68,10 +68,7 @@ export async function uploadArtifactCommand(
             `Artifact "${response.display_name}" uploaded successfully to Confluent Cloud.`,
             {
               "Register UDFs": async () => {
-                await detectClassesAndRegisterUDFs(
-                  { selectedFile: params.selectedFile },
-                  response.id,
-                );
+                await detectClassesAndRegisterUDFs(params.selectedFile, response.id);
               },
             },
           );
