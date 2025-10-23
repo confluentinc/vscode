c6c1a85a47cf5835382ff780aff3c0b2beda6088
James Robinson <jrobinson@confluent.io>
2025-10-23 09:51:54 -0400

    WIP

--- src/commands/flinkDatabaseView.test.ts
+++ src/commands/flinkDatabaseView.test.ts
@@ -1,11 +1,16 @@
 import * as sinon from "sinon";
 
 import * as indexModule from ".";
+import { FlinkDatabaseViewProvider } from "../viewProviders/flinkDatabase";
+import * as kafkaClusterCommandsModule from "./kafkaClusters";
+
 import {
+  createTopicInFlinkDatabaseViewCommand,
   registerFlinkDatabaseViewCommands,
   setFlinkRelationsViewModeCommand,
 } from "./flinkDatabaseView";
 
+import { TEST_CCLOUD_FLINK_DB_KAFKA_CLUSTER } from "../../tests/unit/testResources";
 import * as contextValuesModule from "../context/values";
 import * as emittersModule from "../emitters";
 import { FlinkDatabaseViewProviderMode } from "../viewProviders/multiViewDelegates/constants";
@@ -28,15 +33,20 @@ describe("commands/flinkDatabaseView.ts", () => {
       registerCommandWithLoggingStub = sandbox.stub(indexModule, "registerCommandWithLogging");
     });
 
-    it("should register the setFlinkRelationsViewModeCommand command", () => {
-      registerFlinkDatabaseViewCommands();
+    const commandNameAndFunctionPairs: [string, () => Promise<void>][] = [
+      ["confluent.flinkdatabase.setRelationsViewMode", setFlinkRelationsViewModeCommand],
+      ["confluent.flinkdatabase.createTopic", createTopicInFlinkDatabaseViewCommand],
+    ];
 
-      sinon.assert.calledOnce(registerCommandWithLoggingStub);
-      sinon.assert.calledWithExactly(
-        registerCommandWithLoggingStub,
-        "confluent.flinkdatabase.setRelationsViewMode",
-        setFlinkRelationsViewModeCommand,
-      );
+    commandNameAndFunctionPairs.forEach(([commandName, commandFunction]) => {
+      it(`should register the ${commandName} command`, () => {
+        registerFlinkDatabaseViewCommands();
+        sinon.assert.calledWithExactly(
+          registerCommandWithLoggingStub,
+          commandName,
+          commandFunction,
+        );
+      });
     });
   });
 
@@ -62,6 +72,47 @@ describe("commands/flinkDatabaseView.ts", () => {
         contextValuesModule.ContextValues.flinkDatabaseViewMode,
         FlinkDatabaseViewProviderMode.Relations,
       );
+    });
+  });
+
+  describe("createTopicInFlinkDatabaseViewCommand", () => {
+    let flinkDatabaseViewProviderInstance: FlinkDatabaseViewProvider;
+    let flinkDatabaseViewProviderGetInstanceStub: sinon.SinonStub;
+    let createTopicCommandStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      flinkDatabaseViewProviderInstance = new FlinkDatabaseViewProvider();
+      flinkDatabaseViewProviderGetInstanceStub = sandbox.stub(
+        FlinkDatabaseViewProvider,
+        "getInstance",
+      );
+      flinkDatabaseViewProviderGetInstanceStub.returns(flinkDatabaseViewProviderInstance);
+      createTopicCommandStub = sandbox.stub(kafkaClusterCommandsModule, "createTopicCommand");
+    });
+
+    afterEach(() => {
+      flinkDatabaseViewProviderInstance.dispose();
+    });
+
+    it("should start to create a topic in the selected Flink database's Kafka cluster", async () => {
+      // Mock a selected Flink database
+      sinon
+        .stub(flinkDatabaseViewProviderInstance, "database")
+        .get(() => TEST_CCLOUD_FLINK_DB_KAFKA_CLUSTER);
+      await createTopicInFlinkDatabaseViewCommand();
+
+      sinon.assert.calledOnceWithExactly(
+        createTopicCommandStub,
+        TEST_CCLOUD_FLINK_DB_KAFKA_CLUSTER,
+      );
+    });
+
+    it("should bail early Flink database is selected", async () => {
+      // Mock no selected Flink database
+      sinon.stub(flinkDatabaseViewProviderInstance, "database").get(() => undefined);
+      await createTopicInFlinkDatabaseViewCommand();
+
+      sinon.assert.notCalled(createTopicCommandStub);
     });
   });
 });
