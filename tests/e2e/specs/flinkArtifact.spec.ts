dcf6b9f2d7423178b2a0ac611c105dd2e90c97b5
Cerchie <lcerchie@confluent.io>
2025-11-03 14:34:54 -0700

    styling: spaces

--- tests/e2e/specs/flinkArtifact.spec.ts
+++ tests/e2e/specs/flinkArtifact.spec.ts
@@ -3,6 +3,7 @@ import { test } from "../baseTest";
 import { ConnectionType } from "../connectionTypes";
 import { ArtifactsView, SelectFlinkDatabase } from "../objects/views/ArtifactsView";
 import { Tag } from "../tags";
+
 test.describe("Flink Artifacts", { tag: [Tag.CCloud, Tag.FlinkArtifacts] }, () => {
   test.use({ connectionType: ConnectionType.Ccloud });
 
@@ -15,6 +16,7 @@ test.describe("Flink Artifacts", { tag: [Tag.CCloud, Tag.FlinkArtifacts] }, () =
     await artifactsView.loadArtifacts(SelectFlinkDatabase.FromResourcesView);
     await expect(artifactsView.header).toHaveAttribute("aria-expanded", "true");
   });
+
   test("should show Artifacts view when cluster selected from Artifacts view button", async ({
     page,
   }) => {
