3b05256427ffb75130144fbde126b24159ef7efd
Dave Shoup <dave.shoup@gmail.com>
2025-11-13 18:16:23 -0500

    fix tests and add test for collapsible state

--- src/viewProviders/multiViewDelegates/flinkDatabaseResource.test.ts
+++ src/viewProviders/multiViewDelegates/flinkDatabaseResource.test.ts
@@ -6,37 +6,46 @@ import type { IdItem } from "../../models/main";
 import type { IResourceBase, ISearchable } from "../../models/resource";
 import { FlinkDatabaseResourceContainer } from "./flinkDatabaseResource";
 
+// not Flink Database specific
+const fakeResource: IResourceBase & IdItem & ISearchable = {
+  connectionId: CCLOUD_CONNECTION_ID,
+  connectionType: ConnectionType.Ccloud,
+  id: "Resource1",
+  searchableText: () => "",
+};
+
 describe("viewProviders/multiViewDelegates/flinkDatabaseResource", () => {
   describe("FlinkDatabaseResource", () => {
     describe("constructor", () => {
       it("should create an instance with correct properties", () => {
         const label = "Test Database";
-        const collapsibleState = TreeItemCollapsibleState.Collapsed;
-        // fake base resources, not Flink Database specific
-        const fakeResource: IResourceBase & IdItem & ISearchable = {
-          connectionId: CCLOUD_CONNECTION_ID,
-          connectionType: ConnectionType.Ccloud,
-          id: "Resource1",
-          searchableText: () => "",
-        };
+
         const resources = [fakeResource, { ...fakeResource, id: "Resource2" }];
 
-        const item = new FlinkDatabaseResourceContainer(label, collapsibleState, resources);
+        const item = new FlinkDatabaseResourceContainer(label, resources);
 
         assert.strictEqual(item.label, label);
-        assert.strictEqual(item.collapsibleState, collapsibleState);
         assert.deepStrictEqual(item.children, resources);
         assert.strictEqual(item.id, `${item.connectionId}-${label}`);
+      });
+
+      it("should set collapsible state based on length of `.children`", () => {
+        const label = "Test Database";
+
+        const withChildren = new FlinkDatabaseResourceContainer(label, [fakeResource]);
+        assert.strictEqual(withChildren.collapsibleState, TreeItemCollapsibleState.Collapsed);
+
+        const withoutChildren = new FlinkDatabaseResourceContainer(label, []);
+        assert.strictEqual(withoutChildren.collapsibleState, TreeItemCollapsibleState.None);
       });
     });
 
     describe("searchableText", () => {
       it("should return the label as searchable text", () => {
         const label = "Searchable Database Resource";
-        const collapsibleState = TreeItemCollapsibleState.Collapsed;
         const resources: (IResourceBase & IdItem & ISearchable)[] = [];
 
-        const item = new FlinkDatabaseResourceContainer(label, collapsibleState, resources);
+        const item = new FlinkDatabaseResourceContainer(label, resources);
 
         const searchableText = item.searchableText();
         assert.strictEqual(searchableText, label);
