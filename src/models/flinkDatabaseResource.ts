8844ffe5509c255ddad670f6c04c3285542eef81
Dave Shoup <dave.shoup@gmail.com>
2025-11-17 17:03:45 -0500

    remove FlinkRelation for now since it doesn't include `databaseId` and isn't cached

--- src/models/flinkDatabaseResource.ts
+++ src/models/flinkDatabaseResource.ts
@@ -1,6 +1,5 @@
 import type { FlinkAIModel } from "./flinkAiModel";
-import type { FlinkRelation } from "./flinkRelation";
 import type { FlinkUdf } from "./flinkUDF";
 
-/** Union type for all resources that can belong to a Flink database. */
-export type FlinkDatabaseResource = FlinkUdf | FlinkRelation | FlinkAIModel;
+/** Union type for resources with a Flink database parent. */
+export type FlinkDatabaseResource = FlinkUdf | FlinkAIModel;
