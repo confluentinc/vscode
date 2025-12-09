1979b4939af0f51698eed8e8bf550c78d7ad5761
Dave Shoup <dave.shoup@gmail.com>
2025-12-09 14:20:25 -0500

    move createFakeFlinkDatabaseResource to tests/unit/

--- src/models/flinkDatabaseResourceContainer.test.ts
+++ src/models/flinkDatabaseResourceContainer.test.ts
@@ -1,41 +1,11 @@
 import * as assert from "assert";
 import type { ThemeIcon } from "vscode";
 import { TreeItemCollapsibleState } from "vscode";
-import {
-  TEST_CCLOUD_ENVIRONMENT_ID,
-  TEST_CCLOUD_FLINK_DB_KAFKA_CLUSTER,
-  TEST_CCLOUD_PROVIDER,
-  TEST_CCLOUD_REGION,
-} from "../../tests/unit/testResources";
+import { createFakeFlinkDatabaseResource } from "../../tests/unit/testResources/flinkDatabaseResource";
 import { ConnectionType } from "../clients/sidecar";
 import { CCLOUD_CONNECTION_ID, IconNames } from "../constants";
 import type { FlinkDatabaseResource } from "./flinkDatabaseResource";
 import { FlinkDatabaseResourceContainer } from "./flinkDatabaseResourceContainer";
-
-/**
- * Create a general-purpose {@link FlinkDatabaseResource} object.
- * This will return only the core/shared properties of the database resource types to help test
- * the {@link FlinkDatabaseResourceContainer} class.
- */
-function createFakeFlinkDatabaseResource(
-  options?: Partial<Omit<FlinkDatabaseResource, "connectionId" | "connectionType">>,
-): FlinkDatabaseResource {
-  return {
-    // these won't change until we support other connection types:
-    connectionId: CCLOUD_CONNECTION_ID,
-    connectionType: ConnectionType.Ccloud,
-    // database details:
-    environmentId: options?.environmentId ?? TEST_CCLOUD_ENVIRONMENT_ID,
-    provider: options?.provider ?? TEST_CCLOUD_PROVIDER,
-    region: options?.region ?? TEST_CCLOUD_REGION,
-    databaseId: options?.databaseId ?? TEST_CCLOUD_FLINK_DB_KAFKA_CLUSTER.id,
-    // base resource details:
-    id: options?.id ?? "Resource1",
-    name: options?.name ?? "Test Resource",
-    iconName: options?.iconName ?? IconNames.PLACEHOLDER,
-    searchableText: options?.searchableText ?? (() => ""),
-  };
-}
 
 describe("models/flinkDatabaseResourceContainer", () => {
   describe("FlinkDatabaseResourceContainer", () => {
