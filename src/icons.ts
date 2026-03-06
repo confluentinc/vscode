441388a66b2dcf8433c84724b037056b17f9e8f4
James Robinson <jrobinson@confluent.io>
2026-03-06 11:48:42 -0500

    Simplify icons for demo

--- src/icons.ts
+++ src/icons.ts
@@ -43,11 +43,11 @@ export enum IconNames {
   /** General-purpose icon to use when we don't have a dedicated icon for a given resource. */
   PLACEHOLDER = "symbol-misc",
   /** Icon for ROW types in TreeView */
-  FLINK_TYPE_ROW = "symbol-struct",
+  FLINK_TYPE_ROW = "confluent-function",
   /** Icon for ARRAY types in TreeView */
   FLINK_TYPE_ARRAY = "symbol-array",
   /** Icon for MULTISET types in TreeView */
-  FLINK_TYPE_MULTISET = "symbol-package",
+  FLINK_TYPE_MULTISET = "confluent-function",
 }
 
 /** Red "warning" icon to use when dealing with connectivity issues or failed resource fetching. */
