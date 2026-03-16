76e3bd4ab9b6ef88486122fabfca1d68e220c581
Dave Shoup <dave.shoup@gmail.com>
2026-03-16 11:57:51 -0400

    check for coverage.json file before trying to create report

--- Gulpfile.js
+++ Gulpfile.js
@@ -756,7 +756,7 @@ export async function testRun() {
       ],
     });
   } finally {
-    if (reportCoverage) {
+    if (reportCoverage && existsSync("./coverage.json")) {
       let coverageMap = libCoverage.createCoverageMap();
       let sourceMapStore = libSourceMaps.createSourceMapStore();
       coverageMap.merge(JSON.parse(await readFile("./coverage.json")));
